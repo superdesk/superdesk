107198ceee514c884d2e4539911bb87c9c257466
petr.jasek <petr.jasek@sourcefabric.org>
2013-08-01 09:46:55 +0200

    Initial eve server.

---
+++ run.py
@@ -0,0 +1,6 @@
+from eve import Eve
+
+app = Eve()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- settings.py
+++ settings.py
@@ -2,4 +2,49 @@ import os
 
 DEBUG = False
 
-DB_NAME = os.environ.get('MONGOHQ_URL', 'superdesk')
+MONGO_DBNAME = os.environ.get('MONGOHQ_URL', 'superdesk')
+
+SERVER_NAME = 'localhost:5000'
+
+DATE_CREATED = 'firstCreated'
+LAST_UPDATED = 'versionCreated'
+
+DOMAIN = {
+    'items': {
+        'schema': {
+            'guid': {
+                'type': 'string',
+                'unique': True,
+            },
+            'headline': {
+                'type': 'string',
+                'required': True,
+            },
+            'slugline': {
+                'type': 'string',
+            },
+            'creditline': {
+                'type': 'string',
+            },
+            'copyrightHolder': {
+                'type': 'string',
+            },
+            'firstCreated': {
+                'type': 'datetime',
+            },
+            'versionCreated': {
+                'type': 'datetime',
+            },
+            'version': {
+                'type': 'integer',
+            },
+            'itemClass': {
+                'type': 'string',
+            },
+            'contents': {
+                'type': 'list',
+            },
+        },
+        'resource_methods': ['GET', 'POST'],
+    },
+}
