39b49166a73c107753098ca1d73e9a1e9566bf05
petr.jasek <petr.jasek@sourcefabric.org>
2013-08-28 16:42:47 +0200

    Add user logout action.

--- app/scripts/superdesk/auth/module.js
+++ app/scripts/superdesk/auth/module.js
@@ -18,5 +18,11 @@ define([
             $rootScope.$on('auth.login', function() {
                 $route.reload();
             });
-        });
+        }).
+        controller('UserController', ['$scope', 'authService', function($scope, authService) {
+            $scope.logout = function() {
+                authService.logout();
+                $scope.$emit('auth.doLogin');
+            };
+        }]);
 });
