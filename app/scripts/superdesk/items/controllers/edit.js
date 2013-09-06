772a344a8d23d019445cdede5d00394e55adaef8
petr.jasek <petr.jasek@sourcefabric.org>
2013-09-06 17:06:00 +0200

    Manage list pagination via route params.

--- app/scripts/superdesk/items/controllers/edit.js
+++ app/scripts/superdesk/items/controllers/edit.js
@@ -1,10 +1,14 @@
 define(['angular'], function(angular) {
-    return ['$scope', 'item', 'ItemService', function($scope, item, ItemService) {
-
+    return ['$scope', '$routeParams', 'item', 'ItemService',
+    function($scope, $routeParams, item, ItemService) {
         $scope.item = item;
 
         $scope.save = function(item) {
             ItemService.update(item);
         };
+
+        if ('skip' in $routeParams) {
+            $scope.closeLinkParams = '?skip=' + $routeParams.skip;
+        }
     }];
 });
--- app/scripts/superdesk/items/controllers/list.js
+++ app/scripts/superdesk/items/controllers/list.js
@@ -2,36 +2,44 @@ define(['angular'], function(angular) {
     'use strict';
 
     var LIST_CLASS = 'icls:package';
-    var STORAGE_KEY = 'items.list';
-    var storage = sessionStorage;
 
-    return ['$scope', 'ItemListLoader', function($scope, ItemListLoader) {
-        $scope.params = {
+    return ['$scope', '$routeParams', 'ItemListLoader', function($scope, $routeParams, ItemListLoader) {
+        $scope.params = angular.extend({
             sort: '[("firstCreated", -1)]',
             where: {itemClass: LIST_CLASS},
             skip: 0,
             limit: 25
+        }, $routeParams);
+
+        $scope.links = {
+            prev_link: null,
+            next_link: null
         };
 
-        $scope.fetch = function(options) {
-        	angular.extend($scope.params, options);
-            storage.setItem(STORAGE_KEY, angular.toJson($scope.params));
-        	$scope.items = ItemListLoader($scope.params);
-        };
-
-        $scope.prev = function() {
+        function getPrevLink() {
             var skip = $scope.params.skip - $scope.params.limit;
             if (skip < 0) {
                 skip = 0;
             }
-            $scope.fetch({skip: skip});
-        };
 
-        $scope.next = function() {
-            var skip = $scope.params.skip + $scope.params.limit;
-            $scope.fetch({skip: skip});
+            return skip === 0 ? '' : '?skip=' + skip;
         }
 
+        function getNextLink() {
+            var skip = parseInt($scope.params.skip) + parseInt($scope.params.limit);
+            return '?skip=' + skip;
+        }
+
+        $scope.fetch = function(options) {
+        	angular.extend($scope.params, options);
+        	ItemListLoader($scope.params).
+                then(function(items) {
+                    $scope.items = items;
+                    $scope.links.prev_link = items.has_prev ? getPrevLink() : null;
+                    $scope.links.next_link = items.has_next ? getNextLink() : null;
+                });
+        };
+        
         $scope.search = function(query) {
         	if (query && query.length > 2) {
         		$scope.fetch({
@@ -46,11 +54,6 @@ define(['angular'], function(angular) {
         	}
         };
 
-        if (storage.hasOwnProperty(STORAGE_KEY)) {
-            var options = angular.fromJson(storage.getItem(STORAGE_KEY));
-            $scope.fetch(options);
-        } else {
-            $scope.fetch();
-        }
+        $scope.fetch();
     }];
 });
