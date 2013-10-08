b5ccddaeaf86847f2b71f09caa2c43a825d3b53c
petr.jasek <petr.jasek@sourcefabric.org>
2013-10-08 11:01:19 +0200

    Ignore bower_components specs when testing.

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
 
         // list of files to exclude
         exclude: [
+          'app/scripts/bower_components/**/*Spec.js',
           'app/main.js'
         ],
 
