08a07bab90f372e94aaf8e0e2efe3d1c3e148f75
petr.jasek <petr.jasek@sourcefabric.org>
2013-10-09 11:54:38 +0200

    Document superdesk.services.translate module.

--- app/scripts/superdesk/services/translate.js
+++ app/scripts/superdesk/services/translate.js
@@ -5,6 +5,12 @@ define([
 ], function(angular) {
     'use strict';
 
+    /**
+     * Translate module
+     *
+     * This module provides localization support.
+     * It's using angular-gettext.
+     */
     angular.module('superdesk.services.translate', ['gettext']).
         run(['gettextCatalog', '$location', function(gettextCatalog, $location) {
             var params = $location.search();
@@ -13,6 +19,15 @@ define([
                 gettextCatalog.debug = true;
             }
         }]).
+        /**
+         * Gettext service to be used in controllers/services/directives.
+         *
+         * Usage:
+         * function($scope, gettext) { $scope.translatedMessage = gettext("Translate Me"); }
+         *
+         * This way "Translate Me" can be found by the string extractor and it will return
+         * translated string if appropriet.
+         */
         factory('gettext', function(gettextCatalog) {
             return function(input) {
                 return gettextCatalog.getString(input);
