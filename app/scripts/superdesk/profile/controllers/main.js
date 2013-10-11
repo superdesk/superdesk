5c2c6230c765c112e147f8ee064d098d8b08ada0
petr.jasek <petr.jasek@sourcefabric.org>
2013-10-11 11:34:50 +0200

    Implement my profile update.
    SDUX-13 #resolve

--- app/scripts/superdesk/profile/controllers/main.js
+++ app/scripts/superdesk/profile/controllers/main.js
@@ -1,32 +1,22 @@
 define(['angular'], function(angular) {
     'use strict';
 
-    return ['$scope', 'user', 'usersResource', 'activityResource',
-        function($scope, user, usersResource, activityResource) {
+    return ['$scope', '$timeout', 'user', 'server',
+    function($scope, $timeout, user, server) {
+        $scope.user = user;
 
-            $scope.userroles = ["administrator","journalist","photographer","editor"];
-            $scope.user = angular.extend(user, {role: []});
-
-            usersResource.get(function(data){
-                $scope.privacy = data.privacy;  
-                $scope.showrow = data.showrow;            
-            });
-    		
-                		
-            activityResource.get(function(data){
-                $scope.activityfeed = data.activityfeed;
-            });
-
-            $scope.selectrole = function(role) {
-                if ($scope.user.role.indexOf(role) == -1) {         //if not in array
-                     $scope.$apply($scope.user.role.push(role));    //put it
-                }
-                else {                                              //if is in array
-                    if ($scope.user.role.length !== 1)   {          //and there is more than one value
-                        $scope.$apply($scope.user.role.splice($scope.user.role.indexOf(role),1)); //remove it
-                    }
-                }  
+        $scope.save = function() {
+            if ('password' in $scope.user && !$scope.user.password) {
+                delete $scope.user.password;
             }
-            
+
+            $scope.msg = 'info';
+            server.update($scope.user).then(function() {
+                $scope.msg = 'success';
+                $timeout(function() {
+                    $scope.msg = null;
+                }, 3000);
+            });
+        };
     }];
 });
