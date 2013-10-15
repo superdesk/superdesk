160966ca80bef8ba7fcd30e3978bae87149f1a9f
eozan <erdemozanozbek@gmail.com>
2013-10-16 00:48:35 +0200

    State service
    Removed old code from users list controller.

--- app/scripts/superdesk/users/controllers/list.js
+++ app/scripts/superdesk/users/controllers/list.js
@@ -1,26 +1,11 @@
 define(['angular'], function(angular) {
     'use strict';
 
-    return ['$scope', '$location', '$routeParams', 'settings', 'state', 'server', 'users', 'defaultListParams',
-    function($scope, $location, $routeParams, settings, state, server, users, defaultListParams) {
+    return ['$scope', '$location', 'settings', 'state', 'server', 'users',
+    function($scope, $location, settings, state, server, users) {
         
-        $scope.$watch('routeParams', function() {
-            var routeParams = {};
-            for (var i in $scope.routeParams) {
-                if ($scope.routeParams[i] !== defaultListParams[i]) {
-                    routeParams[i] = $scope.routeParams[i];
-                }
-            }
-            $location.search(routeParams);
-        }, true);
-
-        //
-
         $scope.users = users;
         $scope.settings = settings;
         $scope.state = state;
-
-        $routeParams = angular.extend({}, defaultListParams, $routeParams);
-        $scope.routeParams = $routeParams;
     }];
 });
