acfd8b6d68fda0848accb8d2d7c28918300e01c8
Nikola Nikolic <nikolic_nikola@live.com>
2013-10-16 21:19:50 +0200

    Make simple dashboard edit mode
    User have ability to add, remove and resize widget, and rearrange
    widgets on dashboard

--- app/scripts/superdesk/dashboard/controllers/main.js
+++ app/scripts/superdesk/dashboard/controllers/main.js
@@ -1,46 +1,90 @@
-define(['angular'], function(angular) {
+define(['angular'], function(){
     'use strict';
 
     return ['$scope',
-    	function($scope) {
-
+        function($scope){
             $scope.widgets = [
                 {
-                    name:'World Clock', 
-                    row:1, col:1, sizex:1, sizey:1, 
-                    icon: "time",
-                    template : "scripts/superdesk/dashboard/views/widgets/widget-worldclock.html",
-                    class : "world-clock"
+                    name:'Perfect Dash',
+                    row:1,
+                    col:2,
+                    sizex:1,
+                    sizey:2,
+                    icon: 'leaf',
+                    template: 'scripts/superdesk/dashboard/views/widgets/widget-default.html',
+                    class : 'default'
                 },
                 {
-                    name:'Perfect Dash', 
-                    row:1, col:2, sizex:1, sizey:2, 
-                    icon: "leaf",
-                    template: "scripts/superdesk/dashboard/views/widgets/widget-default.html",
-                    class : "default"
+                    name:'World Clock',
+                    row:1,
+                    col:1,
+                    sizex:1,
+                    sizey:1,
+                    icon: 'time',
+                    template : 'scripts/superdesk/dashboard/views/widgets/widget-worldclock.html',
+                    class : 'world-clock'
                 },
                 {
-                    name:'Default Widget', 
-                    row:2, col:1, sizex:1, sizey:1, 
-                    icon: "leaf",
-                    template: "scripts/superdesk/dashboard/views/widgets/widget-default.html",
-                    class : "default"
+                    name:'Default Widget',
+                    row:2,
+                    col:1,
+                    sizex:1,
+                    sizey:1,
+                    icon: 'leaf',
+                    template: 'scripts/superdesk/dashboard/views/widgets/widget-default.html',
+                    class : 'default'
                 },
                 {
-                    name:'Widget Cool', 
-                    row:1, col:3, sizex:2, sizey:1, 
-                    icon: "leaf",
-                    template: "scripts/superdesk/dashboard/views/widgets/widget-default.html",
-                    class : "default"
+                    name:'Widget Cool',
+                    row:1,
+                    col:3,
+                    sizex:2,
+                    sizey:1,
+                    icon: 'leaf',
+                    template: 'scripts/superdesk/dashboard/views/widgets/widget-default.html',
+                    class : 'default'
                 },
                 {
-                    name:'DashDash', 
-                    row:2, col:3, sizex:1, sizey:1, 
-                    icon: "leaf",
-                    template: "scripts/superdesk/dashboard/views/widgets/widget-default.html",
-                    class : "default"
+                    name:'DashDash',
+                    row:2,
+                    col:3,
+                    sizex:1,
+                    sizey:1,
+                    icon: 'leaf',
+                    template: 'scripts/superdesk/dashboard/views/widgets/widget-default.html',
+                    class : 'default'
                 }
                 
             ];
-    }];
+
+            $scope.addWidget = function() {
+                //add
+                $scope.widgets.push({
+                    name:'Widget #'+($scope.widgets.length+1),
+                    row:1,
+                    col:1,
+                    sizex:1,
+                    sizey:1,
+                    icon: 'leaf',
+                    template: 'scripts/superdesk/dashboard/views/widgets/widget-default.html',
+                    class : 'default'
+                });
+            };
+
+
+
+            $scope.editmode = false;
+
+            $scope.disableDragging = function() {
+                $scope.gridster.disable();
+                $scope.editmode = false;
+            };
+
+            $scope.enableDragging = function() {
+                $scope.gridster.enable();
+                $scope.editmode = true;
+            };
+
+
+        }];
 });
--- app/scripts/superdesk/dashboard/directives.js
+++ app/scripts/superdesk/dashboard/directives.js
@@ -1,8 +1,7 @@
 define([
     'jquery',
     'angular',
-    'bootstrap_ui'
-], function($, angular, bootstrap_ui) {
+], function($, angular) {
     'use strict';
 
     angular.module('superdesk.dashboard.directives', []).
@@ -11,18 +10,15 @@ define([
                 templateUrl : 'scripts/superdesk/dashboard/views/widget.html',
                 replace: true,
                 transclude: true,
-                restrict: 'A',
-                require : 'ngModel'
+                restrict: 'A'
             };
         }).
-        directive('gridster', function($timeout) {
+        directive('dashboardManager', function($timeout) {
             return {
                 restrict: 'A',
-                require : 'ngModel',
-                template: '<ul><div sd-grid-widget ng-repeat="widget in widgets" ng-model="widget"></div></ul>',
-                link: function($scope, $element, $attributes, $controller) {
-                    
-                    var gridster;
+                template: '<ul class="dash-grid"><div sd-grid-widget ng-repeat="widget in widgets" ng-model="widget"></div></ul>',
+                controller : function($scope, $element) {
+                   
                     var ul = $element.find('ul');
 
                     var defaultOptions = {
@@ -31,35 +27,86 @@ define([
                     };
 
                     $timeout(function() {
-                        gridster = ul.gridster(defaultOptions).data('gridster');
+                        $scope.gridster = ul.gridster(defaultOptions).data('gridster');
                     
-                        gridster.options.draggable.stop = function(event, ui) {
+                        $scope.gridster.options.draggable.stop = function() {
                             angular.forEach(ul.find('> li'), function(item, index) {
                                 var li = angular.element(item);
-                                if (li.attr('class') === 'preview-holder') return;
+                                if (li.attr('class') === 'preview-holder') { return; }
                                 var widget = $scope.widgets[index];
                                 widget.row = li.attr('data-row');
                                 widget.col = li.attr('data-col');
                             });
                             $scope.$apply();
                         };
-                    });                    
+
+                        $scope.gridster.disable();
+                        
+                    });
+
+
+                    
+
+                    var addWidgetToGridster = function() {
+                        var li = ul.find('> li').eq($scope.widgets.length-1);
+                        var $w = li.addClass('gs_w').appendTo($scope.gridster.$el);
+                        $scope.gridster.$widgets = $scope.gridster.$widgets.add($w);
+                        $scope.gridster.register_widget($w).add_faux_rows(1).set_dom_grid_height();
+                    };
+
+                    $scope.$watch('widgets.length', function(newValue, oldValue) {
+                        if (newValue !== oldValue+1) { return; }
+                        $timeout(function() { addWidgetToGridster(); });
+                    });
+
+                    $scope.removeWidget = function(elindex) {
+                        var $w = ul.find('> li').eq(elindex);
+                        $scope.widgets.splice(elindex,1);
+                        $scope.gridster.remove_widget($w);
+                    };
+
+                    $scope.resizeWidget = function(index, direction) {
+                        var $w = ul.find('> li').eq(index);
+                        var widget = $scope.widgets[index];
+                        switch(direction) {
+                            case "left" : 
+                                widget.sizex == 1 ?  '' : widget.sizex--;
+                                break;
+                            case "right" :
+                                widget.sizex == 4 ? '' : widget.sizex++;
+                                break;
+                            case "up" : 
+                                widget.sizey == 1 ?  '' : widget.sizey--;
+                                break;
+                            case "down" :
+                                widget.sizey++;
+                                break;
+                        }
+                       
+                        $scope.gridster.resize_widget($w, widget.sizex, widget.sizey )
+                    };
                 }
             };
         }).
         
         directive('sdGridWidget', function() {
             return {
-                restrict: 'AC',
-                require : 'ngModel',
+                restrict: 'A',
                 replace: true,
                 template:
                     '<li data-col="{{widget.col}}" data-row="{{widget.row}}" data-sizex="{{widget.sizex}}" data-sizey="{{widget.sizey}}">'+
-                        '<div sd-widget ng-model="widget"></div>'+
+                        '<div class="widget-resize-width" ng-show="editmode">'+
+                            '<div class="resize-left" ng-click="resizeWidget($index,\'left\')" ></div>'+
+                            '<div class="resize-right" ng-click="resizeWidget($index,\'right\')" ></div>'+
+                        '</div>'+
+                        '<div class="widget-resize-height" ng-show="editmode">'+
+                            '<div class="resize-up" ng-click="resizeWidget($index,\'up\')" ></div>'+
+                            '<div class="resize-down" ng-click="resizeWidget($index,\'down\')" ></div>'+
+                        '</div>'+
+                        '<div class="widget-close" ng-click="removeWidget($index)" ng-show="editmode"></div>'+
+                        '<div sd-widget></div>'+
                     '</li>',
-                link: function($scope, $element, $attributes, $controller) {
-                }
             };
-        })
+        });
         
 });
