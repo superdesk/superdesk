4c79f25675bed443bcde8a6181d7a66f3a73f604
Nikola Nikolic <nikolic_nikola@live.com>
2013-11-14 15:41:13 +0100

    Show "waiting wheel" while ingesting item
    Add loading status to thumbnail view
    SDUX-78

--- app/scripts/superdesk-items/controllers/ingest.js
+++ app/scripts/superdesk-items/controllers/ingest.js
@@ -1,17 +1,18 @@
 define(['angular'], function(angular) {
     'use strict';
 
-    return ['$scope', 'items', 'em', 'notify', 'gettext',
-    function($scope, items, em, notify, gettext) {
+    return ['$scope', 'items', 'em',
+    function($scope, items, em) {
 
         $scope.items = items;
 
         $scope.archive = function(item) {
-            notify.info(gettext('Saving..'));
             em.create('archive', item).then(function() {
-                notify.pop();
-                notify.success(gettext('Item archived!'), 3000);
+                item.archived = true;
+                item.archiving = false;
             });
+            item.archiving = true;
         };
+
     }];
 });
