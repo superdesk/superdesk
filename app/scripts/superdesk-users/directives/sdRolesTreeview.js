3c7dba93f92f513735631c8fe25e739f2a2cfc63
Nikola Nikolic <nikolic_nikola@live.com>
2013-11-28 15:55:34 +0100

    Add collapse button for treeview

--- app/scripts/superdesk-users/directives/sdRolesTreeview.js
+++ app/scripts/superdesk-users/directives/sdRolesTreeview.js
@@ -7,9 +7,24 @@ define(function() {
             terminal : true,
             scope: { val: '='},
             link: function(scope, element, attrs) {
-                var template = '<div class="leaf">{{val.name}}</div>';
+                var template = '';
+                
                 if (angular.isArray(scope.val.items)) {
-                    template += '<ul class="indent"><li ng-repeat="item in val.items"><div sd-roles-treeview val="item"></div></li></ul>';
+                    template += '<div class="leaf">'+
+                                    '<span class="collapse" ng-click="collapsed = !collapsed">'+
+                                        '<i class="icon-chevron-down" ng-show="collapsed"></i>'+
+                                        '<i class="icon-chevron-right" ng-show="!collapsed"></i>'+
+                                    '</span>'+
+                                    '{{val.name}}'+
+                                '</div>';
+                    template += '<ul class="indent" ng-show="collapsed">'+
+                                    '<li ng-repeat="item in val.items">'+
+                                        '<div sd-roles-treeview val="item"></div>'+
+                                    '</li>'+
+                                '</ul>';
+                }
+                else {
+                    template += '<div class="leaf">{{val.name}}</div>';
                 }
                 var newElement = angular.element(template);
                 $compile(newElement)(scope);
