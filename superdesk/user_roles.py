de30ffa40f4feb6f6af5f2e050472d0593f337d9
petr.jasek <petr.jasek@sourcefabric.org>
2013-12-03 16:41:30 +0100

    Remove dead code.

--- superdesk/user_roles.py
+++ superdesk/user_roles.py
@@ -1,17 +1,6 @@
 
 import superdesk
 
-
-def sync_users_role_name(data, **kwargs):
-    if kwargs['updates'].get('name'):
-        old_role = data.find_one('user_roles', _id=str(kwargs['id']))
-        query = {'role': old_role['name']}
-        update = {'role': kwargs['updates']['name']}
-        data.update_all('users', query, update)
-
-
-superdesk.connect('update:user_roles', sync_users_role_name)
-
 superdesk.domain('user_roles', {
     'schema': {
         'name': {
