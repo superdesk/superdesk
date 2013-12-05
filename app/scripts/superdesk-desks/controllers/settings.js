3cf2a44fc0343da453c55b953d662e3a64b06321
Nikola Nikolic <nikolic_nikola@live.com>
2013-12-05 14:44:36 +0100

    Port desk listing UI from superdesk
    Add some basic static data
    List desks, its members and boards

--- app/scripts/superdesk-desks/controllers/settings.js
+++ app/scripts/superdesk-desks/controllers/settings.js
@@ -4,5 +4,104 @@ define(['angular', 'lodash'], function(angular, _) {
     return ['$scope',
         function($scope) {
 
-    }];
+			$scope.desks = [
+				{
+					name : 'Culture',
+					members : [
+						{
+							name : 'Paco González'
+						},
+						{
+							name : 'Matina Stevis'
+						},
+						{
+							name : 'April O\'Neil'
+						}
+					],
+					cards : [
+						{
+							name : 'To Do',
+							limit : 50,
+							color : 1,
+							statuses : [
+								{
+									key : 'created'
+								},
+								{
+									key : 'just added'
+								},
+								{
+									key : 'breaking'
+								}
+							]
+						},
+						{
+							name : 'In progress',
+							limit : 10,
+							color : 2,
+							statuses : [
+								{
+									key : 'in progress'
+								},
+								{
+									key : 'writing'
+								}
+							]
+						},
+						{
+							name : 'In progress',
+							limit : 300,
+							color : 3,
+							statuses : [
+								{
+									key : 'done'
+								},
+								{
+									key : 'finished'
+								}
+							]
+						}
+					],
+					statuses : [
+						{
+							key : 'created'
+						},
+						{
+							key : 'just added'
+						},
+						{
+							key : 'breaking'
+						},
+						{
+							key : 'in progress'
+						},
+						{
+							key : 'writing'
+						},
+						{
+							key : 'finished'
+						},
+						{
+							key : 'in review'
+						},
+						{
+							key : 'done'
+						}
+					]
+				},
+				{
+					name : 'Politics',
+					members : [],
+					cards : [],
+					taskstatuses : []
+				},
+				{
+					name : 'Sport',
+					members : [],
+					cards : [],
+					taskstatuses : []
+				}
+			];
+
+		}];
 });
