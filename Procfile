cbf789b4198542771f8e346cd097f8bc53d562bd
petr.jasek <petr.jasek@sourcefabric.org>
2013-12-06 17:30:32 +0100

    Set stdout access log when gunicorn runs localy.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: newrelic-admin run-program gunicorn -b "0.0.0.0:$PORT" -w 3 wsgi
+web: newrelic-admin run-program gunicorn -c gunicorn_config.py wsgi
---
+++ gunicorn_config.py
@@ -0,0 +1,11 @@
+
+import os
+
+port = int(os.environ.get('PORT', 5000))
+bind = '%s:%d' % ('0.0.0.0', port)
+workers = 4
+
+if port == 5000:
+    debug = True
+    accesslog = '-'
+    access_log_format = '%(h)s %(l)s %(u)s %(t)s “%(r)s” %(s)s %(b)s “%(f)s” %(D)s'
