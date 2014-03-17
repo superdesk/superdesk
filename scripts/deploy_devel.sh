c022955d21f6a39e52ac9f0bad297ea99ef9693c
petr.jasek <petr.jasek@sourcefabric.org>
2014-03-17 14:36:38 +0100

    fix(deploy): fix test server url

--- scripts/deploy_devel.sh
+++ scripts/deploy_devel.sh
@@ -3,7 +3,7 @@
 set -e
 
 # build
-grunt build --server='//apytest.apy.sd-test.sourcefabric.org'
+grunt build --server='https://apytest.apy.sd-test.sourcefabric.org/api'
 
 # deploy
 rsync -ave ssh dist/* lab:superdesk/devel/
