b91d6ec36e79411595d3e117ed509af7608e0502
petr.jasek <petr.jasek@sourcefabric.org>
2014-03-26 10:57:21 +0100

    fix(auth): reload page on logout
    fix SD-168

--- app/scripts/superdesk/auth/auth.js
+++ app/scripts/superdesk/auth/auth.js
@@ -9,66 +9,67 @@ define([
 ], function(angular, require) {
     'use strict';
 
+    /**
+     * Expire session on 401 server response
+     */
+    AuthExpiredInterceptor.$inject = ['session', '$injector', '$rootScope'];
+    function AuthExpiredInterceptor(session, $injector, $rootScope) {
+        return {
+            response: function(response) {
+                if (response.status === 401) {
+                    session.expire();
+                    $rootScope.forcedLogout = true;
+                    return session.getIdentity().then(function() {
+                        $rootScope.forcedLogout = false;
+                        var $http = $injector.get('$http');
+                        $http.defaults.headers.common.Authorization = session.token;
+                        response.config.headers.Authorization = session.token;
+                        return $http(response.config);
+                    });
+                }
+
+                return response;
+            }
+        };
+    }
+
     angular.module('superdesk.auth', ['superdesk', 'ngRoute'])
         .service('auth', require('./auth-service'))
         .service('session', require('./session-service'))
         .service('authAdapter', require('./allypy-auth-service'))
         .directive('sdLoginModal', require('./login-modal-directive'))
-
-        /**
-         * Intercept $http response errors and do login on 401
-         */
         .config(['$httpProvider', function($httpProvider) {
-            $httpProvider.interceptors.push(['session', '$injector', function(session, $injector) {
-                return {
-                    response: function(response) {
-                        if (response.status === 401) {
-                            session.expire();
-                            return session.getIdentity().then(function() {
-                                var $http = $injector.get('$http');
-                                $http.defaults.headers.common.Authorization = session.token;
-                                response.config.headers.Authorization = session.token;
-                                return $http(response.config);
-                            });
-                        }
-
-                        return response;
-                    }
-                };
-            }]);
+            $httpProvider.interceptors.push(AuthExpiredInterceptor);
         }])
 
-        // watch session
-        .run(['$rootScope', '$http', 'session', function($rootScope, $http, session) {
-
-            $rootScope.forcedLogout = false;
+        // watch session token, identity
+        .run(['$rootScope', '$route', '$location', '$http', '$window', 'session',
+        function($rootScope, $route, $location, $http, $window, session) {
 
             $rootScope.logout = function() {
                 session.expire();
-                $rootScope.forcedLogout = false;
+                $window.location.replace('/'); // reset page for new user
             };
 
+            // populate current user
             $rootScope.$watch(function() {
                 return session.identity;
             }, function (identity) {
                 $rootScope.currentUser = session.identity;
             });
 
+            // set auth header
             $rootScope.$watch(function() {
                 return session.token;
             }, function(token) {
-
-                if (token != null) {
+                if (token) {
                     $http.defaults.headers.common.Authorization = token;
-                    $rootScope.forcedLogout = true;
                 } else {
                     delete $http.defaults.headers.common.Authorization;
                 }
             });
-        }])
 
-        // stop routing when there is no session and reload after
-        .run(['$rootScope', '$route', '$http', 'session', function($rootScope, $route, $http, session) {
+            // prevent routing when there is no token
             $rootScope.$on('$locationChangeStart', function (e) {
                 if (!session.token) {
                     session.getIdentity().then(function() {
