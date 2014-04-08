03eb5ce6fe89d3691a88bb7d1a1b03758aa7f247
Ozan Ozbek <ozan.ozbek@sourcefabric.org>
2014-04-08 10:57:38 +0200

    fix(core): Fixed Raven initialization.
    SD-74: Implement sentry to inform us if users are experiencing errors

--- app/scripts/errorCatcher.js
+++ app/scripts/errorCatcher.js
@@ -4,33 +4,37 @@ define([
 ], function(angular, Raven) {
     'use strict';
 
-    Raven.config(Configuration.raven.dsn, {logger: 'javascript-client'}).install();
+    var app = angular.module('errorCatcher', []);
+    
+    if (Configuration.raven.dsn) {
+        Raven.config(Configuration.raven.dsn, {logger: 'javascript-client'}).install();
 
-    angular.module('errorCatcher', [])
-    .factory('$exceptionHandler', function () {
-        return function errorCatcherHandler(exception, cause) {
-            Raven.captureException(exception, {tags: {component: 'ui'}, extra: exception});
-            console.log(exception.stack);
-            console.log(cause);
-        };
-    })
-    .factory('errorHttpInterceptor', ['$q', function ($q) {
-        return {
-            responseError: function responseError(rejection) {
-                if (rejection.status >= 500 && rejection.status < 600) {
-                    Raven.captureException(new Error('HTTP response error'), {
-                        tags: {component: 'server'},
-                        extra: {
-                            config: rejection.config,
-                            status: rejection.status
-                        }
-                    });
+        app
+        .factory('$exceptionHandler', function () {
+            return function errorCatcherHandler(exception, cause) {
+                Raven.captureException(exception, {tags: {component: 'ui'}, extra: exception});
+                console.log(exception.stack);
+                console.log(cause);
+            };
+        })
+        .factory('errorHttpInterceptor', ['$q', function ($q) {
+            return {
+                responseError: function responseError(rejection) {
+                    if (rejection.status >= 500 && rejection.status < 600) {
+                        Raven.captureException(new Error('HTTP response error'), {
+                            tags: {component: 'server'},
+                            extra: {
+                                config: rejection.config,
+                                status: rejection.status
+                            }
+                        });
+                    }
+                    return $q.reject(rejection);
                 }
-                return $q.reject(rejection);
-            }
-        };
-    }])
-    .config(['$httpProvider', function($httpProvider) {
-        $httpProvider.interceptors.push('errorHttpInterceptor');
-    }]);
+            };
+        }])
+        .config(['$httpProvider', function($httpProvider) {
+            $httpProvider.interceptors.push('errorHttpInterceptor');
+        }]);
+    }
 });
