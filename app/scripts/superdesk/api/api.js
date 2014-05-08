3530a521bc2c96ce42a3932a66e0ef2c5a2824f8
Petr Jasek <jasekpetr@gmail.com>
2014-05-08 10:34:46 +0200

    fix(api): register url resolver service

--- app/scripts/superdesk/api/api.js
+++ app/scripts/superdesk/api/api.js
@@ -3,13 +3,15 @@ define([
     'require',
     './api-service',
     './timeout-interceptor',
-    './request-service'
+    './request-service',
+    './url-resolver-service'
 ], function(angular, require) {
     'use strict';
 
     return angular.module('superdesk.data', [])
         .provider('api', require('./api-service'))
         .service('request', require('./request-service'))
+        .service('urlResolver', require('./url-resolver-service'))
         .config(['$httpProvider', function($httpProvider) {
             $httpProvider.interceptors.push(require('./timeout-interceptor'));
         }]);
