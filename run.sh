2f224a81532a438f91a916ac48da0a499200e8a0
petr.jasek <petr.jasek@sourcefabric.org>
2014-05-12 10:28:09 +0200

    fix(build): make run script stop on error

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # setup virtual env
 [ ! -f ./env/bin/activate ] && (
-    virtualenv -p python3.3 ./env
+    virtualenv -p python3 ./env
 )
 . ./env/bin/activate
+
+python --version
 
 # install app
 pip install -U pip distribute
