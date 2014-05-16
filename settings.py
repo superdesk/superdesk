b460d4d615820827b94c20b9f40241c9014d0529
petr.jasek <petr.jasek@sourcefabric.org>
2014-05-16 09:08:20 +0200

    fix(heroku): remove SERVER_NAME setting

--- settings.py
+++ settings.py
@@ -13,7 +13,6 @@ X_HEADERS = ['Content-Type', 'Authorization', 'If-Match']
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'superdesk')
 if os.environ.get('MONGOLAB_URI'):
     MONGO_URI = os.environ.get('MONGOLAB_URI')
-    SERVER_NAME = 'superdesk-api.herokuapp.com'
 
 ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL', 'http://localhost:9200')
 ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX', 'superdesk')
