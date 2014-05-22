0fe147f0d5ddb4f845df062cbf965a7c645978fd
Nikola Nikolic <nikolic_nikola@live.com>
2014-05-22 15:04:42 +0200

    fix(styles): move user styles to module folder

--- tasks/options/less.js
+++ tasks/options/less.js
@@ -1,22 +1,27 @@
 module.exports = {
     dev: {
         options: {
-            paths: ['<%= appDir %>/styles/less'],
+            paths: [
+                '<%= appDir %>/styles/less',
+                '<%= appDir %>/scripts/superdesk-users/styles'
+                ],
             compress: false,
             cleancss: true
         },
         files: {
-            '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less'
+            '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less',
+            '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/styles.less',
         }
     },
     prod: {
         options: {
-            paths: ['<%= appDir %>/styles/less'],
+            paths: ['<%= appDir %>/styles/less','<%= appDir %>/scripts/superdesk-users/styles'],
             compress: true,
             cleancss: true
         },
         files: {
-            '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less'
+            '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less',
+            '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/styles.less',
         }
     }
 };
