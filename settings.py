b8424e4ccd566908180c230894b8947ef9d77579
petr.jasek <petr.jasek@sourcefabric.org>
2014-05-23 09:45:50 +0200

    fix(url): define server name to return full urls
    It's using `SUPERDESK_URL` environment variable which is parsed
    and used for `URL_PROTOCOL`, `SERVER_NAME` and `URL_PREFIX`.

--- settings.py
+++ settings.py
@@ -1,15 +1,19 @@
 
 import os
-
-IF_MATCH = False
-
-URL_PREFIX = os.environ.get('SUPERDESK_URL_PREFIX', '')
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%S+0000'
-BANDWIDTH_SAVER = False
-
-CACHE_CONTROL = 'max-age=0, no-cache'
+from urllib.parse import urlparse
 
 XML = False
+IF_MATCH = False
+BANDWIDTH_SAVER = False
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S+0000'
+
+
+server_url = urlparse(os.environ.get('SUPERDESK_URL', 'http://localhost:5000/api'))
+URL_PROTOCOL = server_url.scheme or None
+SERVER_NAME = server_url.netloc or None
+URL_PREFIX = server_url.path.lstrip('/') or ''
+
+CACHE_CONTROL = 'max-age=0, no-cache'
 
 X_DOMAINS = '*'
 X_HEADERS = ['Content-Type', 'Authorization', 'If-Match']
@@ -37,6 +41,13 @@ INSTALLED_APPS = (
 RESOURCE_METHODS = ['GET', 'POST']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 EXTENDED_MEDIA_INFO = ['content_type', 'name', 'length']
+
+RENDITIONS = {
+    'picture': {
+        'thumbnail': {'width': 150, 'height': 150},
+        'view': {'width': 640, 'height': 640}
+    }
+}
 
 # uncomment to use local file storage
 # DEFAULT_FILE_STORAGE = 'superdesk.storage.FileSystemStorage'
