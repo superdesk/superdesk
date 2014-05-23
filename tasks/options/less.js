ab82732710fcebd1e19ef2a2b025b2cec01a8e2d
Nikola Nikolic <nikolic_nikola@live.com>
2014-05-23 11:19:14 +0200

    fix(styles): move settings page styles and uploader styles to related modules

--- tasks/options/less.js
+++ tasks/options/less.js
@@ -10,7 +10,9 @@ module.exports = {
         files: {
             '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less',
             '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/users.less',
-            '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less'
+            '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less',
+            '<%= distDir %>/styles/css/settings.css': '<%= appDir %>/scripts/superdesk-settings/styles/settings.less',
+            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less'
         }
     },
     prod: {
@@ -22,7 +24,9 @@ module.exports = {
         files: {
             '<%= distDir %>/styles/css/bootstrap.css': '<%= appDir %>/styles/less/bootstrap.less',
             '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/users.less',
-            '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less'
+            '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less',
+            '<%= distDir %>/styles/css/settings.css': '<%= appDir %>/scripts/superdesk-settings/styles/settings.less',
+            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less'
         }
     }
 };
