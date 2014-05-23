bb6ae505c84bde318de84f1241ca04b9c2c38209
petr.jasek <petr.jasek@sourcefabric.org>
2014-05-23 17:13:53 +0200

    fix(setup): add setup.py with initial config

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open('README.md').read()
+
+setup(
+    name='Superdesk-Server',
+    version='0.1-dev',
+    description='Superdesk REST API server',
+    long_description=LONG_DESCRIPTION,
+    author='petr jasek',
+    author_email='petr.jasek@sourcefabric.org',
+    url='https://github.com/superdesk/superdes-server',
+    license='GPLv3',
+    platforms=['any'],
+    packages=find_packages(),
+    install_requires=[
+        'Eve==0.4-dev',
+        'Eve-Elastic==0.1.6',
+        'Eve-Docs==0.1.2',
+        'Flask-Script==2.0.3',
+        'Pillow==2.4.0',
+    ],
+    scripts=['settings.py', 'app.py', 'wsgi.py', 'manage.py', 'docs.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
