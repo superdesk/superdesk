af04d89b50da2eb4a09e4f78a126ebbc2c2ba285
Nikola Nikolic <nikolic_nikola@live.com>
2014-05-23 12:14:16 +0200

    fix(styles): move media-archive and desks styles to related modules

--- tasks/options/less.js
+++ tasks/options/less.js
@@ -12,7 +12,9 @@ module.exports = {
             '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/users.less',
             '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less',
             '<%= distDir %>/styles/css/settings.css': '<%= appDir %>/scripts/superdesk-settings/styles/settings.less',
-            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less'
+            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less',
+            '<%= distDir %>/styles/css/media-archive.css': '<%= appDir %>/scripts/superdesk-items-common/styles/media-archive.less',
+            '<%= distDir %>/styles/css/desks.css': '<%= appDir %>/scripts/superdesk-desks/styles/desks.less'
         }
     },
     prod: {
@@ -26,7 +28,9 @@ module.exports = {
             '<%= distDir %>/styles/css/users.css': '<%= appDir %>/scripts/superdesk-users/styles/users.less',
             '<%= distDir %>/styles/css/dashboard.css': '<%= appDir %>/scripts/superdesk-dashboard/styles/dashboard.less',
             '<%= distDir %>/styles/css/settings.css': '<%= appDir %>/scripts/superdesk-settings/styles/settings.less',
-            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less'
+            '<%= distDir %>/styles/css/uploader.css': '<%= appDir %>/scripts/superdesk/upload/styles/uploader.less',
+            '<%= distDir %>/styles/css/media-archive.css': '<%= appDir %>/scripts/superdesk-items-common/styles/media-archive.less',
+            '<%= distDir %>/styles/css/desks.css': '<%= appDir %>/scripts/superdesk-desks/styles/desks.less'
         }
     }
 };
