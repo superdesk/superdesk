a14a733df6d1f63d42dfab3e0039b9127a342e87
eozan <erdemozanozbek@gmail.com>
2014-05-30 16:43:36 +0200

    fix(archive): archive upload refactoring done, dev version.

--- app/scripts/superdesk-archive/controllers/upload.js
+++ app/scripts/superdesk-archive/controllers/upload.js
@@ -4,42 +4,56 @@ define(['lodash'], function(_) {
     UploadController.$inject = ['$scope', '$q', 'upload', 'api'];
     function UploadController($scope, $q, upload, api) {
 
-        var promises = [];
         $scope.items = [];
+        $scope.saving = false;
+        $scope.failed = false;
 
-        var startUpload = function(item) {
-            return api.archiveMedia.getUrl().then(function(url) {
-                item.upload = upload.start({
-                    method: 'POST',
-                    url: url,
-                    data: {media: item.file},
-                    headers: api.archiveMedia.getHeaders()
-                }).then(function(response) {
-                    item.model = response.data;
-                    return item;
-                }, null, function(progress) {
-                    item.progress = Math.round(progress.loaded / progress.total * 100.0);
+        var uploadFile = function(item) {
+            return api.archiveMedia.getUrl()
+                .then(function(url) {
+                    var x;
+                    if (Math.random() > 0.50) {
+                        x = upload;
+                    } else {
+                        x = {
+                            start: function() {
+                                var p = $q.defer();
+                                p.reject();
+                                return p.promise;
+                            },
+                            abort: function() {}
+                        };
+                    }
+                    item.upload = x.start({
+                    //item.upload = upload.start({
+                        method: 'POST',
+                        url: url,
+                        data: {media: item.file},
+                        headers: api.archiveMedia.getHeaders()
+                    })
+                    .then(function(response) {
+                        item.model = response.data;
+                        return item;
+                    }, function(response) {
+                        item.model = false;
+                        return $q.reject();
+                    }, function(progress) {
+                        item.progress = Math.round(progress.loaded / progress.total * 100.0);
+                    })
+                    .finally(function() {
+                        checkFail();
+                    });
+                    return item.upload;
                 });
-
-                return item.upload;
-            });
         };
 
-        /**
-         * Add files
-         *
-         * @param {Collection} files
-         */
-        $scope.addFiles = function(files) {
-            _.each(files, function(file) {
-                var item = {
-                    file: file,
-                    meta: {},
-                    progress: 0
-                };
-
-                promises.push(startUpload(item));
-                $scope.items.unshift(item);
+        var checkFail = function() {
+            $scope.failed = false;
+            _.each($scope.items, function(item) {
+                if (item.model === false) {
+                    $scope.failed = true;
+                    return false;
+                }
             });
         };
 
@@ -49,44 +63,64 @@ define(['lodash'], function(_) {
             });
         };
 
+        $scope.addFiles = function(files) {
+            _.each(files, function(file) {
+                var item = {
+                    file: file,
+                    meta: {},
+                    progress: 0
+                };
+                $scope.items.unshift(item);
+            });
+            $scope.upload();
+        };
+
+        $scope.upload = function() {
+            var promises = [];
+            _.each($scope.items, function(item) {
+                if (!item.model) {
+                    promises.push(uploadFile(item));
+                }
+            });
+            if (promises.length) {
+                return $q.all(promises);
+            }
+            return $q.resolve();
+        };
+
+        $scope.save = function() {
+            $scope.saving = true;
+            $scope.upload().then(function(results) {
+                $q.all(_.map($scope.items, function(item) {
+                    return api.archive.update(item.model, item.meta);
+                })).then(function(results) {
+                    $scope.resolve(results);
+                });
+            })
+            .finally(function() {
+                $scope.saving = false;
+                checkFail();
+            });
+        };
+
         $scope.cancel = function() {
-            _.each($scope.items, cancelItem);
+            _.each($scope.items, function(item) {
+                $scope.cancelItem(item);
+            });
             $scope.reject();
         };
 
-        $scope.cancelOne = function(item, index) {
-            cancelItem(item);
-            $scope.items.splice(index, 1);
-        };
-
-        /**
-         * Wait for uploads to finish and save meta
-         */
-        $scope.save = function() {
-            $scope.saving = true;
-            return $q.all(promises)
-                .then(function() {
-                    return $q.all(_.map($scope.items, function(item) {
-                        return api.archive.update(item.model, item.meta);
-                    })).then(function(results) {
-                        $scope.resolve(results);
-                        return results;
-                    });
-                });
-        };
-
-        /**
-         * Cancel uploading of single item
-         *
-         * @param {Object} item
-         */
-        function cancelItem(item) {
+        $scope.cancelItem = function(item, index) {
             if (item.model) {
                 api.archive.remove(item.model);
             } else if (item.upload) {
-                item.upload.abort();
+                //item.upload.abort();
             }
-        }
+            if (index !== undefined) {
+                $scope.items.splice(index, 1);
+            }
+            checkFail();
+        };
     }
 
     return UploadController;
