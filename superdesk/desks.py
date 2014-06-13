e239c93156e6529de62bb8b2dd94ddd595b914ef
mugur.rus <mugur.rus@sourcefabric.org>
2014-06-13 10:31:13 +0300

    SD-395 : adding desks for given users ...

--- superdesk/desks.py
+++ superdesk/desks.py
@@ -1,18 +1,40 @@
-
 import superdesk
 
-superdesk.domain('desks', {
-    'schema': {
-        'name': {
-            'type': 'string',
-            'unique': True,
-            'required': True,
-        },
-        'members': {
-            'type': 'list'
-        }
+
+desks_schema = {
+    'name': {
+        'type': 'string',
+        'unique': True,
+        'required': True,
     },
+    'members': {
+        'type': 'list',
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'user': {
+                    'type': 'objectid',
+                    'data_relation': {'resource': 'users', 'field': '_id', 'embeddable': True}
+                }
+            }
+        }
+    }
+}
+
+
+superdesk.domain('desks', {
+    'schema': desks_schema,
     'datasource': {
         'default_sort': [('created', -1)]
     }
+})
+
+
+superdesk.domain('user_desks', {
+    'url': 'users/<regex("[a-f0-9]{24}"):user>/desks',
+    'schema': desks_schema,
+    'datasource': {
+        'source': 'desks'
+    },
+    'resource_methods': ['GET'],
 })
