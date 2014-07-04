5ef3df3ae93c158911b3d70c816d556e9e06d415
Nikola Nikolic <nikolic_nikola@live.com>
2014-07-04 08:14:16 +0200

    Move mock data in a module

--- app/scripts/superdesk-planning/controllers/main.js
+++ app/scripts/superdesk-planning/controllers/main.js
@@ -1,8 +1,8 @@
 define(['lodash'], function(_) {
     'use strict';
 
-    PlanningDashboardController.$inject = ['$scope'];
-    function PlanningDashboardController($scope) {
+    PlanningDashboardController.$inject = ['$scope', 'mockItems'];
+    function PlanningDashboardController($scope, mockItems) {
 
     	$scope.newItem = {
             headline: null
@@ -18,27 +18,7 @@ define(['lodash'], function(_) {
     		$scope.selectedItem = item;
     	};
 
-    	$scope.items = [
-    		{
-    			headline: 'Et harum quidem rerum facilis est et expedita distinctio',
-    			description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam' +
-    			'nisi ut aliquid ex ea commodi consequatu',
-    			duedate: 'Today at 15:30',
-    			tasks: 'Story,Photo',
-    			comments: 2,
-    			attachments: 3,
-    			links: 2
-    		},
-    		{
-    			headline: ' Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates',
-    			description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci' +
-    			'velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
-    			duedate: 'Today at 16:30',
-    			tasks: 'Story',
-    			comments: 4,
-    			attachments: 1
-    		}
-    	];
+    	$scope.items = mockItems.list;
     }
 
     return PlanningDashboardController;
--- app/scripts/superdesk-planning/module.js
+++ app/scripts/superdesk-planning/module.js
@@ -11,9 +11,28 @@ define([
     ]);
 
     return app
-        .value('mockDataExample', {
-            list: ['array1', 'array2', 'array3'],
-            name: 'sample name'
+        .value('mockItems', {
+            list: [
+	    		{
+	    			headline: 'Et harum quidem rerum facilis est et expedita distinctio',
+	    			description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam' +
+	    			'nisi ut aliquid ex ea commodi consequatu',
+	    			duedate: 'Today at 15:30',
+	    			tasks: 'Story,Photo',
+	    			comments: 2,
+	    			attachments: 3,
+	    			links: 2
+	    		},
+	    		{
+	    			headline: ' Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates',
+	    			description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci' +
+	    			'velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+	    			duedate: 'Today at 16:30',
+	    			tasks: 'Story',
+	    			comments: 4,
+	    			attachments: 1
+	    		}
+	    	]
         })
         .config(['superdeskProvider', function(superdesk) {
             superdesk
