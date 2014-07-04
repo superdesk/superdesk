a3437cfd2e5ad12c3d1030690c3833e3d1126480
Nikola Nikolic <nikolic_nikola@live.com>
2014-07-04 10:21:14 +0200

    Enable editing of planning item
    + move item preview to separate directive
    + make the item in editable

--- app/scripts/superdesk-planning/module.js
+++ app/scripts/superdesk-planning/module.js
@@ -1,6 +1,6 @@
 define([
     'angular'
-], function(angular, require) {
+], function(angular) {
     'use strict';
 
     PlanningDashboardController.$inject = ['$scope', 'mockItems'];
@@ -9,7 +9,7 @@ define([
     	$scope.newItem = {
             headline: null
         };
-    	$scope.selectedItem = null;
+    	$scope.selected = {item: null};
 
     	$scope.addItem = function() {
     		$scope.items.unshift(_.clone($scope.newItem));
@@ -17,21 +17,54 @@ define([
     	};
 
     	$scope.preview = function(item) {
-    		$scope.selectedItem = item;
+    		$scope.selected.item = item;
     	};
 
     	$scope.items = mockItems.list;
     }
 
+    function SdPreviewItem() {
+    	return {
+    		templateUrl: 'scripts/superdesk-planning/views/item-preview.html',
+    		scope: {
+    			origItem: '=item'
+    		},
+    		link: function(scope, elem) {
+
+    			scope.$watch('origItem', resetItem);
+
+    			scope.dirty = false;
+
+    			scope.$watchCollection('item', function(item) {
+                    scope.dirty = !angular.equals(item, scope.origItem);
+                });
+
+                scope.cancel = function() {
+                    resetItem(scope.origItem);
+                };
+
+                scope.save = function() {
+                	console.log('save changes');
+                };
+
+    			function resetItem(item) {
+    				scope.item = _.create(item);
+    			}
+    		}
+    	};
+    }
+
     var app = angular.module('superdesk.planning', []);
 
     return app
+    	.directive('sdPreviewItem', SdPreviewItem)
         .value('mockItems', {
             list: [
 	    		{
 	    			headline: 'Et harum quidem rerum facilis est et expedita distinctio',
 	    			description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam' +
 	    			'nisi ut aliquid ex ea commodi consequatu',
+	    			createdon: '07:55:44, 26. June 2014',
 	    			duedate: 'Today at 15:30',
 	    			tasks: 'Story,Photo',
 	    			comments: 2,
@@ -42,6 +75,7 @@ define([
 	    			headline: ' Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates',
 	    			description: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci' +
 	    			'velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+	    			createdon: '08:13:20, 26. June 2014',
 	    			duedate: 'Today at 16:30',
 	    			tasks: 'Story',
 	    			comments: 4,
