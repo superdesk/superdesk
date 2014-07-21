897d083d17a0bbfdd34d82e4d65eeb9079710df3
chupym <gabriel.nistor@sourcefabric.org>
2014-07-21 11:15:05 +0300

    SD-366 : New activities in the Activity stream
    Finalized auto reload.

--- superdesk/activity.py
+++ superdesk/activity.py
@@ -1,7 +1,10 @@
 from .base_model import BaseModel
 import flask
 from eve.methods.post import post_intern
+from superdesk.notification import push_notification
+import logging
 
+log = logging.getLogger(__name__)
 # 
 # class ActivityLogHandler(logging.Handler):
 #     """Logging handler storing data into mongodb."""
@@ -22,7 +25,9 @@ from eve.methods.post import post_intern
     
 def init_app(app):
     activityModel = ActivityModel(app=app)
-    app.on_insert += activityModel.on_generic_insert
+    app.on_insert += activityModel.on_generic_inserted
+    app.on_update += activityModel.on_generic_updated
+    app.on_delete_item += activityModel.on_generic_deleted
 
 
 class ActivityModel(BaseModel):
@@ -43,8 +48,8 @@ class ActivityModel(BaseModel):
         }
     }
     exclude = {endpoint_name, 'notification'}
-
-    def on_generic_insert(self, resource, docs):
+    
+    def on_generic_inserted(self, resource, docs):
         if resource in self.exclude: return
 
         user = getattr(flask.g, 'user', None)
@@ -53,7 +58,41 @@ class ActivityModel(BaseModel):
         activity = {
             'user': user.get('_id'),
             'resource': resource,
-            'action': 'create',
+            'action': 'created',
             'extra': docs[0]
         }
         post_intern(self.endpoint_name, activity)
+        push_notification(self.endpoint_name, created=1, keys=(user.get('_id'),))
+        
+    def on_generic_updated(self, resource, doc, original):
+        if resource in self.exclude: return
+
+        user = getattr(flask.g, 'user', None)
+        if not user: return
+
+        activity = {
+            'user': user.get('_id'),
+            'resource': resource,
+            'action': 'updated',
+            'extra': doc
+        }
+        post_intern(self.endpoint_name, activity)
+    
+        push_notification(self.endpoint_name, updated=1, keys=(str(user.get('_id')),))
+            
+        
+    def on_generic_deleted(self, resource, doc):
+        if resource in self.exclude: return
+
+        user = getattr(flask.g, 'user', None)
+        if not user: return
+
+        activity = {
+            'user': user.get('_id'),
+            'resource': resource,
+            'action': 'deleted',
+            'extra': doc
+        }
+        post_intern(self.endpoint_name, activity)
+        push_notification(self.endpoint_name, deleted=1, keys=(user.get('_id'),))
+
