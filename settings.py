f8f51cf8716cff16442218a6b85f07837b33ef1e
petr.jasek <petr.jasek@sourcefabric.org>
2014-08-05 15:58:09 +0200

    fix(celery): avoid celery deprecation warning

--- settings.py
+++ settings.py
@@ -34,6 +34,8 @@ ELASTICSEARCH_INDEX = os.environ.get('ELASTICSEARCH_INDEX', 'superdesk')
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 CELERY_ALWAYS_EAGER = (os.environ.get('CELERY_ALWAYS_EAGER', False) == 'True')
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']  # it's using pickle when in eager mode
 
 CELERYBEAT_SCHEDULE = {
     'fetch_ingest': {
