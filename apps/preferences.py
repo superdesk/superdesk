87e5d15d19541fc4241c2b1e29aa47e380b3ea36
amagdas <magdasadrian@gmail.com>
2014-09-14 23:26:36 +0300

    spike(services) - update preferences to use BaseService class

--- apps/preferences.py
+++ apps/preferences.py
@@ -1,4 +1,6 @@
 from superdesk.models import BaseModel
+from superdesk.services import BaseService
+from superdesk import get_backend
 
 
 preference_schema = {
@@ -7,11 +9,12 @@ preference_schema = {
 
 
 def init_app(app):
-    PreferencesModel(app=app)
+    endpoint_name = 'preferences'
+    service = BaseService(endpoint_name=endpoint_name, backend=get_backend())
+    PreferencesModel(app=app, endpoint_name=endpoint_name, service=service)
 
 
 class PreferencesModel(BaseModel):
-    endpoint_name = 'preferences'
     datasource = {'source': 'users', 'projection': {'preferences': 1}}
     schema = preference_schema
     resource_methods = ['GET']
