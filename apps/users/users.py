0fcc96277a729b1e9c685a727fc9036e6a270d83
Siva Kuna <kunas@aap.com.au>
2014-09-16 15:39:44 +1000

    [SD-715]: Created a new command to import profile from AD. Still need to add the code to parse Exceptions.

--- apps/users/users.py
+++ apps/users/users.py
@@ -2,8 +2,11 @@
 
 import bcrypt
 from flask import current_app as app
+from flask.ext.script.commands import InvalidCommand
+from apps.auth.ldap_auth import authenticate_and_fetch_profile
 
 import superdesk
+from superdesk import isLDAP
 from superdesk.models import BaseModel
 from superdesk.utc import utcnow
 from apps.activity import add_activity
@@ -58,6 +61,10 @@ class CreateUserCommand(superdesk.Command):
     )
 
     def run(self, username, password, email):
+        if isLDAP():
+            raise InvalidCommand(""" Can't create a Profile with a password while AD is enabled.
+                                    Consider using 'users:copyfromad' command instead.""")
+
         userdata = {
             'username': username,
             'password': password,
@@ -90,14 +97,57 @@ class HashUserPasswordsCommand(superdesk.Command):
                 superdesk.apps['users'].update(id=user_id, updates=updates, trigger_events=False)
 
 
+class ImportUserProfileFromADCommand(superdesk.Command):
+    """
+    Responsible for importing a user profile from Active Directory (AD) to Mongo.
+    This command runs on assumption that the user executing this command and
+    the user whose profile need to be imported need not to be the same. Uses ad_username and ad_password to bind to AD
+    and then searches for a user identified by username_to_import and if found imports into Mongo.
+    """
+
+    option_list = (
+        superdesk.Option('--ad_username', '-adu', dest='ad_username', required=True),
+        superdesk.Option('--ad_password', '-adp', dest='ad_password', required=True),
+        superdesk.Option('--username_to_import', '-u', dest='username', required=True),
+    )
+
+    def run(self, ad_username, ad_password, username):
+        """
+        Imports or Updates a User Profile from AD to Mongo.
+        :param ad_username: Active Directory Username
+        :param ad_password: Password of Active Directory Username
+        :param username: Username as in Active Directory whose profile needs to be imported to Superdesk.
+        :return: User Profile.
+        """
+
+        if not isLDAP():
+            raise InvalidCommand("Authentication using AD isn't enabled. Consider using 'users:create' command instead")
+
+        #Authenticate and fetch profile from AD
+        user_data = authenticate_and_fetch_profile(ad_username, ad_password, username)
+
+        #Check if User Profile already exists in Mongo
+        user = superdesk.app.data.find_one('users', username=username, req=None)
+
+        if user:
+            user_data[app.config['LAST_UPDATED']] = utcnow()
+            superdesk.apps['users'].update(user.get('_id'), user_data, trigger_events=False)
+            return user_data
+        else:
+            user_data[app.config['DATE_CREATED']] = utcnow()
+            user_data[app.config['LAST_UPDATED']] = utcnow()
+            superdesk.apps['users'].create([user_data], trigger_events=True)
+            return user_data
+
+
 superdesk.connect('read:users', on_read_users)
 superdesk.connect('created:users', on_read_users)
 superdesk.command('users:create', CreateUserCommand())
 superdesk.command('users:hash_passwords', HashUserPasswordsCommand())
+superdesk.command('users:copyfromad', ImportUserProfileFromADCommand())
 
 
 class RolesModel(BaseModel):
-
     endpoint_name = 'roles'
     schema = {
         'name': {
