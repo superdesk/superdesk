8bec7b06ed2a9c008b6496cfa4c1b06e0100072a
Siva Kuna <kunas@aap.com.au>
2014-09-19 14:15:25 +1000

    [SD-714] [SD-718]: Disabling the ResetPassword endpoint if Authentication via AD is enabled

--- apps/users/__init__.py
+++ apps/users/__init__.py
@@ -1,9 +1,13 @@
 from .reset_password import ResetPasswordModel, ActiveTokensModel  # NOQA
+import superdesk
 from .users import RolesModel, UsersModel, CreateUserCommand  # noqa
 
 
 def init_app(app):
     UsersModel(app=app)
     RolesModel(app=app)
-    ResetPasswordModel(app=app)
+
+    if not superdesk.isLDAP():
+        ResetPasswordModel(app=app)
+
     ActiveTokensModel(app=app)
