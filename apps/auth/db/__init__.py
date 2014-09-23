e1f0e5e06ad69bfdf0610b020adf9a8812235228
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2014-09-23 17:03:34 +1000

    [SD-716] Units for create user for ldap authentication.

--- apps/auth/db/__init__.py
+++ apps/auth/db/__init__.py
@@ -2,7 +2,7 @@ from apps.auth import AuthResource
 from .reset_password import ResetPasswordService, ResetPasswordResource
 import superdesk
 from .db import DbAuthService, authenticate  # noqa
-
+from .commands import CreateUserCommand, HashUserPasswordsCommand
 
 def init_app(app):
     endpoint_name = 'auth'
--- apps/auth/ldap/__init__.py
+++ apps/auth/ldap/__init__.py
@@ -1,7 +1,7 @@
 import superdesk
 from apps.auth import AuthResource
 from .ldap import LdapAuthService, authenticate, ADAuth  # noqa
-
+from .commands import ImportUserProfileFromADCommand
 
 def init_app(app):
     endpoint_name = 'auth'
