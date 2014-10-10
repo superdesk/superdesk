cc9beeaa4f6e462e27c2f720346b2124e0dd30fa
Nikola Nikolic <nikolic_nikola@live.com>
2014-10-10 09:14:45 +0200

    fix(tasks): Fix styling in task preview

--- app/scripts/superdesk-dashboard/module.js
+++ app/scripts/superdesk-dashboard/module.js
@@ -14,7 +14,10 @@ define([
     function TaskPreviewDirective() {
         return {
             templateUrl: 'scripts/superdesk-dashboard/views/task-preview.html',
-            scope: {item: '='},
+            scope: {
+                item: '=',
+                users: '='
+            },
             link: function(scope, element, attrs) {}
         };
     }
