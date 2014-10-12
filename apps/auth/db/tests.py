9d0c35b867de01026530a6c5f4935d2ce4c28ac8
Siva Kuna <kunas@aap.com.au>
2014-10-13 09:57:01 +1100

    [SD-828] Fixing the nosetests and behave tests when AD is active. Because of role changes these tests were failing.

--- apps/auth/db/tests.py
+++ apps/auth/db/tests.py
@@ -1,3 +1,4 @@
+from settings import LDAP_SERVER
 from superdesk.tests import TestCase
 from superdesk import get_resource_service
 from .commands import CreateUserCommand
@@ -6,14 +7,15 @@ from .commands import CreateUserCommand
 class UsersTestCase(TestCase):
 
     def test_create_user_command(self):
-        user = {'username': 'foo', 'password': 'bar', 'email': 'baz'}
-        cmd = CreateUserCommand()
-        with self.app.test_request_context():
-            cmd.run(user['username'], user['password'], user['email'])
-            auth_user = get_resource_service('auth').authenticate(user)
-            self.assertEquals(auth_user['username'], user['username'])
+        if not LDAP_SERVER:
+            user = {'username': 'foo', 'password': 'bar', 'email': 'baz'}
+            cmd = CreateUserCommand()
+            with self.app.test_request_context():
+                cmd.run(user['username'], user['password'], user['email'])
+                auth_user = get_resource_service('auth').authenticate(user)
+                self.assertEquals(auth_user['username'], user['username'])
 
-            cmd.run(user['username'], user['password'], user['email'])
-            auth_user2 = get_resource_service('auth').authenticate(user)
-            self.assertEquals(auth_user2['username'], user['username'])
-            self.assertEquals(auth_user2['_id'], auth_user['_id'])
+                cmd.run(user['username'], user['password'], user['email'])
+                auth_user2 = get_resource_service('auth').authenticate(user)
+                self.assertEquals(auth_user2['username'], user['username'])
+                self.assertEquals(auth_user2['_id'], auth_user['_id'])
