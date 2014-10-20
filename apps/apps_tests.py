eaeb519cb289ee0afd8853f20920249aab20af7e
akintolga <akint@aap.com.au>
2014-10-20 15:57:05 +1100

    [SD-852] preferences unit tests

--- apps/apps_tests.py
+++ apps/apps_tests.py
@@ -1,9 +1,9 @@
 from superdesk.tests import TestCase
 from apps.preferences import PreferencesService
 
-class Preference_Tests(TestCase):
 
-    _default_user_settings =  {
+class Preference_Tests(TestCase):
+    _default_user_settings = {
         "archive:view": {
             "default": "mgrid",
             "label": "Users archive view format",
@@ -31,12 +31,11 @@ class Preference_Tests(TestCase):
         }
     }
 
-    _default_session_settings =  {
+    _default_session_settings = {
         "desk:items": [],
         "pinned:items": [],
         "scratchpad:items": []
     }
-
 
     _session_update = {
         "session_preferences": {
@@ -44,13 +43,13 @@ class Preference_Tests(TestCase):
         }
     }
 
-    _user_update = { "user_preferences": {
+    _user_update = {
+        "user_preferences": {
             "archive:view": {
                 "label": "Testing user preferences"
             }
         }
     }
-
 
     def test_setting_partial_session_preferences_with_empty_existing(self):
         update = self._session_update
@@ -58,14 +57,15 @@ class Preference_Tests(TestCase):
         self.assertListEqual(update["session_preferences"]["scratchpad:items"], [123])
 
     def test_setting_partial_session_preferences_with_existing(self):
-        existing_session_settings =  {
-        "desk:items": [],
-        "pinned:items": ['a', 'b', 'c'],
-        "scratchpad:items": []
+        existing_session_settings = {
+            "desk:items": [],
+            "pinned:items": ['a', 'b', 'c'],
+            "scratchpad:items": []
         }
 
         update = self._session_update
-        PreferencesService.partial_update(self, update, existing_session_settings, self._default_session_settings, "session_preferences")
+        PreferencesService.partial_update(self, update, existing_session_settings,
+                                          self._default_session_settings, "session_preferences")
         self.assertListEqual(update["session_preferences"]["scratchpad:items"], [123])
 
     def test_setting_partial_user_preferences_with_existing(self):
@@ -75,7 +75,7 @@ class Preference_Tests(TestCase):
 
     def test_setting_partial_user_preferences_with_empty_existing(self):
         update = self._user_update
-        existing_user_settings =  {
+        existing_user_settings = {
             "archive:view": {
                 "default": "mgrid",
                 "label": "Users archive view format",
@@ -89,5 +89,6 @@ class Preference_Tests(TestCase):
             }
         }
 
-        PreferencesService.partial_update(self, update, existing_user_settings, self._default_user_settings, "user_preferences")
+        PreferencesService.partial_update(self, update, existing_user_settings,
+                                          self._default_user_settings, "user_preferences")
         self.assertEqual(update["user_preferences"]["archive:view"]["label"], "Testing user preferences")
