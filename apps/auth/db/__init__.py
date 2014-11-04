16d5200008f03a812ab0622f05ec75a16cc4635d
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2014-11-04 09:26:24 +0200

    SD-919 - The logged user is not able to change his password

--- apps/auth/db/__init__.py
+++ apps/auth/db/__init__.py
@@ -4,6 +4,8 @@ import superdesk
 from .db import DbAuthService
 from .commands import CreateUserCommand, HashUserPasswordsCommand  # noqa
 from superdesk.services import BaseService
+from apps.auth.db.change_password import ChangePasswordService,\
+    ChangePasswordResource
 
 
 def init_app(app):
@@ -14,6 +16,10 @@ def init_app(app):
     endpoint_name = 'reset_user_password'
     service = ResetPasswordService(endpoint_name, backend=superdesk.get_backend())
     ResetPasswordResource(endpoint_name, app=app, service=service)
+
+    endpoint_name = 'change_user_password'
+    service = ChangePasswordService(endpoint_name, backend=superdesk.get_backend())
+    ChangePasswordResource(endpoint_name, app=app, service=service)
 
     endpoint_name = 'active_tokens'
     service = BaseService(endpoint_name, backend=superdesk.get_backend())
---
+++ apps/auth/db/change_password.py
@@ -0,0 +1,46 @@
+import logging
+import superdesk
+from superdesk.resource import Resource
+from superdesk.services import BaseService
+from superdesk import get_resource_service
+
+
+logger = logging.getLogger(__name__)
+
+
+class ChangePasswordResource(Resource):
+    schema = {
+        'username': {
+            'type': 'string',
+            'required': True
+        },
+        'old_password': {
+            'type': 'string',
+            'required': True
+        },
+        'new_password': {
+            'type': 'string',
+            'required': True
+        },
+    }
+    public_methods = ['POST']
+    resource_methods = ['POST']
+    item_methods = []
+
+
+class ChangePasswordService(BaseService):
+
+    def create(self, docs, **kwargs):
+        for doc in docs:
+            username = doc['username']
+            try:
+                get_resource_service('auth').authenticate({'username': username, 'password': doc['old_password']})
+            except Exception:
+                payload = 'The provided old password is not correct.'
+                raise superdesk.SuperdeskError(payload=payload)
+
+            user = superdesk.get_resource_service('users').find_one(req=None, username=username)
+            superdesk.get_resource_service('users').update_password(user['_id'], doc['new_password'])
+            del doc['old_password']
+            del doc['new_password']
+            return [user['_id']]
