f218890a73dd318b994e6ee54e0d87247d9da6e2
akintolga <akint@aap.com.au>
2014-11-25 17:23:40 +1100

    [SD-1122] Unit tests are added for the privileges

--- apps/users/users_test.py
+++ apps/users/users_test.py
@@ -10,5 +10,14 @@ class PrivilegesTestCase(TestCase):
         with self.app.app_context():
             service = UsersService('users', backend=superdesk.get_backend())
             user = {'user_type': 'administrator'}
-            service.set_privileges(user)
+            service.set_privileges(user, None)
             self.assertEqual(user['active_privileges']['users'], 1)
+
+    def test_user_has_merged_privileges(self):
+        with self.app.app_context():
+            service = UsersService('users', backend=superdesk.get_backend())
+            user = {'user_type': 'user', 'privileges': {'users': 1}}
+            role = {'privileges': {'archive': 1}}
+            service.set_privileges(user, role)
+            self.assertEqual(user['active_privileges']['users'], 1)
+            self.assertEqual(user['active_privileges']['archive'], 1)
