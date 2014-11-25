4990ff96a54d78ad48685bdd7635e3cb9f8b3f04
Nikola Nikolic <nikolic_nikola@live.com>
2014-11-25 18:49:17 +1100

    feat(packages): add modal

--- app/scripts/superdesk-authoring/packages/packages.js
+++ app/scripts/superdesk-authoring/packages/packages.js
@@ -7,8 +7,50 @@
 
     }
 
-    PackagesCtrl.$inject = ['$scope', 'packagesService'];
-    function PackagesCtrl($scope, packagesService) {
+    PackagesCtrl.$inject = ['$scope', 'packagesService', 'superdesk'];
+    function PackagesCtrl($scope, packagesService, superdesk) {
+
+        $scope.itemTypes = [
+            {
+                name: 'text',
+                label: 'Story'
+            },
+            {
+                name: 'picture',
+                label: 'Image'
+            },
+            {
+                name: 'video',
+                label: 'Video'
+            },
+            {
+                name: 'audio',
+                label: 'Audio'
+            }
+        ];
+
+        $scope.create = function(type) {
+            superdesk.intent('create', 'package', {type: type}).then(function(val) {
+                //do the things in the sidebar when modal get closed
+            });
+        };
+    }
+
+    AddToPackageController.$inject = ['$scope', 'api'];
+    function AddToPackageController($scope, api) {
+
+        api.archive.query().then(function(result) {
+            $scope.items = result;
+        });
+
+        $scope.cancel = function() {
+            $scope.reject();
+        };
+
+        $scope.save = function() {
+            //do the saving
+            $scope.resolve();
+        };
 
     }
 
@@ -21,6 +63,18 @@
             template: 'scripts/superdesk-authoring/packages/views/packages-widget.html',
             side: 'right'
         });
+    }])
+
+    .config(['superdeskProvider', function(superdesk) {
+        superdesk
+            .activity('create.package', {
+                label: gettext('Create package'),
+                modal: true,
+                cssClass: 'create-package-modal responsive-popup',
+                controller: AddToPackageController,
+                templateUrl: 'scripts/superdesk-authoring/packages/views/create-package-modal.html',
+                filters: [{action: 'create', type: 'package'}]
+            });
     }])
 
     .config(['apiProvider', function(apiProvider) {
