9b2586a83c83be11db603582222cba986c37ae77
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2014-11-27 14:12:57 +0200

    SD-1166 : Refactor saving to legal archive in order to send just the id of original content item

--- apps/legal_archive/tasks.py
+++ apps/legal_archive/tasks.py
@@ -1,13 +1,13 @@
-from superdesk.celery_app import celery
 from apps.common.components.utils import get_component
 from apps.legal_archive.components.legal_archive import LegalArchive
+from superdesk import get_resource_service
+from superdesk.celery_app import celery
 
 
 @celery.task(bind=True, max_retries=3)
-def archive_item(self, items, task_id=None):
-    return get_component(LegalArchive).create(items)
-
-
-@celery.task(bind=True, max_retries=3)
-def update_item(self, original, updates, task_id=None):
-    return get_component(LegalArchive).update(original, updates)
+def update_legal_archive(self, ids):
+    for _id in ids:
+        archived_doc = get_resource_service('archive').find_one_in_base_backend(req=None, _id=_id)
+        if not archived_doc:
+            continue
+        return get_component(LegalArchive).create([archived_doc])
