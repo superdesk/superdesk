2331af2f5f37b0eea5b7c3f9c67f5d98ca88eaf0
mugur.rus <mugur.rus@sourcefabric.org>
2014-12-08 11:24:08 +0200

    filter activity by desk

--- app/scripts/superdesk-stream/stream.js
+++ app/scripts/superdesk-stream/stream.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
 	'use strict';
 
 	var app = angular.module('superdesk.stream', [
@@ -6,26 +6,34 @@
         'superdesk.asset'
     ]);
 
-
     app.controller('StreamController', ['$scope', 'api', '$rootScope', 'desks', function($scope, api, $rootScope, desks) {
         $scope.desk = null;
         $scope.activities = null;
+        $scope.pageLength = 10;
+        $scope.max_results = $scope.pageLength;
+
+        $scope.loadMore = function() {
+            if ($scope.activities._meta.total > $scope.max_results) {
+                $scope.max_results += $scope.pageLength;
+                fetchActivities();
+            }
+        };
 
         $scope.showDateHeader = function(activity) {
             $scope.currentDate = new Date($scope.activities._items[activity.index]._created);
-            if (activity.index == 0) {
+            if (activity.index === 0) {
                 return true;
-            };
-            var previousDate = new Date($scope.activities._items[activity.index-1]._created);
-            return previousDate.getYear() != $scope.currentDate.getYear()
-                || previousDate.getMonth() != $scope.currentDate.getMonth()
-                || previousDate.getDate() != $scope.currentDate.getDate();
+            }
+            var previousDate = new Date($scope.activities._items[activity.index - 1]._created);
+            return previousDate.getYear() !== $scope.currentDate.getYear() ||
+                   previousDate.getMonth() !== $scope.currentDate.getMonth() ||
+                   previousDate.getDate() !== $scope.currentDate.getDate();
         };
 
         var fetchActivities = function() {
-            var filter = {embedded: {user: 1}};
+            var filter = {embedded: {user: 1}, max_results: $scope.max_results};
             if ($scope.desk) {
-                filter.where = {desk: $scope.desk._id}
+                filter.where = {desk: $scope.desk._id};
             }
 
             api('activity').query(filter)
@@ -34,11 +42,11 @@
                     activity.display_message = activity.message;
                     for (var tag in activity.data) {
                         if (activity.data.hasOwnProperty(tag)) {
-                            var tagRegex = new RegExp('{{\\s*'+tag+'\\s*}}', 'gi');
+                            var tagRegex = new RegExp('{{\\s*' + tag + '\\s*}}', 'gi');
                             activity.display_message = activity.display_message.replace(tagRegex, activity.data[tag]);
                             activity.index = index;
                         }
-                    };
+                    }
                 });
                 $scope.activities = result;
             });
