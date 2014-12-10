4d42d1d3b8e8bd8f636fbb725abe287d16ea8a70
amagdas <magdasadrian@gmail.com>
2014-12-10 21:53:03 +0200

    fix 'Create package' button and style fixes

--- app/scripts/superdesk-authoring/packages/packages.js
+++ app/scripts/superdesk-authoring/packages/packages.js
@@ -4,7 +4,6 @@
 
     PackagesCtrl.$inject = ['$scope', 'superdesk'];
     function PackagesCtrl($scope, superdesk) {
-        $scope.selected.hide_menu = true;
         $scope.contenttab = true;
 
         $scope.create = function() {
