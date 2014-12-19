65d8540d35a7280b92e350caf21c55cbf44046d1
Siva Kuna <kunas@aap.com.au>
2014-12-19 17:05:24 +1100

    Moved DAYS_TO_KEEP to environment variables

--- superdesk/io/ingest_provider_model.py
+++ superdesk/io/ingest_provider_model.py
@@ -15,11 +15,10 @@ from superdesk.io import allowed_providers
 from superdesk.activity import ACTIVITY_CREATE, ACTIVITY_EVENT, \
     ACTIVITY_DELETE, ACTIVITY_UPDATE, notify_and_add_activity
 from superdesk import get_resource_service
+from settings import DAYS_TO_KEEP
 
 
-DAYS_TO_KEEP = 2
 logger = logging.getLogger(__name__)
-
 
 class IngestProviderResource(Resource):
     schema = {
