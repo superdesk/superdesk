eebf5a7d06980368dab1d9f47e8418fe990a1c78
Siva Kuna <kunas@aap.com.au>
2014-12-23 09:04:30 +1100

    Adding Copyright Information

--- apps/tasks.py
+++ apps/tasks.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+
 from eve.utils import ParsedRequest
 from eve.versioning import resolve_document_version
 from apps.archive.common import insert_into_versions, is_assigned_to_a_desk
