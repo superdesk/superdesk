314852b8ac774f4516d974aa622a9f39c4a2dd17
sjunaid <sjunaid@aap.com.au>
2015-01-07 16:34:32 +1100

    [SD-457] : Fix: Pagination should be defaulted each time new filter is applied

--- app/scripts/superdesk/list/list.js
+++ app/scripts/superdesk/list/list.js
@@ -101,7 +101,17 @@ define([
                     scope.total = 0;
                     if (meta) {
                         scope.total = meta.total;
-                        scope.page = $location.search().page || 1;
+                       if (scope.page !== 'undefined')
+                        {
+                            if (scope.page !== $location.search().page) {
+                               scope.page = $location.search().page || 1;
+                            } else {
+                                scope.page = 1;
+                                scope.setPage(0);
+                            }
+                        } else {
+                            scope.page = $location.search().page || 1;
+                        }
                         scope.limit = Number(localStorage.getItem('pagesize')) || Number($location.search().max_results) || size;
                         scope.lastPage = scope.limit ? Math.ceil(scope.total / scope.limit) : scope.page;
                         scope.from = (scope.page - 1) * scope.limit + 1;
