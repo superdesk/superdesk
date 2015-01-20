3bc167185e0f77388a77296d1295ae6cf67a299d
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2015-01-20 12:45:07 +1100

    [SD-1072] adding the invisible stages to the query filter.

--- apps/search/__init__.py
+++ apps/search/__init__.py
@@ -28,9 +28,12 @@ class SearchService(superdesk.Service):
     private_filters = [{
         'or': [
             {'exists': {'field': 'task.desk'}},
-            {'not': {'term': {'_type': 'archive'}}},
+            {'not': {'term': {'_type': 'archive'}}}
         ]
     }]
+
+    def get_private_filter(self):
+        pass
 
     def _get_query(self, req):
         """Get elastic query."""
@@ -51,6 +54,8 @@ class SearchService(superdesk.Service):
         query = self._get_query(req)
         types = self._get_types(req)
         query['aggs'] = aggregations
+        stages = ["54aa09831d41c835947a102f"]
+        self.private_filters.append({'and': [{'not': {'terms': {'task.stage': stages}}}]})
         # if the system has a setting value for the maximum search depth then apply the filter
         if not app.settings['MAX_SEARCH_DEPTH'] == -1:
             set_filters(query, self.private_filters + [{'limit': {'value': app.settings['MAX_SEARCH_DEPTH']}}])
