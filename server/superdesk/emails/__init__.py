efe1decba877145d67d6a1656cebc8b99353e5cd
petr.jasek <petr.jasek@sourcefabric.org>
2015-02-17 13:05:50 +0100

    fix(celery): make sure workers won't hang with emails
    without configured mail server celery workers get stuck with email
    sending tasks. this will make such tasks fail after 10s

--- server/superdesk/emails/__init__.py
+++ server/superdesk/emails/__init__.py
@@ -13,7 +13,7 @@ from superdesk.celery_app import celery
 from flask import current_app as app, render_template, render_template_string
 
 
-@celery.task(bind=True, max_retries=3)
+@celery.task(bind=True, max_retries=3, soft_time_limit=10)
 def send_email(self, subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
