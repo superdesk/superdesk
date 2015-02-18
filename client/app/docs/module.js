c5f30dbc25f429d8f0903f260ab75dcf36e84645
Nikola Nikolic <nikolic_nikola@live.com>
2015-02-18 19:02:48 +0100

    feat(docs): Add buttons basics

--- client/app/docs/module.js
+++ client/app/docs/module.js
@@ -14,15 +14,33 @@
 
     var app = angular.module('superdesk.docs', []);
 
-    MainDocsView.$inject = [];
-    function MainDocsView() {
+    MainDocsView.$inject = ['$location', '$anchorScroll'];
+    function MainDocsView($location, $anchorScroll) {
         return {
             templateUrl: 'docs/views/main.html',
             link: function(scope, elem, attrs) {
+                scope.scrollTo = function(id) {
+                    $location.hash(id);
+                    $anchorScroll();
+                };
             }
         };
     }
+
     app.directive('sdDocs', MainDocsView);
+    app.directive('prettyprint', function() {
+        return {
+            restrict: 'C',
+            link: function postLink(scope, element, attrs) {
+                var langExtension = attrs['class'].match(/\blang(?:uage)?-([\w.]+)(?!\S)/);
+                if (langExtension) {
+                    langExtension = langExtension[1];
+                }
+                element.html(window.prettyPrintOne(_.escape(element.html()), langExtension, true));
+            }
+
+        };
+    });
 
     return app;
 
