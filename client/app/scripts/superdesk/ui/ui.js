f087e9bab22701fea1f424df3816b63686e63bb8
Nikola Nikolic <nikolic_nikola@live.com>
2015-02-19 15:40:18 +0100

    fix(styles): Responsive activity dropdown

--- client/app/scripts/superdesk/ui/ui.js
+++ client/app/scripts/superdesk/ui/ui.js
@@ -252,6 +252,29 @@ define([
 		};
 	}
 
+	DropdownPositionDirective.$inject = ['$document'];
+	function DropdownPositionDirective($document) {
+		return {
+			link: function(scope, element) {
+
+				var tolerance = 250;
+
+				element.bind('click', function(event) {
+					if (closeToBottom(event)) {
+						element.addClass('dropup');
+					} else {
+						element.removeClass('dropup');
+					}
+				});
+
+				function closeToBottom(e) {
+					var docHeight = $document.height();
+					return e.pageY > docHeight - tolerance;
+				}
+			}
+		};
+	}
+
 	PopupService.$inject = ['$document'];
 	function PopupService($document) {
 		var service = {};
@@ -750,5 +773,6 @@ define([
 		.directive('sdTimepicker', TimepickerDirective)
 		.service('popupService', PopupService)
 		.service('datetimeHelper', DateTimeHelperService)
-		.filter('leadingZero', LeadingZeroFilter);
+		.filter('leadingZero', LeadingZeroFilter)
+		.directive('sdDropdownPosition', DropdownPositionDirective);
 });
