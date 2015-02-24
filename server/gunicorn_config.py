2b2a767ed0777cfd790a8742c1bccbf541d58c0d
petr.jasek <petr.jasek@sourcefabric.org>
2015-02-24 15:08:02 +0100

    feat(rest): run rest server via gunicorn

---
+++ server/gunicorn_config.py
@@ -0,0 +1,11 @@
+
+import os
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+bind = '0.0.0.0:%s' % os.environ.get('PORT', 5000)
+
+accesslog = '-'
+access_log_format = '%(r)s\nstatus=%(s)s time=%(T)ss bytes=%(b)s pid=%(p)s remote=%(h)s referer=%(f)s'
+
+pidfile = 'gunicorn.pid'
