97e949e1c0e5bbba29e5b399d52216afcbfbd6aa
petr.jasek <petr.jasek@sourcefabric.org>
2015-02-25 15:31:04 +0100

    fix(bamboo): fix server name config for :80
    server name 127.0.0.1:80 won't match 127.0.0.1 url, so in case
    port is :80 it's removed from name.

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -1,15 +1,9 @@
 
 import os
 import multiprocessing
-from urllib.parse import urlparse
 
+bind = '0.0.0.0:%s' % os.environ.get('PORT', '5000')
 workers = multiprocessing.cpu_count() * 2 + 1
-
-if os.environ.get('SUPERDESK_URL'):
-    url = urlparse(os.environ.get('SUPERDESK_URL'))
-    bind = url.netloc
-else:
-    bind = ':%s' % os.environ.get('PORT', 5000)
 
 accesslog = '-'
 access_log_format = '%(r)s\nstatus=%(s)s time=%(T)ss bytes=%(b)s pid=%(p)s remote=%(h)s referer=%(f)s'
