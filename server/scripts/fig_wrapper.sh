3d716302f63e2f6841d37dfc0a217722813e78c3
actionless <actionless.loveless@gmail.com>
2015-03-19 20:21:16 +0100

    wip(client: spec: utils): add more logging in waitForAngular
    wip(client: spec: utils): use webdriver.promise instead of Q
    
    fix(client: spec: login): don't navigate to the same uri twice
    
    fix(server: scripts): nicer output for fig_wrapper
    
    fix(client: spec: utils): format stack trace
    
    fix(server: scripts): nicer output for fig_wrapper - 2
    
    refactor(client: spec: helpers): reuse changeUrl in openUrl
    
    wip(client: spec: utils): refresh the page if changeUrl fails
    
    wip(client: spec: utils): refresh the page if changeUrl fails -2
    
    wip(client: spec: utils): don't print trace from waitForAngular fail
    
    style(client: spec: utils): fix logging
    
    fix(server: scripts): nicer output for fig_wrapper - 3
    
    fix(server: scripts): nicer output for fig_wrapper - 4

--- server/scripts/fig_wrapper.sh
+++ server/scripts/fig_wrapper.sh
@@ -2,14 +2,27 @@
 
 TIMEOUT=2
 
-LISTEN=0
-while [ $LISTEN -eq 0 ]
+MONGO=0
+REDIS=0
+ELASTIC=0
+while [ $MONGO -eq 0 ] || [ $ELASTIC -eq 0 ] || [ $REDIS -eq 0 ]
 do
-	echo "waiting..."
-	LISTEN=1
-	curl -# -m $TIMEOUT mongodb:27017 || LISTEN=0
-	curl -# -m $TIMEOUT redis:6379 | grep "wrong number of arguments for 'get' command" || LISTEN=0
-	curl -# -m $TIMEOUT -o /dev/null elastic:9200 || LISTEN=0
+	if [ $MONGO -eq 0 ] ; then
+		curl -s -m $TIMEOUT mongodb:27017 2>&1 > /dev/null &&
+		MONGO=1 ||
+		echo "waiting for mongo..."
+	fi
+	if [ $REDIS -eq 0 ] ; then
+		curl -# -m $TIMEOUT redis:6379 2>&1 | grep "wrong number of arguments for 'get' command"  2>&1 > /dev/null &&
+		REDIS=1 ||
+		echo "waiting for redis..."
+	fi
+	if [ $ELASTIC -eq 0 ] ; then
+		curl -s -m $TIMEOUT -o /dev/null elastic:9200  2>&1 > /dev/null &&
+		ELASTIC=1 ||
+		echo "waiting for elastic..."
+	fi
+	sleep 0.2
 done
 echo "All services are started :3"
 
