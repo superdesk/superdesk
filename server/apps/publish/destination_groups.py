88316710dea2c0dc17a59337f409a2b783b26e40
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2015-03-31 14:27:53 +1100

    Adding behave test for the output channels.

--- server/apps/publish/destination_groups.py
+++ server/apps/publish/destination_groups.py
@@ -9,6 +9,7 @@
 # at https://www.sourcefabric.org/superdesk/license
 
 import logging
+import superdesk
 from superdesk.resource import Resource
 from superdesk.services import BaseService
 from eve.utils import ParsedRequest
@@ -63,24 +64,33 @@ class DestinationGroupsService(BaseService):
     def on_update(self, updates, original):
         dest_groups = updates.get('destination_groups', [])
         if dest_groups:
-            if self.__is_group_self_referenced(dest_groups):
+            if self.is_group_self_referenced(original[superdesk.config.ID_FIELD], dest_groups):
                 raise SuperdeskApiError.badRequestError(
-                    message='Destination Group is resolves back to itself.')
+                    message='Circular dependency in Destination Group.')
 
     def on_delete(self, doc):
-        parsed_request = ParsedRequest()
-        parsed_request.args = {'destination_groups': doc.get('_id')}
-        if list(get_resource_service('archive').find(req=parsed_request, lookup=None)).count() > 0:
+        archive_content = get_resource_service('archive')\
+            .get(req=None, lookup={'destination_groups': doc.get(superdesk.config.ID_FIELD)})
+        if archive_content and archive_content.count() > 0:
             raise SuperdeskApiError.preconditionFailedError(
                 message='Destination Group is referenced by items.',
                 payload={'item': 1})
 
-        parsed_request = ParsedRequest()
-        parsed_request.args = {'destination_groups.group': doc.get('_id')}
-        if list(get_resource_service('destination_groups').find(req=parsed_request, lookup=None)).count() > 0:
+        dest_groups = self.get(req=None, lookup={'destination_groups.group': doc.get(superdesk.config.ID_FIELD)})
+        if dest_groups and dest_groups.count() > 0:
             raise SuperdeskApiError.preconditionFailedError(
                 message='Destination Group is referenced by another destination group.',
                 payload={'destination_group': 1})
 
-    def __is_group_self_referenced(self):
-        pass
+    def is_group_self_referenced(self, dest_group_id, dest_groups):
+        dest_groups = dest_groups or []
+        for dest_group in dest_groups:
+            if dest_group_id == dest_group['group']:
+                return True
+
+            group = self.find_one(req=None, _id=dest_group['group'])
+            referenced_groups = group.get('destination_groups', [])
+            if referenced_groups:
+                return self.is_group_self_referenced(dest_group_id, referenced_groups)
+
+        return False
