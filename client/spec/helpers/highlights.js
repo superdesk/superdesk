1d84639e33d044c8a937a658f47ae7efc9e11d1e
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2015-04-01 15:29:33 +0300

    chose(highlights): Fix js formatting issues

--- client/spec/helpers/highlights.js
+++ client/spec/helpers/highlights.js
@@ -9,7 +9,7 @@ function Highlights() {
 	this.list = element.all(by.repeater('config in configurations._items'));
 	this.name = element(by.model('configEdit.name'));
 	this.desks = element.all(by.repeater('desk in assignedDesks'));
-	
+
 	this.get = function() {
 	  openUrl('/#/settings/highlights');
 	};
@@ -21,16 +21,16 @@ function Highlights() {
 			  });
 		  });
 	  };
-	
+
 	  this.getCount = function(index) {
 		return this.list.count();
 	  };
-	
+
 	  this.add = function() {
 		element(by.className('icon-plus-sign')).click();
 	    browser.sleep(500);
 	  };
-	
+
 	  this.edit = function(name) {
 		this.getRow(name).then(function(rows) {
 			rows[0].click();
@@ -38,7 +38,7 @@ function Highlights() {
 	    	browser.sleep(500);
 		});
 	  };
-	
+
 	  this.remove = function(name) {
 		this.getRow(name).then(function(rows) {
 			rows[0].click();
@@ -47,16 +47,16 @@ function Highlights() {
 		element(by.buttonText('OK')).click();
 		});
 	  };
-	
+
 	  this.getName = function() {
 	    return this.name.getText();
 	  };
-	
+
 	  this.setName = function(name) {
 		  this.name.clear();
 		  this.name.sendKeys(name);
 	  };
-	
+
 	  this.getDesk = function(name) {
 		  return this.desks.filter(function(elem, index) {
 			  return elem.element(by.binding('desk.name')).getText().then(function(text) {
@@ -64,13 +64,13 @@ function Highlights() {
 			  });
 		  });
 	  };
-	
+
 	  this.toggleDesk = function(name) {
 		  this.getDesk(name).then(function(desks) {
 			  desks[0].element(by.className('sd-checkbox')).click();
 		  });
 	  };
-	
+
 	  this.expectDeskSelection = function(name, selected) {
 		  this.getDesk(name).then(function(desks) {
 			  if (selected) {
@@ -80,15 +80,15 @@ function Highlights() {
 			  }
 		  });
 	  };
-	
+
 	  this.save = function() {
 		  element(by.css('[ng-click="save()"]')).click();
 	  };
-	
+
 	  this.cancel = function() {
 		  element(by.css('[ng-click="cancel()"]')).click();
 	  };
-	
+
 	  this.getHighlights = function(elem) {
 		  return elem.all(by.repeater('h in highlights')).filter(function(elem, index) {
 			  return elem.getText().then(function(text) {
@@ -96,21 +96,21 @@ function Highlights() {
 			  });
 		  });
 	  };
-	
+
 	  this.selectHighlight = function(elem, name) {
 		  elem.all(by.repeater('h in highlights')).all(by.css('[option="' + name.toUpperCase() + '"]')).click();
 	  };
-	
+
 	  this.createHighlightsPackage = function(highlight) {
 		  element(by.className('svg-icon-create-list')).click();
-	  this.selectHighlight(element(by.id('highlightPackage')), highlight);
+	      this.selectHighlight(element(by.id('highlightPackage')), highlight);
 	  };
-	
+
 	  this.switchHighlightFilter = function(name) {
 		  element(by.id('search-highlights')).element(by.className('icon-dots-vertical')).click();
 	  element(by.id('search-highlights')).element(by.css('[option="' + name.toUpperCase() + '"]')).click();
 	  };
-	
+
 	  this.exportHighlights = function() {
 		  element(by.id('export')).click();
 	  };
