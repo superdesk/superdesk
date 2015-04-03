cff04550ed271e12b80ddf231537fc2af6ae8fb2
Peter Lamut <peter.lamut@niteoweb.com>
2015-04-03 12:24:26 +0200

    Add publicapi application skeleton

---
+++ server/publicapi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+from publicapi import app
+
+
+if __name__ == '__main__':
+    app.run(port=5050)  # XXX: have PUBAPI_PORT in config... and other things
---
+++ server/publicapi/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import os
+
+from eve import Eve
+
+
+app = Eve(settings=os.path.join(__name__, 'settings.py'))
