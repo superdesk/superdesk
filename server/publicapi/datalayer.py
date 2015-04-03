d2e997325cc03abf4018b150a71c89052f5e64be
Peter Lamut <peter.lamut@niteoweb.com>
2015-04-03 12:24:26 +0200

    Add publicapi application skeleton

---
+++ server/publicapi/datalayer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+
+# TODO: implement a custom data layer that knows how to fetch data
+# from Superdesk (i.e. byusing Superdesk's existing data layer)
+# basically we need to subclass eve.io.base. DataLayer class
+# (only themethods for reading data, writing not needed for now)
---
+++ server/publicapi/settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+items_schema = {}
+packages_schema = {}
+
+
+DOMAIN = {
+    'items': items_schema,
+    'packages': packages_schema,
+}
