20f29fa7638a914309ebc9591a436d7f04ed5a34
Siva Kuna <skuna@aap.com.au>
2015-04-03 23:44:16 +1100

    [SD-1895] [SD-1896] Rebased destinations branch and resolved github code review comments

--- client/app/scripts/superdesk-publish/publish.js
+++ client/app/scripts/superdesk-publish/publish.js
@@ -32,64 +32,65 @@
         return {
             templateUrl: 'scripts/superdesk-publish/views/output-channels.html',
             link: function ($scope) {
-                    $scope.outputChannel = null;
-                    $scope.origOutputChannel = null;
+                $scope.outputChannel = null;
+                $scope.origOutputChannel = null;
 
-                    function fetchOutputChannels() {
-                        adminPublishSettingsService.fetchOutputChannels()
-                            .then(function(result) {
-                                $scope.outputChannels = result;
-                        });
-                    }
+                function fetchOutputChannels() {
+                    adminPublishSettingsService.fetchOutputChannels().then(
+                        function(result) {
+                            $scope.outputChannels = result;
+                        }
+                    );
+                }
 
-                    function confirm() {
-                        return modal.confirm(gettext('Are you sure you want to delete output channel?'));
-                    }
+                function confirm() {
+                    return modal.confirm(gettext('Are you sure you want to delete output channel?'));
+                }
 
-                    $scope.save = function() {
-                        api.output_channels.save($scope.origOutputChannel, $scope.outputChannel)
-                            .then(
-                                function() {
-                                    notify.success(gettext('Output Channel saved.'));
-                                    $scope.cancel();
-                                },
-                                function(response) {
-                                    if (angular.isDefined(response.data._issues) &&
-                                            angular.isDefined(response.data._issues['validator exception'])) {
-                                        notify.error(gettext('Error: ' + response.data._issues['validator exception']));
-                                    } else {
-                                        notify.error(gettext('Error: Failed to save Output Channel.'));
-                                    }
-                                }
-                            ).then(fetchOutputChannels);
-                    };
-
-                    $scope.edit = function(outputChannel) {
-                        $scope.origOutputChannel = outputChannel || {};
-                        $scope.outputChannel = _.create($scope.origOutputChannel);
-                    };
-
-                    $scope.remove = function(outputChannel) {
-                        confirm().then(function() {
-                            api.output_channels.remove(outputChannel)
-                            .then(function(result) {
-                                _.remove($scope.outputChannels, outputChannel);
-                            }, function(response) {
-                                if (angular.isDefined(response.data._message)) {
-                                    notify.error(gettext('Error: ' + response.data._message));
+                $scope.save = function() {
+                    api.output_channels.save($scope.origOutputChannel, $scope.outputChannel)
+                        .then(
+                            function() {
+                                notify.success(gettext('Output Channel saved.'));
+                                $scope.cancel();
+                            },
+                            function(response) {
+                                if (angular.isDefined(response.data._issues) &&
+                                        angular.isDefined(response.data._issues['validator exception'])) {
+                                    notify.error(gettext('Error: ' + response.data._issues['validator exception']));
                                 } else {
-                                    notify.error(gettext('There is an error. Output Channel cannot be deleted.'));
+                                    notify.error(gettext('Error: Failed to save Output Channel.'));
                                 }
-                            }).then(fetchOutputChannels);
-                        });
-                    };
+                            }
+                        ).then(fetchOutputChannels);
+                };
 
-                    $scope.cancel = function() {
-                        $scope.origOutputChannel = null;
-                        $scope.outputChannel = null;
-                    };
+                $scope.edit = function(outputChannel) {
+                    $scope.origOutputChannel = outputChannel || {};
+                    $scope.outputChannel = _.create($scope.origOutputChannel);
+                };
 
-                    fetchOutputChannels();
+                $scope.remove = function(outputChannel) {
+                    confirm().then(function() {
+                        api.output_channels.remove(outputChannel)
+                        .then(function(result) {
+                            _.remove($scope.outputChannels, outputChannel);
+                        }, function(response) {
+                            if (angular.isDefined(response.data._message)) {
+                                notify.error(gettext('Error: ' + response.data._message));
+                            } else {
+                                notify.error(gettext('There is an error. Output Channel cannot be deleted.'));
+                            }
+                        }).then(fetchOutputChannels);
+                    });
+                };
+
+                $scope.cancel = function() {
+                    $scope.origOutputChannel = null;
+                    $scope.outputChannel = null;
+                };
+
+                fetchOutputChannels();
             }
         };
     }
@@ -99,76 +100,77 @@
         return {
             templateUrl: 'scripts/superdesk-publish/views/destination-groups.html',
             link: function ($scope) {
-                    $scope.destinationGroup = null;
-                    $scope.origDestinationGroup = null;
+                $scope.destinationGroup = null;
+                $scope.origDestinationGroup = null;
 
-                    function fetchDestinationGroups() {
-                        adminPublishSettingsService.fetchDestinationGroups()
-                            .then(function(result) {
-                                $scope.destinationGroups = result;
-                        });
-                    }
+                function fetchDestinationGroups() {
+                    adminPublishSettingsService.fetchDestinationGroups().then(
+                        function(result) {
+                            $scope.destinationGroups = result;
+                        }
+                    );
+                }
 
-                    function confirm() {
-                        return modal.confirm(gettext('Are you sure you want to delete destination group?'));
-                    }
+                function confirm() {
+                    return modal.confirm(gettext('Are you sure you want to delete destination group?'));
+                }
 
-                    $scope.save = function() {
-                        api.destination_groups.save($scope.origDestinationGroup, $scope.destinationGroup)
-                            .then(
-                                function() {
-                                    notify.success(gettext('Destination Group saved.'));
-                                    $scope.cancel();
-                                },
-                                function(response) {
-                                    if (angular.isDefined(response.data._issues) &&
-                                            angular.isDefined(response.data._issues['validator exception'])) {
-                                        notify.error(gettext('Error: ' + response.data._issues['validator exception']));
-                                    } else {
-                                        notify.error(gettext('Error: Failed to save Destination Group.'));
-                                    }
-                                }
-                            ).then(fetchDestinationGroups);
-                    };
-
-                    $scope.edit = function(destinationGroup) {
-                        $scope.origDestinationGroup = destinationGroup || {};
-                        $scope.destinationGroup = _.create($scope.origDestinationGroup);
-
-                        adminPublishSettingsService.fetchOutputChannels().then(function(result) {
-                            if (angular.isDefined(result) && angular.isDefined(result._items)) {
-                                $scope.outputChannels = result._items;
-                            }
-                        });
-
-                        adminPublishSettingsService.fetchDestinationGroups().then(function(result) {
-                            if (angular.isDefined(result) && angular.isDefined(result._items)) {
-                                $scope.destinationGroupsLookup = result._items;
-                            }
-                        });
-                    };
-
-                    $scope.remove = function(destinationGroup) {
-                        confirm().then(function() {
-                            api.destination_groups.remove(destinationGroup)
-                            .then(function(result) {
-                                _.remove($scope.destinationGroups, destinationGroup);
-                            }, function(response) {
-                                if (angular.isDefined(response.data._message)) {
-                                    notify.error(gettext('Error: ' + response.data._message));
+                $scope.save = function() {
+                    api.destination_groups.save($scope.origDestinationGroup, $scope.destinationGroup)
+                        .then(
+                            function() {
+                                notify.success(gettext('Destination Group saved.'));
+                                $scope.cancel();
+                            },
+                            function(response) {
+                                if (angular.isDefined(response.data._issues) &&
+                                        angular.isDefined(response.data._issues['validator exception'])) {
+                                    notify.error(gettext('Error: ' + response.data._issues['validator exception']));
                                 } else {
-                                    notify.error(gettext('There is an error. Destination Group cannot be deleted.'));
+                                    notify.error(gettext('Error: Failed to save Destination Group.'));
                                 }
-                            }).then(fetchDestinationGroups);
-                        });
-                    };
+                            }
+                        ).then(fetchDestinationGroups);
+                };
 
-                    $scope.cancel = function() {
-                        $scope.origDestinationGroup = null;
-                        $scope.destinationGroup = null;
-                    };
+                $scope.edit = function(destinationGroup) {
+                    $scope.origDestinationGroup = destinationGroup || {};
+                    $scope.destinationGroup = _.create($scope.origDestinationGroup);
 
-                    fetchDestinationGroups();
+                    adminPublishSettingsService.fetchOutputChannels().then(function(result) {
+                        if (angular.isDefined(result) && angular.isDefined(result._items)) {
+                            $scope.outputChannels = result._items;
+                        }
+                    });
+
+                    adminPublishSettingsService.fetchDestinationGroups().then(function(result) {
+                        if (angular.isDefined(result) && angular.isDefined(result._items)) {
+                            $scope.destinationGroupsLookup = result._items;
+                        }
+                    });
+                };
+
+                $scope.remove = function(destinationGroup) {
+                    confirm().then(function() {
+                        api.destination_groups.remove(destinationGroup)
+                        .then(function(result) {
+                            _.remove($scope.destinationGroups, destinationGroup);
+                        }, function(response) {
+                            if (angular.isDefined(response.data._message)) {
+                                notify.error(gettext('Error: ' + response.data._message));
+                            } else {
+                                notify.error(gettext('There is an error. Destination Group cannot be deleted.'));
+                            }
+                        }).then(fetchDestinationGroups);
+                    });
+                };
+
+                $scope.cancel = function() {
+                    $scope.origDestinationGroup = null;
+                    $scope.destinationGroup = null;
+                };
+
+                fetchDestinationGroups();
             }
         };
     }
