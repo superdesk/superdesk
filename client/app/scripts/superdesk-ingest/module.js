96d7ee01e82047d6b3e570386fb7e02759353d55
eozan <erdemozanozbek@gmail.com>
2015-04-07 07:19:29 +0200

    feat(publish): Added destination groups and output channels.
    SD-1964

--- client/app/scripts/superdesk-ingest/module.js
+++ client/app/scripts/superdesk-ingest/module.js
@@ -956,6 +956,13 @@ define([
 
                 scope.addFetch = function() {
                     if (scope.newFetch.desk && scope.newFetch.stage) {
+                        if (scope.newFetch.destination_groups) {
+                            var destinationGroups = _.pluck(scope.newFetch.destination_groups, '_id');
+                            scope.newFetch.destination_groups = [];
+                            _.each(destinationGroups, function(destinationGroup) {
+                                scope.newFetch.destination_groups.push({group: destinationGroup});
+                            });
+                        }
                         scope.rule.actions.fetch.push(scope.newFetch);
                         scope.newFetch = {};
                     }
@@ -969,6 +976,13 @@ define([
 
                 scope.addPublish = function() {
                     if (scope.newPublish.desk && scope.newPublish.stage) {
+                        if (scope.newPublish.destination_groups) {
+                            var destinationGroups = _.pluck(scope.newPublish.destination_groups, '_id');
+                            scope.newPublish.destination_groups = [];
+                            _.each(destinationGroups, function(destinationGroup) {
+                                scope.newPublish.destination_groups.push({group: destinationGroup});
+                            });
+                        }
                         scope.rule.actions.publish.push(scope.newPublish);
                         scope.newPublish = {};
                     }
