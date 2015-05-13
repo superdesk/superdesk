59fe315c28a2ca09d13449c4a6505b4d9fc31871
akintolga <akint@aap.com.au>
2015-05-13 16:54:55 +1000

    [SD-1988] - Refactoring te errors end point

--- server/superdesk/io/io_errors.py
+++ server/superdesk/io/io_errors.py
@@ -11,36 +11,42 @@
 import logging
 import superdesk
 from superdesk.io import provider_errors
+from superdesk.publish import transmitter_errors
 from superdesk.utils import ListCursor
 
 logger = logging.getLogger(__name__)
 
 
-class IngestErrorsService(superdesk.Service):
+class IOErrorsService(superdesk.Service):
     def get(self, req, lookup):
         """Return all ingest errors."""
+        errors = provider_errors
+        io_type = getattr(req, 'args', {}).get('io_type', 'ingest')
+        if io_type == 'publish':
+            errors = transmitter_errors
+
         source_type = getattr(req, 'args', {}).get('source_type')
         if source_type:
-            return ListCursor([self.get_errors_by_source_type(source_type)])
+            return ListCursor([self.get_errors_by_source_type(source_type, errors)])
         else:
-            return ListCursor([self.get_all_errors()])
+            return ListCursor([self.get_all_errors(errors)])
 
-    def get_errors_by_source_type(self, source_type):
-        return {'source_errors': provider_errors[source_type.lower()],
+    def get_errors_by_source_type(self, source_type, errors):
+        return {'source_errors': errors[source_type.lower()],
                 'all_errors': self._get_all_errors()}
 
-    def get_all_errors(self):
-        return {'all_errors': self._get_all_errors()}
+    def get_all_errors(self, errors):
+        return {'all_errors': self._get_all_errors(errors)}
 
-    def _get_all_errors(self):
+    def _get_all_errors(self, errors):
         all_errors = {}
-        for k, v in provider_errors.items():
+        for k, v in errors.items():
             all_errors.update(v)
 
         return all_errors
 
 
-class IngestErrorsResource(superdesk.Resource):
+class IOErrorsResource(superdesk.Resource):
     resource_methods = ['GET']
     item_methods = []
 
@@ -52,4 +58,7 @@ class IngestErrorsResource(superdesk.Resource):
         'source_type': {
             'type': 'string'
         },
+        'io_type': {
+            'type': 'string'
+        }
     }
