529f6dd8842395a4c93d321df5bcd0a37bc1fb40
Syed <sjunaid@aap.com.au>
2015-05-18 22:09:53 -0700

    [sd-2286-2](fix-Edit issue) protoype auth space (production)

--- client/app/scripts/superdesk-production/production.js
+++ client/app/scripts/superdesk-production/production.js
@@ -1,14 +1,11 @@
 (function() {
     'use strict';
 
-    ProductionController.$inject = ['$scope', 'production', 'superdesk', 'authoring', '$location', 'referrer'];
-    function ProductionController($scope, production, superdesk, authoring, $location, referrer) {
+    ProductionController.$inject = ['$scope', 'production', 'superdesk', 'authoring', '$location', 'referrer', '$timeout'];
+    function ProductionController($scope, production, superdesk, authoring, $location, referrer, $timeout) {
         $scope.productionPreview = true;
-        $scope.origItem = {};
-        $scope.action = 'view';
         $scope.viewdefault = true;
         $scope.selected_id = null;
-        $scope.items = {};
         $scope.$on('itemClosing', function() {
             $scope.viewdefault = true;
         });
@@ -16,24 +13,36 @@
         $scope.$on('handleItemPreview', function(event, item) {
             referrer.setReferrerUrl($location.path());
             $scope.origItem = item;
-            $scope.selected_id = item._id;
             $scope.action = 'view';
-            $scope._editable = null;
+            $scope._editable = false;
             $scope.viewdefault = false;
+            item._editable = false;
+
+            var data = {};
+            data.item = item;
+            data.action = 'view';
+            $scope.viewdefault = false;
+            $timeout(function() {
+                $scope.$root.$broadcast('showPreview', data);
+            }, 200);
         });
         $scope.$on('handleItemEdit', function(event, item) {
             referrer.setReferrerUrl($location.path());
             item._editable = true;
+            $scope.origItem = item;
+            $scope.action = 'view';
+            $scope._editable = false;
+
             authoring.open(item._id, false).then(function(item) {
-                $scope.origItem = item;
-                $scope.action = 'edit';
-                $scope.origItem._editable = true;
+                var data = {};
+                data.item = item;
+                data.action = 'edit';
+                $scope.$root.$broadcast('showPreview', data);
             })
             ['finally'](function() {
                 $scope.viewdefault = false;
             });
         });
-
     }
 
     ProductionService.$inject = ['api', '$q'];
