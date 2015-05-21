4c65959862fab9eb1ba1dd0d5508fadc59e368cf
Peter Lamut <peter.lamut@niteoweb.com>
2015-05-21 13:41:29 +0200

    public API: adjust items/packages schema and demo data to NINJS format
    SD-2223

--- server/publicapi/packages/resource.py
+++ server/publicapi/packages/resource.py
@@ -8,34 +8,11 @@
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
 
-from superdesk.resource import Resource
+from publicapi.items.resource import ItemsResource
 
 
-class PackagesResource(Resource):
+class PackagesResource(ItemsResource):
     """A class defining and configuring the /packages API endpoint."""
-
-    # Example of an ID of an object in database (whitout quotes):
-    #
-    #   "tag:example.com,0000:newsml_BRE9A605"
-    #
-    item_url = 'regex("[\w,.:-]+")'
-
-    schema = {
-        'guid': {'type': 'string'},
-        'type': {'type': 'string'},
-        'unique_id': {'type': 'integer'},
-        'unique_name': {'type': 'string'},
-        'pubstatus': {'type': 'string'},
-        'firstcreated': {'type': 'datetime'},
-        'versioncreated': {'type': 'datetime'},
-        'original_creator': {'type': 'string'},
-        'version_creator': {'type': 'string'},
-        'groups': {'type': 'list'},
-        'language': {'type': 'string'},
-        'headline': {'type': 'string'},
-        'slugline': {'type': 'string'},
-        'description': {'type': 'string'},
-    }
 
     datasource = {
         'filter': {'type': 'composite'},
@@ -45,6 +22,3 @@ class PackagesResource(Resource):
             '_updated': 0,
         }
     }
-
-    item_methods = ['GET']
-    resource_methods = ['GET']
