79faefc6e175d30e046e014aaa17e1c20f556264
Aca-jov <aca84.jov@gmail.com>
2015-05-27 16:05:51 +0200

    Merge

--- client/app/scripts/superdesk-desks/aggregate.js
+++ client/app/scripts/superdesk-desks/aggregate.js
@@ -137,6 +137,13 @@
                     scope.closeModal();
                 };
 
+                $(document).on('hidden.bs.modal', '.modal', function () {
+                    scope.closeModal();
+                    if (!scope.$$phase) {
+                        scope.$digest();
+                    }
+                });
+
                 scope.setDeskInfo = function(_id) {
                     var item = scope.editGroups[_id];
                     item._id = _id;
