1e2ce31e0839a3d4bed0cd93163b3d6ece3498e2
pavlovicnemanja92 <me@pavlovicnemanja.com>
2015-06-01 16:04:10 +0200

    feat(e2e test): Added e2e test for subject codes

--- client/spec/search_spec.js
+++ client/spec/search_spec.js
@@ -39,6 +39,33 @@ describe('Search', function() {
         expect(element.all(by.repeater('items._items')).count()).toBe(1);
         expect(element.all(by.repeater('parameter in tags.selectedKeywords')).count()).toBe(1);
     });
+    
+    it('can search by subject codes field', function() {
+        workspace.switchToDesk('SPORTS DESK').then(content.setListView);
+        expect(element.all(by.repeater('items._items')).count()).toBe(2);
+        
+        var filterPanelButton = element(by.css('.fitler-trigger'));
+        var subject = element.all(by.css('.dropdown-nested')).first();
+        var subjectToggle = subject.element(by.css('.dropdown-toggle'));
+        
+        filterPanelButton.click().then(
+            function(){
+                return element.all(by.css('[ng-click="toggleModule()"]')).first().click();
+            }).then(
+            function(){             
+                return subjectToggle.click();
+            }).then(
+            function () {
+                return subject.all(by.css('.nested-toggle')).first().click();
+            }).then(
+            function () {
+                return subject.all(by.repeater('term in activeTree')).first().click();
+            }).then(function () {
+                expect(element.all(by.repeater('item in items._items')).count()).toBe(0);
+                expect(element.all(by.repeater('parameter in tags.selectedParameters')).count()).toBe(1);
+                expect(element.all(by.repeater('t in item[field]')).count()).toBe(1);
+            });
+    });
 
     it('can filter by state', function() {
         workspace.switchToDesk('SPORTS DESK').then(content.setListView);
