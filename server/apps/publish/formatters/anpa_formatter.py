4714b6085319d7c1f0eb9c1e8a835a8a30743c95
marwoodandrew <amarwood@aap.com.au>
2015-06-03 11:28:32 +1000

    [SD-2373] construct bytes using join

--- server/apps/publish/formatters/anpa_formatter.py
+++ server/apps/publish/formatters/anpa_formatter.py
@@ -21,97 +21,97 @@ class AAPAnpaFormatter(Formatter):
 
             pub_seq_num = superdesk.get_resource_service('output_channels').generate_sequence_number(output_channel)
 
-            anpa = b''
+            anpa = []
 
             # Selector code specific optional
             if selector_codes and len(selector_codes) > 0:
-                anpa = b'\x05'
+                anpa.append(b'\x05')
                 sel_codes = selector_codes[output_channel['_id']]
-                anpa += ' '.join(sel_codes).lower().encode('ascii')
-                anpa += b'\x0D\x0A'
+                anpa.append(' '.join(sel_codes).lower().encode('ascii'))
+                anpa.append(b'\x0D\x0A')
 
             # start of message header (syn syn soh)
-            anpa += b'\x16\x16\x01'
-            anpa += article.get('service_level', 'a').lower().encode('ascii')
+            anpa.append(b'\x16\x16\x01')
+            anpa.append(article.get('service_level', 'a').lower().encode('ascii'))
             # story number
-            anpa += str(pub_seq_num).zfill(4).encode('ascii')
+            anpa.append(str(pub_seq_num).zfill(4).encode('ascii'))
             # field seperator
-            anpa += b'\x0A'  # -LF
-            anpa += article.get('priority', 'r').encode('ascii')
-            anpa += b'\x20'
+            anpa.append(b'\x0A')  # -LF
+            anpa.append(article.get('priority', 'r').encode('ascii'))
+            anpa.append(b'\x20')
 
             category = article.get('anpa-category', {})
             if 'qcode' in category:
-                anpa += article.get('anpa-category', {})['qcode'].encode('ascii')
+                anpa.append(article.get('anpa-category', {})['qcode'].encode('ascii'))
             # TODO TODO TODO
             # NEED TO SORT THIS OUT I THINK THE AUTHORING GETS IT WRONG
             if 'value' in category:
-                anpa += article.get('anpa-category', {})['value'].encode('ascii')
+                anpa.append(article.get('anpa-category', {})['value'].encode('ascii'))
 
-            anpa += b'\x13'
+            anpa.append(b'\x13')
             # format identifier
             if article['type'] == 'preformatted':
-                anpa += b'\x12'
+                anpa.append(b'\x12')
             else:
-                anpa += b'\x11'
-            anpa += b'\x20'
+                anpa.append(b'\x11')
+            anpa.append(b'\x20')
 
             # keyword
             keyword = 'bc-{}'.format(article.get('slugline', '')).replace(' ', '-')
             keyword = keyword[:24] if len(keyword) > 24 else keyword
-            anpa += keyword.encode('ascii')
-            anpa += b'\x20'
+            anpa.append(keyword.encode('ascii'))
+            anpa.append(b'\x20')
 
             # version field
-            anpa += b'\x20'
+            anpa.append(b'\x20')
 
             # reference field
-            anpa += b'\x20'
+            anpa.append(b'\x20')
 
             # filing date
-            anpa += '{}-{}'.format(article['_updated'].strftime('%m'),
-                                   article['_updated'].strftime('%d')).encode('ascii')
-            anpa += b'\x20'
+            anpa.append('{}-{}'.format(article['_updated'].strftime('%m'),
+                                       article['_updated'].strftime('%d')).encode('ascii'))
+            anpa.append(b'\x20')
 
             # add the word count
-            anpa += str(article.get('word_count', '0000')).zfill(4).encode('ascii')
-            anpa += b'\x0D\x0A'
+            anpa.append(str(article.get('word_count', '0000')).zfill(4).encode('ascii'))
+            anpa.append(b'\x0D\x0A')
 
-            anpa += b'\x02'  # STX
+            anpa.append(b'\x02')  # STX
 
             # Add the actual story bits
-            anpa += article.get('headline', '').encode('ascii', 'replace')
-            anpa += b'\x0D\x0A'
+            anpa.append(article.get('headline', '').encode('ascii', 'replace'))
+            anpa.append(b'\x0D\x0A')
 
             keyword = article.get('slugline', '').encode('ascii', 'ignore')
-            anpa += keyword
+            anpa.append(keyword)
             take_key = article.get('anpa_take_key', '').encode('ascii', 'ignore')
-            anpa += (b'\x20' + take_key) if len(take_key) > 0 else b''
-            anpa += b'\x0D\x0A'
+            anpa.append((b'\x20' + take_key) if len(take_key) > 0 else b'')
+            anpa.append(b'\x0D\x0A')
 
             if article['type'] == 'preformatted':
-                anpa += article.get('body_html', '').encode('ascii', 'replace')
+                anpa.append(article.get('body_html', '').encode('ascii', 'replace'))
             else:
-                anpa += BeautifulSoup(article.get('body_html', '')).text.encode('ascii', 'replace')
+                anpa.append(BeautifulSoup(article.get('body_html', '')).text.encode('ascii', 'replace'))
 
-            anpa += b'\x0D\x0A'
+            anpa.append(b'\x0D\x0A')
             if article.get('more_coming', False):
-                anpa += 'MORE'.encode('ascii')
+                anpa.append('MORE'.encode('ascii'))
             else:
-                anpa += article.get('source', '').encode('ascii')
+                anpa.append(article.get('source', '').encode('ascii'))
             sign_off = article.get('sign_off', '').encode('ascii')
-            anpa += (b'\x20' + sign_off) if len(sign_off) > 0 else b''
-            anpa += b'\x0D\x0A'
+            anpa.append((b'\x20' + sign_off) if len(sign_off) > 0 else b'')
+            anpa.append(b'\x0D\x0A')
 
-            anpa += b'\x03'  # ETX
+            anpa.append(b'\x03')  # ETX
 
             # time and date
-            anpa += datetime.datetime.now().strftime('%d-%m-%y %H-%M-%S').encode('ascii')
+            anpa.append(datetime.datetime.now().strftime('%d-%m-%y %H-%M-%S').encode('ascii'))
 
-            anpa += b'\x04'  # EOT
-            anpa += b'\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A'
+            anpa.append(b'\x04')  # EOT
+            anpa.append(b'\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A')
 
-            return pub_seq_num, anpa
+            return pub_seq_num, b''.join(anpa)
         except Exception as ex:
             raise FormatterError.AnpaFormatterError(ex, output_channel)
 
