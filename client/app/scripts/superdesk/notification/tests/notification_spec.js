13636c699b188077529d957cf9c0facde8baacc0
Syed <sjunaid@aap.com.au>
2015-06-10 03:03:27 -0700

    [sd-2389-dest]-(improvement)-UI: Reload the privileges, desks and stages when there's a RELOAD message(fix test)

--- client/app/scripts/superdesk/notification/tests/notification_spec.js
+++ client/app/scripts/superdesk/notification/tests/notification_spec.js
@@ -8,27 +8,35 @@ describe('Reload Service', function() {
         _id: '1'
     };
 
-    var rootScope, reloadService;
-    var msg = {
-        event: 'desk_membership_revoked',
-        extra: {
-                    desk_id: '5567ff31102454c7bac47644',
-                    user_ids: ['1']
-                }
-    };
+    var rootScope, reloadService, msg;
     beforeEach(function() {
-        inject(function($rootScope, _reloadService_, session, $window) {
+        inject(function($rootScope, _reloadService_, session, $q, api, preferencesService, desks, $window) {
             rootScope = $rootScope;
             reloadService = _reloadService_;
             session.start({}, USER);
+
+            spyOn(session, 'getIdentity').and.returnValue($q.when({_links: {self: {href: USER_URL}}}));
+            spyOn(api, 'get').and.returnValue($q.when({_items: [{_id: '5567ff31102454c7bac47644'}, {_id: '55394997102454b5ea111bd5'}]}));
+            spyOn(preferencesService, 'get').and.returnValue($q.when([]));
+            spyOn(preferencesService, 'update');
+
+            desks.fetchCurrentUserDesks().then(function(_userDesks) {
+                reloadService.userDesks = _userDesks._items;
+            });
+
+            rootScope.$apply();
+            expect(reloadService.userDesks.length).toBe(2);
         });
     });
 
-    it('can check reloadEvents on raised event for desk', inject(function($window, desks, session, api, preferencesService, $q) {
-        spyOn(session, 'getIdentity').and.returnValue($q.when({_links: {self: {href: USER_URL}}}));
-        spyOn(api, 'get').and.returnValue($q.when({_items: [{_id: '5567ff31102454c7bac47644'}, {_id: '55394997102454b5ea111bd5'}]}));
-        rootScope.$apply();
-        expect(reloadService.userDesks.length).toBe(2);
+    it('can check reloadEvents on raised event for desk', inject(function() {
+        msg = {
+            event: 'desk_membership_revoked',
+            extra: {
+                        desk_id: '5567ff31102454c7bac47644',
+                        user_ids: ['1']
+                    }
+        };
 
         var reload = spyOn(reloadService, 'reload');
         rootScope.$broadcast('reload', msg);
@@ -36,7 +44,7 @@ describe('Reload Service', function() {
         expect(reloadService.result.reload).toBe(true);
     }));
 
-    it('can check reloadEvents on raised event for stage', inject(function($window, desks, session, api, preferencesService, $q) {
+    it('can check reloadEvents on raised event for stage', inject(function() {
         msg = {
             event: 'stage',
             extra: {
@@ -44,15 +52,10 @@ describe('Reload Service', function() {
                         user_ids: ['1']
                     }
         };
-        spyOn(session, 'getIdentity').and.returnValue($q.when({_links: {self: {href: USER_URL}}}));
-        spyOn(api, 'get').and.returnValue($q.when({_items: [{_id: '5567ff31102454c7bac47644'}, {_id: '55394997102454b5ea111bd5'}]}));
-        rootScope.$apply();
-        expect(reloadService.userDesks.length).toBe(2);
 
         var reload = spyOn(reloadService, 'reload');
         rootScope.$broadcast('reload', msg);
         expect(reload).toHaveBeenCalledWith(Object({reload: true, message: 'Stage is created/updated/deleted'}));
         expect(reloadService.result.reload).toBe(true);
     }));
-
 });
