6a70eb2ee26194b1b961703d231cfe30576964e4
Siva Kuna <skuna@aap.com.au>
2015-06-16 13:29:58 +1000

    [SD-2426]  Resolving github comments

--- server/apps/messaging/messages.py
+++ server/apps/messaging/messages.py
@@ -7,7 +7,6 @@
 # For the full copyright and license information, please see the
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
-from eve.utils import config
 
 from apps.archive.common import get_user
 
@@ -45,37 +44,7 @@ class MessageService(Service):
                 raise SuperdeskApiError.forbiddenError(message)
             doc['sender'] = str(user.get('_id'))
 
-            chat_session = get_resource_service(CHAT_SESSIONS).find_one(req=None, _id=doc['chat_session'])
-            if chat_session is None:
-                SuperdeskApiError.badRequestError('Invalid Chat Session')
-
     def on_created(self, docs):
         for doc in docs:
-            recipients = self.resolve_message_recipients(doc)
+            recipients = get_resource_service(CHAT_SESSIONS).resolve_message_recipients(doc['chat_session'])
             push_notification(self.notification_key, message=doc['message'], recipients=recipients)
-
-    def resolve_message_recipients(self, doc):
-        """
-        De-normalizes the chat session if it's tied to either desk or group or both.
-        :return: User ID list
-        """
-        recipients = set()
-
-        chat_session = get_resource_service(CHAT_SESSIONS).find_one(req=None, _id=doc['chat_session'])
-
-        if len(chat_session.get('users', [])):
-            recipients.update([str(user) for user in chat_session['users']])
-
-        if len(chat_session.get('desks', [])):
-            query = {'$and': [{config.ID_FIELD: {'$in': chat_session['desks']}}]}
-            desks = list(get_resource_service('desks').get(req=None, lookup=query))
-
-            recipients.update([str(member['user']) for desk in desks for member in desk.get('members', [])])
-
-        if len(chat_session.get('groups', [])):
-            query = {'$and': [{config.ID_FIELD: {'$in': chat_session['groups']}}]}
-            groups = list(get_resource_service('groups').get(req=None, lookup=query))
-
-            recipients.update([str(member['user']) for group in groups for member in group.get('members', [])])
-
-        return list(recipients)
