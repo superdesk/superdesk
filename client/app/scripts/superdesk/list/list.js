403493c679fb5045930357012c4b4acc05e87366
Aca-jov <aca84.jov@gmail.com>
2015-06-19 13:54:30 +0200

    Fix (on search close update user list)

--- client/app/scripts/superdesk/list/list.js
+++ client/app/scripts/superdesk/list/list.js
@@ -115,6 +115,7 @@
 
                 scope.close = function() {
                     scope.q = null;
+                    scope.search();
                     input.focus();
                 };
             }
