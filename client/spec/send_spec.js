de4f56887984fa70c35994fd893111dc1227f569
Nemanja Pavlović <me@pavlovicnemanja.com>
2015-06-26 13:07:11 +0200

    feat(send to e2e test): Added e2e tests for checking spelling mistakes when sending item

--- client/spec/send_spec.js
+++ client/spec/send_spec.js
@@ -17,4 +17,33 @@ describe('Send To', function() {
                 .getText()
         ).toBe('SUBMITTED');
     });
+    it('warns that there are spelling mistakes', function () {
+        workspace.open();
+        workspace.editItem(1);
+        authoring.writeText('mispeled word');
+        authoring.sendTo('Sports Desk');
+        expect(element(by.className('modal-content')).isDisplayed()).toBe(true);
+    });
+    it('can submit item to a desk although there are spelling mistakes', function () {
+        workspace.open();
+        workspace.editItem(1);
+        authoring.writeText('mispeled word');
+        authoring.sendTo('Sports Desk');
+        element(by.className('modal-content')).all(by.css('[ng-click="ok()"]')).click();
+        workspace.switchToDesk('SPORTS DESK');
+        expect(
+                element.all(by.repeater('items._items'))
+                .first()
+                .element(by.css('.state-label'))
+                .getText()
+                ).toBe('SUBMITTED');
+    });
+    it('can cancel submit request because there are spelling mistakes', function () {
+        workspace.open();
+        workspace.editItem(1);
+        authoring.writeText('mispeled word');
+        authoring.sendTo('Sports Desk');
+        element(by.className('modal-content')).all(by.css('[ng-click="cancel()"]')).click();
+        expect(browser.getCurrentUrl()).toMatch(/authoring/);
+    });
 });
