f06a9778d949a98487d809d5c2ac3b7355fcfe62
Syed <sjunaid@aap.com.au>
2015-07-06 00:12:22 -0700

    [sd-2453-1] (enhanced e2e test) undo-redo for medium editor

--- client/spec/authoring_spec.js
+++ client/spec/authoring_spec.js
@@ -5,13 +5,13 @@ var workspace = require('./helpers/workspace'),
     authoring = require('./helpers/authoring');
 
 describe('authoring', function() {
-    it('can open item stage', function() {
+    xit('can open item stage', function() {
         workspace.open();
         workspace.editItem(0, 'SPORTS DESK');
         element(by.css('button.stage')).click();
         expect(browser.getCurrentUrl()).toMatch(/workspace\/content$/);
     });
-    it('Can Undo content', function() {
+    xit('Can Undo content', function() {
         workspace.open();
         workspace.editItem(1);
         authoring.writeText('Two');
@@ -22,6 +22,14 @@ describe('authoring', function() {
 
         var focused = browser.driver.switchTo().activeElement().getText();
         expect(focused).toEqual('Two');
+        // headline editor
+        authoring.writeTextToHeadline('headline');
+
+        browser.actions().sendKeys(Key.chord(Key.CONTROL, 'z')).perform();
+
+        focused = browser.driver.switchTo().activeElement().getText();
+        expect(focused).toEqual('item1');
+
     });
     it('Can Redo content', function () {
         workspace.open();
@@ -39,5 +47,19 @@ describe('authoring', function() {
 
         focused = browser.driver.switchTo().activeElement().getText();
         expect(focused).toEqual('UndoRedo');
+        // abstract editor
+        authoring.writeTextToAbstract('Redo');
+        browser.sleep(200);
+
+        browser.actions().sendKeys(Key.chord(Key.CONTROL, 'z')).perform();
+
+        focused = browser.driver.switchTo().activeElement().getText();
+        expect(focused).toEqual('R');
+
+        browser.actions().sendKeys(Key.chord(Key.CONTROL, 'y')).perform();
+
+        focused = browser.driver.switchTo().activeElement().getText();
+        expect(focused).toEqual('Redo');
+
     });
 });
