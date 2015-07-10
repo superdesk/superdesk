3e30d96c0aec564e55f37a617ee929314572a6c7
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2015-07-10 22:49:58 +1000

    Modified bulletin builder formatter to strip html.

--- server/apps/publish/formatters/aap_bulletinbuilder_formatter.py
+++ server/apps/publish/formatters/aap_bulletinbuilder_formatter.py
@@ -8,10 +8,12 @@
 # AUTHORS and LICENSE files distributed with this source code, or
 # at https://www.sourcefabric.org/superdesk/license
 
+from io import StringIO
 from apps.publish.formatters import Formatter
 import superdesk
 from superdesk.utils import json_serialize_datetime_objectId
 from superdesk.errors import FormatterError
+from bs4 import BeautifulSoup
 
 
 class AAPBulletinBuilderFormatter(Formatter):
@@ -21,6 +23,18 @@ class AAPBulletinBuilderFormatter(Formatter):
     def format(self, article, subscriber):
         try:
             pub_seq_num = superdesk.get_resource_service('subscribers').generate_sequence_number(subscriber)
+            body_html = article.get('body_html', '').strip('\r\n')
+            soup = BeautifulSoup(body_html)
+            for br in soup.find_all('br'):
+                # remove the <br> tag
+                br.replace_with(' {}'.format(br.get_text()))
+
+            for p in soup.find_all('p'):
+                # replace <p> tag with two carriage return
+                p.replace_with('{}\r\n\r\n'.format(p.get_text()))
+
+            article['body_text'] = soup.get_text()
+
             return pub_seq_num, superdesk.json.dumps(article, default=json_serialize_datetime_objectId)
         except Exception as ex:
             raise FormatterError.bulletinBuilderFormatterError(ex, subscriber)
