a8411653b74fb62935b230466fa0ab3e29160da9
Aca-jov <aca84.jov@gmail.com>
2015-07-24 16:39:55 +0200

    Fix (test added)

--- client/app/scripts/superdesk-workspace/content/content.js
+++ client/app/scripts/superdesk-workspace/content/content.js
@@ -18,58 +18,59 @@
                 'type'
             ];
 
-        var scope = $scope;
+            var scope = $scope;
 
-        /**
-         * Create an item and start editing it
-         */
-        this.create = function(type) {
-            if (scope && scope.dirty){
-                scope.closeOpenNew(createItem, type);
-            } else {
-                createItem(type);
-            }
-        };
+            /**
+             * Create an item and start editing it
+             */
+            this.create = function(type) {
+                if (scope && scope.dirty){
+                    scope.closeOpenNew(createItem, type);
+                } else {
+                    createItem(type);
+                }
+            };
 
-        var createItem = function (type) {
-            var item = {type: type || 'text', version: 0};
-            api('archive').save(item).then(function() {
-                superdesk.intent('author', 'article', item);
-            });
-        };
+            var createItem = function (type) {
+                var item = {type: type || 'text', version: 0};
+                api('archive').save(item).then(function() {
+                    superdesk.intent('author', 'article', item);
+                });
+            };
 
-        this.createPackage = function createPackage(current_item) {
-            if (scope && scope.dirty){
-                scope.closeOpenNew(createPackageItem, current_item);
-            } else {
-                createPackageItem(current_item);
-            }
-        };
+            this.createPackage = function createPackage(current_item) {
+                if (scope && scope.dirty){
+                    scope.closeOpenNew(createPackageItem, current_item);
+                } else {
+                    createPackageItem(current_item);
+                }
+            };
 
-        var createPackageItem = function (current_item) {
-            if (current_item) {
-                superdesk.intent('create', 'package', {items: [current_item]});
-            } else {
-                superdesk.intent('create', 'package');
-            }
-        };
+            var createPackageItem = function (current_item) {
+                if (current_item) {
+                    superdesk.intent('create', 'package', {items: [current_item]});
+                } else {
+                    superdesk.intent('create', 'package');
+                }
+            };
 
-        this.createFromTemplate = function(template) {
-            if (scope && scope.dirty){
-                scope.closeOpenNew(createFromTemplateItem, template);
-            } else {
-                createFromTemplateItem(template);
-            }
-        };
+            this.createFromTemplate = function(template) {
+                if (scope && scope.dirty){
+                    scope.closeOpenNew(createFromTemplateItem, template);
+                } else {
+                    createFromTemplateItem(template);
+                }
+            };
 
-        var createFromTemplateItem = function (template) {
-            var item = _.pick(template, templateFields);
-            api('archive').save(item).then(function() {
-                return templates.addRecentTemplate(desks.activeDeskId, template._id);
-            })
-            .then(function() {
-                superdesk.intent('author', 'article', item);
-            });
+            var createFromTemplateItem = function (template) {
+                var item = _.pick(template, templateFields);
+                api('archive').save(item).then(function() {
+                    return templates.addRecentTemplate(desks.activeDeskId, template._id);
+                })
+                .then(function() {
+                    superdesk.intent('author', 'article', item);
+                });
+            };
         };
     }
 
