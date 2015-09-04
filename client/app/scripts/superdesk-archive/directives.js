e992297a58f8b9bfbe5cdcd81ed9e7fd0d96fa34
pavlovicnemanja92 <me@pavlovicnemanja.com>
2015-09-04 13:32:59 +0200

    fix(content): Fixed content scope overlapping

--- client/app/scripts/superdesk-archive/directives.js
+++ client/app/scripts/superdesk-archive/directives.js
@@ -512,6 +512,85 @@
             };
         })
 
+        .directive('sdContentResults', ['$location', 'preferencesService', 'packages', 'tags', 'asset',
+            function ($location, preferencesService, packages, tags, asset) {
+                var update = {
+                    'archive:view': {
+                        'allowed': [
+                            'mgrid',
+                            'compact'
+                        ],
+                        'category': 'archive',
+                        'view': 'mgrid',
+                        'default': 'mgrid',
+                        'label': 'Users archive view format',
+                        'type': 'string'
+                    }
+                };
+
+                return {
+                    require: '^sdSearchContainer',
+                    templateUrl: asset.templateUrl('superdesk-search/views/search-results.html'),
+                    link: function (scope, elem, attr, controller) {
+
+                        var GRID_VIEW = 'mgrid',
+                                LIST_VIEW = 'compact';
+
+                        var multiSelectable = (attr.multiSelectable === undefined) ? false : true;
+
+                        scope.flags = controller.flags;
+                        scope.selected = scope.selected || {};
+
+                        scope.preview = function preview(item) {
+                            if (multiSelectable) {
+                                if (_.findIndex(scope.selectedList, {_id: item._id}) === -1) {
+                                    scope.selectedList.push(item);
+                                } else {
+                                    _.remove(scope.selectedList, {_id: item._id});
+                                }
+                            }
+                            scope.selected.preview = item;
+                            $location.search('_id', item ? item._id : null);
+                        };
+
+                        scope.openLightbox = function openLightbox() {
+                            scope.selected.view = scope.selected.preview;
+                        };
+
+                        scope.closeLightbox = function closeLightbox() {
+                            scope.selected.view = null;
+                        };
+
+                        scope.openSingleItem = function (packageItem) {
+                            packages.fetchItem(packageItem).then(function (item) {
+                                scope.selected.view = item;
+                            });
+                        };
+
+                        scope.setview = setView;
+
+                        var savedView;
+                        preferencesService.get('archive:view').then(function (result) {
+                            savedView = result.view;
+                            scope.view = (!!savedView && savedView !== 'undefined') ? savedView : 'mgrid';
+                        });
+
+                        scope.$on('key:v', toggleView);
+
+                        function setView(view) {
+                            scope.view = view || 'mgrid';
+                            update['archive:view'].view = view || 'mgrid';
+                            preferencesService.update(update, 'archive:view');
+                        }
+
+                        function toggleView() {
+                            var nextView = scope.view === LIST_VIEW ? GRID_VIEW : LIST_VIEW;
+                            return setView(nextView);
+                        }
+                    }
+                };
+            }])
+
         .service('familyService', ['api', 'desks', function(api, desks) {
             this.fetchItems = function(familyId, excludeItem) {
                 var repo = 'archive';
