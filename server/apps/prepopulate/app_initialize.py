8f05780ce6a61e212a76a289d624a387c66c89b3
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2015-09-16 18:52:59 +1000

    Allow multiple index to be created per collection.

--- server/apps/prepopulate/app_initialize.py
+++ server/apps/prepopulate/app_initialize.py
@@ -12,17 +12,18 @@ App initialization information, maps resource name to the file containing the da
 and the index to be created for the resource.
 """
 __entities__ = {
-    'roles': ('roles.json', 'name', False),
-    'users': ('users.json', [('first_name', pymongo.ASCENDING),
-                             ('last_name', pymongo.DESCENDING)], False),
-    'desks': ('desks.json', 'incoming_stage', False),
-    'stages': ('stages.json', 'desk', False),
+    'roles': ('roles.json', ['name'], False),
+    'users': ('users.json', [[('first_name', pymongo.ASCENDING),
+                             ('last_name', pymongo.DESCENDING)],
+                             'username'], False),
+    'desks': ('desks.json', ['incoming_stage'], False),
+    'stages': ('stages.json', ['desk'], False),
     'groups': ('groups.json', '', False),
     'vocabularies': ('vocabularies.json', '', True),
     'validators': ('validators.json', '', True),
-    'content_templates': ('content_templates.json', 'template_name', False),
-    'published': (None, [('item_id', pymongo.ASCENDING),
-                         ('state', pymongo.ASCENDING)], False)
+    'content_templates': ('content_templates.json', ['template_name'], False),
+    'published': (None, [[('item_id', pymongo.ASCENDING),
+                         ('state', pymongo.ASCENDING)]], False)
 }
 
 
@@ -39,9 +40,8 @@ class AppInitializeWithDataCommand(superdesk.Command):
         superdesk.Option('--entity-name', '-n', dest='entity_name', default='')
     ]
 
-    def run(self, entity_name=None):
+    def run(self, entity_name=None, index_only='false'):
         self.logger.info('Starting data import')
-
         if entity_name:
             (file_name, index_params, do_patch) = __entities__[entity_name]
             self.import_file(entity_name, file_name, index_params, do_patch)
@@ -70,27 +70,33 @@ class AppInitializeWithDataCommand(superdesk.Command):
                 data = [app.data.mongo._mongotize(item, service.datasource) for item in json_data]
                 existing_data = []
                 existing = service.get_from_mongo(None, {})
+                update_data = True
                 if not do_patch and existing.count() > 0:
                     self.logger.info('Data already exists for {} none will be loaded'.format(entity_name))
-                    return
+                    update_data = False
                 elif do_patch and existing.count() > 0:
                     self.logger.info('Data already exists for {} it will be updated'.format(entity_name))
-                if do_patch:
-                    for item in existing:
-                        for loaded_item in data:
-                            if '_id' in loaded_item and loaded_item['_id'] == item['_id']:
-                                existing_data.append(loaded_item)
-                                data.remove(loaded_item)
-                if data:
-                    service.post(data)
-                if existing_data and do_patch:
-                    for item in existing_data:
-                        service.patch(item['_id'], item)
+
+                if update_data:
+                    if do_patch:
+                        for item in existing:
+                            for loaded_item in data:
+                                if '_id' in loaded_item and loaded_item['_id'] == item['_id']:
+                                    existing_data.append(loaded_item)
+                                    data.remove(loaded_item)
+
+                    if data:
+                        service.post(data)
+                    if existing_data and do_patch:
+                        for item in existing_data:
+                            service.patch(item['_id'], item)
+
                 self.logger.info('File {} imported successfully.'.format(file_name))
 
         if index_params:
-            app.data.mongo.pymongo(resource=entity_name).db[entity_name].create_index(index_params)
-            self.logger.info('Index for {} created successfully.'.format(entity_name))
+            for index in index_params:
+                index_name = app.data.mongo.pymongo(resource=entity_name).db[entity_name].create_index(index)
+                self.logger.info('Index: {} for collection {} created successfully.'.format(index_name, entity_name))
 
 
 superdesk.command('app:initialize_data', AppInitializeWithDataCommand())
