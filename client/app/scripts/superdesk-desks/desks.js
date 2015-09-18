27738bc1dbd50f91aa63069a12c8140cc5802f5d
pavlovicnemanja92 <me@pavlovicnemanja.com>
2015-09-18 11:15:54 +0200

    feat(setting): Improved functionality of desk list

--- client/app/scripts/superdesk-desks/desks.js
+++ client/app/scripts/superdesk-desks/desks.js
@@ -368,6 +368,7 @@
         //expecting $scope.desks to be defined
 
         $scope.modalActive = false;
+        $scope.numberOfUsers = 3;
         $scope.step = {
             current: null
         };
@@ -405,6 +406,14 @@
                     );
                 }
             );
+        };
+
+        $scope.getDeskStages = function(desk) {
+            return desks.deskStages[desk._id];
+        };
+
+        $scope.getDeskUsers = function (desk) {
+            return desks.deskMembers[desk._id];
         };
     }
 
