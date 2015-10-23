6cc97ace5973a44e1a81c416a2deeec6892ea144
Peter Lamut <peter.lamut@niteoweb.com>
2015-10-23 14:57:46 +1100

    Add Cancel button to the user priviliges settings form
    SD-2548

--- client/spec/helpers/user_prefs.js
+++ client/spec/helpers/user_prefs.js
@@ -10,6 +10,9 @@ var openUrl = require('./utils').open;
  */
 function UserPrefs() {
 
+    this.btnSave = $('.action-bar').element(by.buttonText('Save'));
+    this.btnCancel = $('.action-bar').element(by.buttonText('Cancel'));
+
     // the Preferences tab
     this.prefsTab = element.all(by.css('.nav-tabs button')).get(1);
 
@@ -17,7 +20,10 @@ function UserPrefs() {
     this.categoryCheckboxes = element.all(
         by.repeater('cat in categories')).all(by.css('[type="checkbox"]'));
 
-    this.btnSave = element.all(by.css('.action-bar > button')).get(0);
+    // the Privileges tab
+    this.privlTab = element.all(by.css('.nav-tabs button')).get(2);
+
+    this.privlCheckboxes = $$('.roles-list input[type="checkbox"]');
 
     this.navigateTo = function () {
         return openUrl('/#/profile');
