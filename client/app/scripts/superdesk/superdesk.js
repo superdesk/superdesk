54905eb51428dce683970696d2b2575c14dc8886
Edouard <edou4rd@gmail.com>
2015-11-02 12:51:36 +0100

    make sure there is a `superdesk.templates-cache`  module defined

--- client/app/scripts/superdesk/superdesk.js
+++ client/app/scripts/superdesk/superdesk.js
@@ -95,6 +95,13 @@ define([
                 return false;
             }
         }]);
-
+    // make sure there is templates module defined
+    try {
+        angular.module('superdesk.templates-cache');
+    } catch (e) {
+        angular.module('superdesk.templates-cache', []);
+    } finally {
+        modules.push('superdesk.templates-cache');
+    }
     return angular.module('superdesk', modules);
 });
