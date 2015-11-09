4909cd9cb3c96502d29f74bacd2bd138fe80ed5c
actionless <actionless.loveless@gmail.com>
2015-11-09 12:08:39 +0100

    feat(server: docker): read from env vars in fig wrapper

--- server/scripts/fig_wrapper.sh
+++ server/scripts/fig_wrapper.sh
@@ -2,23 +2,27 @@
 
 TIMEOUT=2
 
+MONGO_HOSTNAME=${MONGO_HOSTNAME:="mongodb"}
+REDIS_HOSTNAME=${REDIS_HOSTNAME:="redis"}
+ELASTIC_HOSTNAME=${ELASTIC_HOSTNAME:="elastic"}
+
 MONGO=0
 REDIS=0
 ELASTIC=0
 while [ $MONGO -eq 0 ] || [ $ELASTIC -eq 0 ] || [ $REDIS -eq 0 ]
 do
 	if [ $MONGO -eq 0 ] ; then
-		curl -s -m $TIMEOUT mongodb:27017 2>&1 > /dev/null &&
+		curl -s -m $TIMEOUT $MONGO_HOSTNAME:27017 2>&1 > /dev/null &&
 		MONGO=1 ||
 		echo "waiting for mongo..."
 	fi
 	if [ $REDIS -eq 0 ] ; then
-		curl -# -m $TIMEOUT redis:6379 2>&1 | grep "wrong number of arguments for 'get' command"  2>&1 > /dev/null &&
+		curl -# -m $TIMEOUT REDIS_HOSTNAME:6379 2>&1 | grep "wrong number of arguments for 'get' command"  2>&1 > /dev/null &&
 		REDIS=1 ||
 		echo "waiting for redis..."
 	fi
 	if [ $ELASTIC -eq 0 ] ; then
-		curl -s -m $TIMEOUT -o /dev/null elastic:9200  2>&1 > /dev/null &&
+		curl -s -m $TIMEOUT -o /dev/null ELASTIC_HOSTNAME:9200  2>&1 > /dev/null &&
 		ELASTIC=1 ||
 		echo "waiting for elastic..."
 	fi
