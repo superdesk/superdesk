f638b3b1a9be78e30ea7e1716aaef1960d79c96e
petr.jasek <petr.jasek@sourcefabric.org>
2015-11-12 16:58:21 +0100

    test(travis): set gunicorn timeout to 10s
    it should fail requests after 10s, so we should see where
    ui is stuck waiting for server

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -8,4 +8,9 @@ workers = multiprocessing.cpu_count()
 accesslog = '-'
 access_log_format = '%(r)s\nstatus=%(s)s time=%(T)ss bytes=%(b)s pid=%(p)s remote=%(h)s referer=%(f)s'
 
+if 'TRAVIS' in os.environ:
+    # a bit more constraint setup to make errors popup
+    workers = 3
+    timeout = 10
+
 reload = 'SUPERDESK_RELOAD' in os.environ
