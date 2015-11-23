dec0ee4f94b253763951d0926ece226dc86bb81e
petr.jasek <petr.jasek@sourcefabric.org>
2015-11-23 11:52:47 +0100

    fix(authoring): rename slugline to keyword
    and keywords to tags
    
    SDPA-30

--- client/app/scripts/translations.js
+++ client/app/scripts/translations.js
@@ -1,5 +1,14 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('en', {"CATEGORY":"SERVICE","SUBJECT":"TOPIC"});
+    gettextCatalog.setStrings('en', {
+        "CATEGORY":"SERVICE",
+        "Category": "Service",
+        "SUBJECT":"TOPIC",
+        "Subject": "Topic",
+        "SLUGLINE": "KEYWORD",
+        "Slugline": "Keyword",
+        "KEYWORDS": "TAGS",
+        "Keywords": "Tags"
+    });
 /* jshint +W100 */
 }]);
