866dc68b5f466a749c0042daf1d9a11364c19a90
actionless <actionless.loveless@gmail.com>
2015-11-30 15:34:42 +0100

    chore(scripts): backport prepopulation for unified container from master

--- scripts/docker-local-create-user.sh
+++ scripts/docker-local-create-user.sh
@@ -5,6 +5,13 @@ SCRIPT_DIR=$(readlink -e $(dirname "$0"))
 set -ue
 pip install -r $SCRIPT_DIR/../docker/requirements.txt
 cd $SCRIPT_DIR/../docker
-docker-compose -p sddemo -f ./docker-compose-prebuilt.yml run backend ./scripts/fig_wrapper.sh python3 manage.py app:initialize_data
-docker-compose -p sddemo -f ./docker-compose-prebuilt.yml run backend ./scripts/fig_wrapper.sh python3 manage.py users:create -u admin -p admin -e 'admin@example.com' --admin=true
-docker-compose -p sddemo -f ./docker-compose-prebuilt.yml run pubapi ./scripts/fig_wrapper.sh python3 content_api_manage.py app:prepopulate
+
+
+docker-compose -p sddemo -f ./docker-compose-prebuilt.yml run superdesk ./scripts/fig_wrapper.sh bash -c "\
+  python3 manage.py app:initialize_data ;\
+  echo '+++ sample data was prepopulated' ;\
+  python3 manage.py users:create -u admin -p admin -e 'admin@example.com' --admin=true ;\
+  echo '+++ new user has been created'"
+docker-compose -p sddemo -f ./docker-compose-prebuilt.yml run pubapi bash ./scripts/fig_wrapper.sh bash -c "\
+  python3 content_api_manage.py app:prepopulate ;\
+  echo '+++ public api was prepopulated'"
