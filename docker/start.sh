6dd5fef3db45f6e1c6aa94c1fafc478d52b62d82
actionless <actionless.loveless@gmail.com>
2015-12-02 10:16:17 +0100

    chore(docker: start.sh): fill embedly key

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /opt/superdesk/client/dist &&
-sed -i -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" index.html -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/" &&
+sed -i \
+ -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" \
+ -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
+ -e 's/"embedly":{"key":""}/"embedly":{"key":"'$EMBEDLY_KEY'"}/g' \
+ index.html &&
 nginx &
 
 cd /opt/superdesk &&
