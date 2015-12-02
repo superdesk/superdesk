220ae9edca5af160f45c98d2553f6e97633a1c2e
actionless <actionless.loveless@gmail.com>
2015-12-02 10:11:05 +0100

    Revert "docker script updated for template"
    This reverts commit 6996ecc643d3d83bc5e28636a2f4875309544957.

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /opt/superdesk/client/ && grunt template
+cd /opt/superdesk/client/dist &&
+sed -i -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" index.html -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/" &&
 nginx &
 
 cd /opt/superdesk &&
