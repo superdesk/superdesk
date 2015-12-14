39d7c7a56f4e9574cb49df4d0d3071750e763ff3
Petr <petr.jasek@sourcefabric.org>
2015-12-14 16:51:20 +0100

    fix(templates): fix template card display
    - fix time rendering
    - make dropdown fit into template card
    
    SD-3789 SD-3784

--- client/app/scripts/superdesk/datetime/datetime_spec.js
+++ client/app/scripts/superdesk/datetime/datetime_spec.js
@@ -11,4 +11,11 @@ define(['superdesk/datetime/datetime'], function() {
             expect($filter('reldate')(date)).toBe('a few seconds ago');
         }));
     });
+
+    describe('time filter', function() {
+        it('can convert date into time str', inject(function($filter) {
+            var date = new Date(2010, 10, 10, 8, 5, 35);
+            expect($filter('time')(date)).toBe('8:05');
+        }));
+    });
 });
