e5f9493a9ed689f8209997b21a511e33bafdcffa
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2015-12-15 13:13:13 +1100

    Fixing the nose tests for create index.

--- server/apps/prepopulate/app_initialization_test.py
+++ server/apps/prepopulate/app_initialization_test.py
@@ -57,8 +57,8 @@ class AppInitializeWithDataCommandTestCase(SuperdeskTestCase):
         self.assertTrue('username_1' in result)
         self.assertTrue('first_name_1_last_name_-1' in result)
         result = app.data.mongo.pymongo(resource='archive').db['archive'].index_information()
-        self.assertTrue('groups.refs.guid_1' in result)
-        self.assertTrue(result['groups.refs.guid_1']['sparse'])
+        self.assertTrue('groups.refs.residRef_1' in result)
+        self.assertTrue(result['groups.refs.residRef_1']['sparse'])
 
     def test_app_initialization_set_env_variables(self):
         def mock_env(variable, default):
