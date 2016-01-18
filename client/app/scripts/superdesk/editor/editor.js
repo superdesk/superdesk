882761757999cb40f2e77da69dbbfeeaa0bf98bb
Syed <sjunaid@aap.com.au>
2016-01-18 14:34:01 +1100

    [sd-3917] - (fix) - I should not be able to Ctrl-z to the original, actionable text when killing an item

--- client/app/scripts/superdesk/editor/editor.js
+++ client/app/scripts/superdesk/editor/editor.js
@@ -90,7 +90,6 @@ function HistoryStack(initialValue) {
     var stack = [];
     var selectionStack = [];
     var index = -1;
-
     /**
      * Add a new value to stack and remove all furhter redo values
      * so after manual change there is no way to redo.
@@ -117,6 +116,20 @@ function HistoryStack(initialValue) {
      */
     this.selectNext = function() {
         index = stack[index + 1] != null ? index + 1 : index;
+    };
+
+    /**
+     * Get current index position
+     */
+    this.getIndex = function() {
+        return index;
+    };
+
+    /**
+     * Set initial value
+     */
+    this.setInitialValue = function(value) {
+        initialValue = value;
     };
 
     /**
@@ -530,6 +543,12 @@ function EditorService(spellcheck, $rootScope, $timeout, $q) {
      * @param {Scope} scope
      */
     this.commitScope = function(scope) {
+        // In case of Kill action, don't undo back to initial value (i.e: original article text) - [ref: SD-3917]
+        // Set initial value to kill template text at the beginning of history.
+        if (scope.$parent.action === 'kill' && scope.history.getIndex() === -1) {
+            scope.history.setInitialValue(scope.model.$viewValue);
+        }
+
         var nodeValue = clean(scope.node).innerHTML;
         if (nodeValue !== scope.model.$viewValue) {
             scope.model.$setViewValue(nodeValue);
