0c9d95cb72e99a4dc00a67d3c5d559d19877cf50
Petr <petr.jasek@sourcefabric.org>
2016-01-22 14:03:21 +1100

    make use of new superdesk-core

--- server/app.py
+++ server/app.py
@@ -10,39 +10,15 @@
 # at https://www.sourcefabric.org/superdesk/license
 
 
-import os
 import settings
 from superdesk.factory import get_app as superdesk_app
 
 
-def get_app(config=None):
-    """App factory.
-
-    :param config: configuration that can override config from `settings.py`
-    :return: a new SuperdeskEve app instance
-    """
-    if config is None:
-        config = {}
-
-    config['APP_ABSPATH'] = os.path.abspath(os.path.dirname(__file__))
-
-    for key in dir(settings):
-        if key.isupper():
-            config.setdefault(key, getattr(settings, key))
-
+def get_app():
     media_storage = None
-    if config['AMAZON_CONTAINER_NAME']:
+    if getattr(settings, 'AMAZON_CONTAINER_NAME'):
         from superdesk.storage.amazon.amazon_media_storage import AmazonMediaStorage
         media_storage = AmazonMediaStorage
 
-    config['DOMAIN'] = {}
-
-    app = superdesk_app(config, media_storage)
+    app = superdesk_app(settings, media_storage)
     return app
-
-if __name__ == '__main__':
-    debug = True
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', '5000'))
-    app = get_app()
-    app.run(host=host, port=port, debug=debug, use_reloader=debug)
---
+++ server/pa/pa_img/pa_img_datalayer_tests.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014, 2015 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+from test_factory import SuperdeskTestCase
+from pa.pa_img.pa_img_datalayer import extract_params
+
+
+class PaImgDatalayer(SuperdeskTestCase):
+
+    def test_validate_query_all_first_succeeds(self):
+        query = 'all headline:(head one) caption:(capt) keywords:(key)'
+        names = ['headline', 'keywords']
+        result = extract_params(query, names)
+        self.assertDictEqual(result, {'q': 'all', 'headline': 'head one', 'keywords': 'key'},
+                             msg='Fail to parse text query all first')
+
+    def test_validate_query_all_middle_succeeds(self):
+        query = 'headline:(head one) all caption:(capt) keywords:(key)'
+        names = ['headline', 'keywords']
+        result = extract_params(query, names)
+        self.assertDictEqual(result, {'q': 'all', 'headline': 'head one', 'keywords': 'key'},
+                             msg='Fail to parse text query all middle')
+
+    def test_validate_query_all_end_succeeds(self):
+        query = 'headline:(head one) caption:(capt) keywords:(key) all'
+        names = ['headline', 'keywords']
+        result = extract_params(query, names)
+        self.assertDictEqual(result, {'q': 'all', 'headline': 'head one', 'keywords': 'key'},
+                             msg='Fail to parse text query all end')
