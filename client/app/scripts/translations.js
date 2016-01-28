8fe62e246b563592a21d7f34769689d315ff2852
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2016-01-28 16:14:58 +0200

    fix(language): Revert the translations and activate back the e2e test

--- client/app/scripts/translations.js
+++ client/app/scripts/translations.js
@@ -1,4 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
+    gettextCatalog.setStrings('de', {"My Profile":"DE - My Profile"});
 /* jshint +W100 */
 }]);
