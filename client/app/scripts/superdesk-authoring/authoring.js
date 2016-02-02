f8d3e3d93d16e153e137378d144a93816a24cdb7
eozan <erdemozanozbek@gmail.com>
2016-02-02 13:07:15 +0100

    feat(authoring): Preview view
    SDPA-182

--- client/app/scripts/superdesk-authoring/authoring.js
+++ client/app/scripts/superdesk-authoring/authoring.js
@@ -902,6 +902,7 @@
                     }
                 });
 
+                $scope.fullPreviewItem = null;
                 $scope.proofread = false;
                 $scope.referrerUrl = referrer.getReferrerUrl();
 
@@ -1232,6 +1233,14 @@
                 $scope.autosave = function(item) {
                     $scope.dirty = true;
                     return authoring.autosave(item);
+                };
+
+                $scope.openFullPreview = function(item) {
+                    $scope.fullPreviewItem = item;
+                };
+
+                $scope.closeFullPreview = function() {
+                    $scope.fullPreviewItem = null;
                 };
 
                 function refreshItem() {
