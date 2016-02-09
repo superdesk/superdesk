bd8179229601ccfe8fc8f86c63179a3686e9e46f
actionless <actionless.loveless@gmail.com>
2016-02-09 14:29:16 +0100

    hotfix(docker: dev): npm link client-core

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+cd /opt/superdesk/client-core &&
+npm link &&
 cd /opt/superdesk/client &&
+npm link superdesk-core &&
 grunt --force server --server='http://localhost:5000/api' --ws='ws://localhost:5100' &
 
 cd /opt/superdesk &&
