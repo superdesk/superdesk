8d90fc768ef1ccd4547afd189c30b07daa10c2d1
Petr <petr.jasek@sourcefabric.org>
2016-02-22 16:31:27 +0100

    refactor: use client-core

--- client/app/scripts/superdesk/elastic/elastic.js
+++
@@ -1,9 +0,0 @@
-define([
-    'angular',
-    './es'
-], function(angular, ElasticSearchService) {
-    'use strict';
-
-    return angular.module('superdesk.elastic', [])
-        .service('es', ElasticSearchService);
-});
