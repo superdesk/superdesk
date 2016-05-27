7d7fba6bdf58bb461fab301d5ae324b213170a01
Ioan v. Pocol <ioan.pocol@sourcefabric.org>
2016-05-27 14:12:27 +0300

    feat(tansa): Configure the proxy for tansa server
    Created by Petr

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,3 +1,32 @@
 'use strict';
 
-module.exports = require('superdesk-core/Gruntfile');
+module.exports = function(grunt) {
+    require('superdesk-core/Gruntfile')(grunt);
+
+    grunt.loadNpmTasks('grunt-connect-proxy');
+
+    var middlewareConfig = grunt.config('connect.test.options.middleware');
+    grunt.config('connect.test.options.middleware', function(connect, options, middlewares) {
+        middlewares.push(require('grunt-connect-proxy/lib/utils').proxyRequest);
+        middlewareConfig(connect, options, middlewares);
+        return middlewares;
+    });
+
+    grunt.config('connect.proxies', [
+        {
+            context: '/tansaclient',
+            host: 'd02.tansademo.net',
+            changeOrigin: true,
+            port: 8080
+        }
+    ]);
+
+    grunt.registerTask('server', [
+        'clean',
+        'style',
+        'template:test',
+        'configureProxies:test',
+        'connect:test',
+        'watch'
+    ]);
+};
