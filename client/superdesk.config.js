9e0f3f8ca85a5257bc4ead7e0fcab94710d760e5
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-07-28 09:46:19 +0100

    added configuration file

---
+++ client/superdesk.config.js
@@ -0,0 +1,12 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+module.exports = function(grunt) {
+    return {
+        defaultRoute: '/workspace/personal',
+        localTranslations: true
+    };
+};
