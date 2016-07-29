9ca348e9057d6497ed4cade721bd9b3205982c49
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-07-29 10:39:09 +0100

    moved translations to config and deleted unused files

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -1,6 +1,6 @@
 /**
  * This is the default configuration file for the Superdesk application. By default,
- * the app will use the file with the name "superdesk.config.js" found in the current
+ * the app will use the file with the name 'superdesk.config.js' found in the current
  * working directory, but other files may also be specified using relative paths with
  * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
  */
@@ -30,6 +30,23 @@ module.exports = function(grunt) {
 
         ui: {
             italicAbstract: true
+        },
+
+        langOverride: {
+            'de': {'My Profile':'DE - My Profile'},
+            'en': {
+                'CATEGORY':'SERVICE',
+                'Category': 'Service',
+                'SUBJECT':'TOPIC',
+                'Subject': 'Topic',
+                'SLUGLINE': 'KEYWORD',
+                'Slugline': 'Keyword',
+                'KEYWORDS': 'TAGS',
+                'Keywords': 'Tags',
+                'URGENCY': 'RANKING',
+                'Urgency': 'Ranking',
+                '\'SLUGLINE is a required field\'': 'Keyword is a required field'
+            }
         }
     };
 };
