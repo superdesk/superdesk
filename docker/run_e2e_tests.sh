49a1357eb8433ffa3e9c03f2a688fab8bc0c04fe
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2016-08-16 12:25:48 +0100

    removed bower related stuff, extra .gitignore file and update protractor file definition

--- docker/run_e2e_tests.sh
+++ docker/run_e2e_tests.sh
@@ -9,7 +9,7 @@ echo 'done.'
 npm install --unsafe-perm=true &&
 ./node_modules/.bin/webdriver-manager update &&
 xvfb-run --server-args="-screen 0, 1920x1080x24" --auto-servernum \
-./node_modules/.bin/protractor protractor-conf.js \
+./node_modules/.bin/protractor protractor.conf.js \
   --stackTrace --verbose \
   --baseUrl 'http://127.0.0.1' \
   --params.baseBackendUrl 'http://127.0.0.1/api' \
