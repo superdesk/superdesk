5f474dcdea9429ee7a5fff7ca9339aaf8f2e5e3e
Loïc Nogues <nogues.loic@gmail.com>
2016-09-16 11:00:01 +0200

    Added e2e docker-compose specific configuration

---
+++ docker/start-e2e.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#cd /opt/superdesk &&
+#bash ./scripts/fig_wrapper.sh honcho start
+
+cat >/tmp/testenv <<EOF
+SUPERDESK_TESTING=true
+
+EOF
+
+cd /opt/superdesk/client-core &&
+npm link &&
+cd /opt/superdesk/client &&
+npm link superdesk-core &&
+grunt --force server --server='http://localhost:5000/api' --ws='ws://localhost:5100' &
+
+cd /opt/superdesk &&
+bash ./scripts/fig_wrapper.sh honcho -e /tmp/testenv start &&
+rm -f /tmp/testenv
