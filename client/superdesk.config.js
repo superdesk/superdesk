f89867b7f2268c263e14265a33ec94d901160401
Mark Lewis <marklewis@Marks-MacBook-Pro.local>
2016-10-10 12:45:59 +0200

    fix(client): update client config to include external superdesk-planning app

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
     return {
         apps: ['superdesk-planning'],
         defaultRoute: '/workspace/personal',
+        apps: ['superdesk-planning'],
         requiredMediaMetadata: ['headline', 'description_text', 'alt_text']
     };
 };
