2022517ec63f0a6b6dda9c97d87fd21682a8547e
Mark Lewis <marklewis@Marks-MacBook-Pro.local>
2016-10-25 12:24:08 +0200

    SDESK-200: FILTERS in advanced search do not work for Search Providers

--- server/ntb/scanpix/scanpix_datalayer.py
+++ server/ntb/scanpix/scanpix_datalayer.py
@@ -90,11 +90,7 @@ class ScanpixDatalayer(DataLayer):
         """
         url = self._app.config['SCANPIX_SEARCH_URL'] + '/search'
         data = {
-            'mainGroup': 'any',
-            'archived': {
-                'max': '',
-                'min': ''
-            }
+            'mainGroup': 'any'
         }
 
         if 'query' in req['query']['filtered']:
@@ -160,12 +156,16 @@ class ScanpixDatalayer(DataLayer):
                 # if there is a special start and no end it's one of the date buttons
                 if start and not end:
                     if start == 'now-24H':
-                        data['timeLimit'] = 'last24 '
+                        data['timeLimit'] = 'last24'
                     if start == 'now-1w':
-                        data['timeLimit'] = 'lastweek '
+                        data['timeLimit'] = 'lastweek'
                     if start == 'now-1M':
-                        data['timeLimit'] = 'lastmonth '
+                        data['timeLimit'] = 'lastmonth'
                 elif start or end:
+                    data['archived'] = {
+                        'min': '',
+                        'max': ''
+                    }
                     if start:
                         data['archived']['min'] = start
                     if end:
@@ -180,6 +180,7 @@ class ScanpixDatalayer(DataLayer):
         offset, limit = int(req.get('from', '0')), max(10, int(req.get('size', '25')))
         data['offset'] = offset
         data['showNumResults'] = limit
+        logger.info('MARK GOING FOR THIS url=%s data=%s' % (url, data))
         r = self._request(url, data)
         hits = self._parse_hits(r.json())
         return ElasticCursor(docs=hits['docs'], hits={'hits': hits})
