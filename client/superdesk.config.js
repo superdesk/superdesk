f32bd7e89d44e1370b8a9fdeb70e9f4208c86344
Petr <petr.jasek@sourcefabric.org>
2016-12-01 15:27:47 +0100

    enable publisher

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
     return {
         defaultRoute: '/workspace/personal',
         requiredMediaMetadata: ['headline', 'description_text', 'alt_text'],
-        features: {noTakes: 1}
+        features: {noTakes: 1},
+        publisher: {
+            protocol: 'http',
+            tenant: 'default',
+            domain: 'master.s-lab.sourcefabric.org',
+            base: 'api/v1'
+        }
     };
 };
