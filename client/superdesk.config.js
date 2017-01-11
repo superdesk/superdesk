d8d55811e6138b59e4cd9c3d752a991a59e3b30b
Rafał Muszyński <rmuszynski1@gmail.com>
2017-01-11 15:30:48 +0100

    fallback to localhost by default

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         publisher: {
             protocol: 'https',
             tenant: 'default',
-            domain: process.env.PUBLISHER_API_DOMAIN || '',
+            domain: process.env.PUBLISHER_API_DOMAIN || 'localhost',
             base: 'api/v1'
         }
     };
