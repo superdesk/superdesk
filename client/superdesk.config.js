a3d65ace1fd18fa87f8b43f826793caf8759bc72
Petr Jašek <jasekpetr@gmail.com>
2017-02-09 14:37:01 +0100

    Update superdesk.config.js
    update media validation specs

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -1,13 +1,21 @@
 module.exports = function(grunt) {
     return {
         defaultRoute: '/workspace/personal',
-        requiredMediaMetadata: ['headline', 'description_text', 'alt_text'],
         features: {noTakes: 1},
         publisher: {
             protocol: 'https',
             tenant: 'default',
             domain: process.env.PUBLISHER_API_DOMAIN || 'localhost',
             base: 'api/v1'
+        },
+        validatorMediaMetadata: {
+            headline: {required: true},
+            alt_text: {required: true},
+            description_text: {required: true},
+            copyrightholder: {required: false},
+            byline: {required: false},
+            usageterms: {required: false},
+            copyrightnotice: {required: false}
         }
     };
 };
