7cd0918d4aff8e82f02c8becd3b4785a2861f803
Petr Jasek <jasekpetr@gmail.com>
2017-02-28 14:04:20 +0100

    implement ansa subjectcodes
    [skip ci]

---
+++ server/ansa/subjects.py
@@ -0,0 +1,14 @@
+
+import os
+from flask import json
+from datetime import datetime
+
+
+def init_app(app):
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(dirname, 'subject_codes_it.json')
+    with open(filename, 'r') as f:
+        codes = json.load(f)
+    last_modified = datetime(2017, 2, 27)
+    app.subjects.subjects.clear()
+    app.subjects.register(codes, last_modified)
--- server/settings.py
+++ server/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'ansa.analysis',
     'ansa.search',
     'ansa.parser',
+    'ansa.subjects',
 ]
 
 
