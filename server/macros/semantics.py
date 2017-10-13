9df3a0274bf295358bcd2cce7a722690f15a2f32
Petr Jasek <jasekpetr@gmail.com>
2017-10-13 17:33:30 +0200

    make semantics macro available in ui

--- server/macros/semantics.py
+++ server/macros/semantics.py
@@ -35,5 +35,5 @@ def populate(item, **kwargs):
 name = 'populate_semantics'
 label = 'Populate Semantics'
 callback = populate
-access_type = 'backend'
+access_type = 'frontend'
 action_type = 'direct'
