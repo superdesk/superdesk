0421e0df5669b5211061e2b8dd3e84ee919bdcc0
Ioan-Vasile Pocol <ivp@ivp-2.local>
2019-03-06 15:35:40 +0200

    fix(macro): Add from and to language info on translate macro (SDANSA-238)

--- server/ansa/macros/translate_text.py
+++ server/ansa/macros/translate_text.py
@@ -28,6 +28,13 @@ def translate(text='', **kwargs):
 def translate_text_macro(item, **kwargs):
     lang = 'en' if item.get('language', 'en') == 'en' else 'it'
     target = 'it' if lang == 'en' else 'en'
+
+    lang = kwargs.get('from_language', lang)
+    target = kwargs.get('to_language', target)
+
+    item['headline'] = process_html(item.get('headline', ''), translate, lang=lang, target=target)
+    item['slugline'] = process_html(item.get('slugline', ''), translate, lang=lang, target=target)
+    item['abstract'] = process_html(item.get('abstract', ''), translate, lang=lang, target=target)
     item['body_html'] = process_html(item.get('body_html', ''), translate, lang=lang, target=target)
 
     return item
@@ -39,3 +46,5 @@ callback = translate_text_macro
 access_type = 'frontend'
 action_type = 'direct'
 replace_type = 'simple-replace'
+from_languages = ['it', 'en', 'es', 'pt', 'de', 'ar']
+to_languages = ['it', 'en', 'es', 'pt', 'de', 'ar']
