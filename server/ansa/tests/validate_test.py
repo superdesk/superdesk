b2fb8ec9d7f7997176e0e4a0f8b7d38144db794e
Petr Jasek <jasekpetr@gmail.com>
2019-07-01 13:19:34 +0200

    implement custom validation based on products
    SDANSA-298

---
+++ server/ansa/tests/validate_test.py
@@ -0,0 +1,44 @@
+
+from superdesk.tests import TestCase
+from ansa.validate import validate, MASK_FIELD
+
+
+class ValidateTestCase(TestCase):
+
+    def test_product_based_validation(self):
+        self.app.data.insert('vocabularies', [{
+            '_id': 'products',
+            'items': [
+                {'qcode': 'foo', 'name': 'Foo', MASK_FIELD: '000011111'},
+                {'qcode': 'bar', 'name': 'Bar', MASK_FIELD: '111110000'},
+                {'qcode': 'baz', 'name': 'Baz'},
+            ],
+        }])
+        item = {
+            'subject': [
+                {
+                    'qcode': 'foo',
+                    'scheme': 'products',
+                },
+                {
+                    'qcode': 'bar',
+                    'scheme': 'products',
+                },
+            ],
+        }
+        fields = {}
+        response = []
+        validate(self, item, response, fields)
+        self.assertIn('Headline is required', response)
+        self.assertIn('Subtitle is required', response)
+        self.assertIn('Short Title is required', response)
+        self.assertIn('Subject is required', response)
+        self.assertIn('Featured photo is required', response)
+        self.assertIn('Photo gallery is required', response)
+        self.assertEqual(6, len(response))
+
+        response = []
+        item['body_html'] = '<p>{}</p>'.format('x' * 1000)
+        validate(self, item, response, fields)
+        self.assertIn('Body is longer than 512 characters', response)
+        self.assertEqual(7, len(response))
---
+++ server/ansa/validate.py
@@ -0,0 +1,81 @@
+
+import superdesk
+
+from enum import IntEnum
+from superdesk.text_utils import get_char_count
+from superdesk.signals import item_validate
+
+MASK_FIELD = 'output_code'
+
+
+class Validators(IntEnum):
+    HEADLINE_REQUIRED = 0
+    SHORTTITLE_REQUIRED = 1
+    SUBTITLE_REQUIRED = 2
+    SUBJECT_REQUIRED = 3
+    BODY_LENGTH_512 = 4
+    BODY_LENGTH_6400 = 5
+    BODY_LENGTH_1712 = 6
+    FEATURED_REQUIRED = 7
+    GALLERY_REQUIRED = 8
+
+
+def get_active_mask(products):
+    mask = {}
+    if products:
+        cv = superdesk.get_resource_service('vocabularies').find_one(req=None, _id='products')
+        codes = {product['qcode']: 1 for product in products}
+        if cv and cv.get('items'):
+            for item in cv['items']:
+                if codes.get(item.get('qcode')) and item.get(MASK_FIELD):
+                    for i in range(9):
+                        if item[MASK_FIELD][i] == '1':
+                            mask[i] = True
+    return mask
+
+
+def validate(sender, item, response, error_fields, **kwargs):
+    products = [subject for subject in item.get('subject', []) if subject.get('scheme') == 'products']
+    mask = get_active_mask(products)
+    extra = item.get('extra', {})
+    length = get_char_count(item.get('body_html') or '<p></p>')
+
+    if mask.get(Validators.HEADLINE_REQUIRED):
+        if not item.get('headline'):
+            response.append('Headline is required')
+
+    if mask.get(Validators.SHORTTITLE_REQUIRED):
+        if not extra.get('shorttitle'):
+            response.append('Short Title is required')
+
+    if mask.get(Validators.SUBTITLE_REQUIRED):
+        if not extra.get('subtitle'):
+            response.append('Subtitle is required')
+
+    if mask.get(Validators.SUBJECT_REQUIRED):
+        subjects = [subject for subject in item.get('subject', []) if subject.get('scheme') is None]
+        if not len(subjects):
+            response.append('Subject is required')
+
+    if mask.get(Validators.BODY_LENGTH_512) and length > 512:
+        response.append('Body is longer than 512 characters')
+    elif mask.get(Validators.BODY_LENGTH_1712) and length > 1712:
+        response.append('Body is longer than 1712 characters')
+    elif mask.get(Validators.BODY_LENGTH_6400) and length > 6400:
+        response.append('Body is longer than 6400 characters')
+
+    if mask.get(Validators.FEATURED_REQUIRED):
+        if not item.get('featuremedia'):
+            response.append('Featured photo is required')
+
+    if mask.get(Validators.GALLERY_REQUIRED):
+        has_gallery_item = False
+        for key, val in item.get('associations', {}).items():
+            if key.startswith('photoGallery') and val:
+                has_gallery_item = True
+        if not has_gallery_item:
+            response.append('Photo gallery is required')
+
+
+def init_app(app):
+    item_validate.connect(validate)
