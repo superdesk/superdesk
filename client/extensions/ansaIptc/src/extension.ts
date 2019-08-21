5a45199b52d646750d92dd0d93f951e9cf6e8e8d
Petr <petr.jasek@sourcefabric.org>
2019-08-21 11:30:52 +0200

    parse photo category from picture
    SDANSA-318

--- client/extensions/ansaIptc/src/extension.ts
+++ client/extensions/ansaIptc/src/extension.ts
@@ -1,16 +1,26 @@
 import {IExtension, IExtensionActivationResult, ISuperdesk} from 'superdesk-api';
 
+const PHOTO_CATEGORIES_ID = 'PhotoCategories';
+
 const extension: IExtension = {
     activate: (superdesk: ISuperdesk) => {
         const result: IExtensionActivationResult = {
             contributions: {
                 iptcMapping: (data, item) => Promise.all([
                     superdesk.entities.vocabulary.getIptcSubjects(),
-                ]).then(([subjects]) =>
+                    superdesk.entities.vocabulary.getVocabulary(PHOTO_CATEGORIES_ID),
+                ]).then(([subjects, categories]) =>
                     Object.assign(item, {
-                        subject: data.SubjectReference != null ?
-                            subjects.filter((subj) => subj.qcode === data.SubjectReference) :
-                            [],
+                        usageterms: data.CopyrightNotice,
+                        subject: (item.subject || []).concat(
+                            data.SubjectReference != null ?
+                                subjects.filter((subj) => subj.qcode === data.SubjectReference) :
+                                [],
+                            data.Category != null ?
+                                categories.filter((cat) => cat.name === data.Category)
+                                    .map((cat) => ({name: cat.name, qcode: cat.qcode, scheme: PHOTO_CATEGORIES_ID})) :
+                                [],
+                        ),
                         extra: {
                             city: data.City,
                             nation: data['Country-PrimaryLocationName'],
