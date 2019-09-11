d6ad12d982a3bbc8bcc2c53654bea5f7f0b08ea6
Mark Pittaway <mark.pittaway@gmail.com>
2019-09-12 08:59:40 +1000

    fix(superdesk.config) Add importApps for Planning

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -7,6 +7,7 @@
 module.exports = function(grunt) {
     return {
         apps: ['superdesk-planning'],
+        importApps: ['superdesk-planning'],
         defaultRoute: '/workspace/personal',
         validatorMediaMetadata: {
             headline: {
