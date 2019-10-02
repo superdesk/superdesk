cf1448bb7cf2f5378a83fee80f43f0c7d7dd5472
Tomas Kikutis <t.kikutis@gmail.com>
2019-10-02 11:23:15 +0200

    left side toolbar privileges

---
+++ server/ansa/__init__.py
@@ -0,0 +1,22 @@
+import superdesk
+
+def init_app(app):
+    superdesk.privilege(
+        name='ansa_metasearch',
+        label='ANSA - Metasearch',
+        decsription=''
+    )
+
+    superdesk.privilege(
+        name='ansa_live_assistant',
+        label='ANSA - live assistant',
+        decsription=''
+    )
+
+    superdesk.privilege(
+        name='ansa_ai_news',
+        label='ANSA - ai news',
+        decsription=''
+    )
+
+   
