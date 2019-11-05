19112118cad3f69cd1f244428a07dac0c62061e7
Petr Jasek <jasekpetr@gmail.com>
2019-11-05 13:08:07 +0100

    change format width macro to 64 characters

--- server/ansa/macros/format_text_width.py
+++ server/ansa/macros/format_text_width.py
@@ -1,19 +1,20 @@
 from .process_html import process_html
 
 """
-    Format text to 60 chars macro will transform the text from body to have max 60 chars width
+Format text to 60 chars macro will transform the text from body to have predefined width
 """
+
+WIDTH = 64
 
 
 def split_text(text='', **kwargs):
-    width = 60
     new_text = ''
     space = ''
     length = 0
 
     words = text.split(' ')
     for word in words:
-        if length + len(word) > width or length == 0 and len(word) > width:
+        if length + len(word) > WIDTH or length == 0 and len(word) > WIDTH:
             length = 0
             new_text = new_text + '\n'
             space = ''
@@ -31,8 +32,8 @@ def format_text_macro(item, **kwargs):
     return item
 
 
-name = 'Format text to 60 chars'
-label = 'Format text to 60 chars'
+name = 'Format text to {} chars'.format(WIDTH)
+label = 'Format text to {} chars'.format(WIDTH)
 callback = format_text_macro
 access_type = 'frontend'
 action_type = 'direct'
