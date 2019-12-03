43554d97b48b2a0cf1ef5ae115a5f921914b44bb
Tomas Kikutis <t.kikutis@gmail.com>
2019-12-03 14:59:12 +0100

    enable date time field extension

--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 import {startApp} from 'superdesk-core/scripts/index';
 import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
 import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
+import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, markForUserExtension],
+        [annotationsLibraryExtension, markForUserExtension, datetimeFieldExtension],
         {},
     );
 });
