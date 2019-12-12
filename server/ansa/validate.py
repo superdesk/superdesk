cfcdcbefb276764f44c84bb151fdd792b355d9d1
Petr Jasek <jasekpetr@gmail.com>
2019-12-12 14:01:47 +0100

    fix headline validate for updates

--- server/ansa/validate.py
+++ server/ansa/validate.py
@@ -1,4 +1,3 @@
-
 import re
 import superdesk
 
@@ -7,7 +6,7 @@ from superdesk.text_utils import get_char_count
 from superdesk.signals import item_validate
 from ansa.constants import GALLERY
 
-MASK_FIELD = 'output_code'
+MASK_FIELD = "output_code"
 
 
 class Validators(IntEnum):
@@ -22,81 +21,109 @@ class Validators(IntEnum):
     GALLERY_REQUIRED = 8
 
 
-class Errors():
-    AFP_IMAGE_USAGE = 'AFP images could not be used'
+class Errors:
+    AFP_IMAGE_USAGE = "AFP images could not be used"
 
 
 def get_active_mask(products):
     mask = {}
     if products:
-        cv = superdesk.get_resource_service('vocabularies').find_one(req=None, _id='products')
-        codes = {product['qcode']: 1 for product in products}
-        if cv and cv.get('items'):
-            for item in cv['items']:
-                if codes.get(item.get('qcode')) and item.get(MASK_FIELD) and len(str(item[MASK_FIELD])) == 9:
+        cv = superdesk.get_resource_service("vocabularies").find_one(
+            req=None, _id="products"
+        )
+        codes = {product["qcode"]: 1 for product in products}
+        if cv and cv.get("items"):
+            for item in cv["items"]:
+                if (
+                    codes.get(item.get("qcode"))
+                    and item.get(MASK_FIELD)
+                    and len(str(item[MASK_FIELD])) == 9
+                ):
                     value = str(item[MASK_FIELD])
                     for i in range(9):
-                        if value[i] == '1':
+                        if value[i] == "1":
                             mask[i] = True
     return mask
 
 
 def validate(sender, item, response, error_fields, **kwargs):
-    if item.get('auto_publish'):
+    if item.get("auto_publish"):
         for key in set(error_fields.keys()):
             error_fields.pop(key)
         response.clear()
         return
 
-    products = [subject for subject in item.get('subject', []) if subject.get('scheme') == 'products']
+    products = [
+        subject
+        for subject in item.get("subject", [])
+        if subject.get("scheme") == "products"
+    ]
     mask = get_active_mask(products)
-    extra = item.get('extra', {})
-    length = get_char_count(item.get('body_html') or '<p></p>')
+    extra = item.get("extra", {})
+    length = get_char_count(item.get("body_html") or "<p></p>")
 
     # allow publishing of headline with update info (1,2,..) over limit
-    if item.get('headline') and re.search(r'\([0-9]+\)$', item['headline']) and len(item['headline']) <= 64:
-        response.remove('HEADLINE is too long')
-        error_fields.pop('headline', None)
+    if (
+        item.get("headline")
+        and re.search(r"\([0-9]+\)$", item["headline"])
+        and len(item["headline"]) <= 64
+        and "HEADLINE is too long" in response
+    ):
+        response.remove("HEADLINE is too long")
+        error_fields.pop("headline", None)
 
     if mask.get(Validators.HEADLINE_REQUIRED):
-        if not item.get('headline'):
-            response.append('Headline is required')
+        if not item.get("headline"):
+            response.append("Headline is required")
 
     if mask.get(Validators.SHORTTITLE_REQUIRED):
-        if not extra.get('shorttitle'):
-            response.append('Short Title is required')
+        if not extra.get("shorttitle"):
+            response.append("Short Title is required")
 
     if mask.get(Validators.SUBTITLE_REQUIRED):
-        if not extra.get('subtitle'):
-            response.append('Subtitle is required')
+        if not extra.get("subtitle"):
+            response.append("Subtitle is required")
 
     if mask.get(Validators.SUBJECT_REQUIRED):
-        subjects = [subject for subject in item.get('subject', []) if subject.get('scheme') is None]
+        subjects = [
+            subject
+            for subject in item.get("subject", [])
+            if subject.get("scheme") is None
+        ]
         if not len(subjects):
-            response.append('Subject is required')
+            response.append("Subject is required")
 
     if mask.get(Validators.BODY_LENGTH_512) and length > 512:
-        response.append('Body is longer than 512 characters')
+        response.append("Body is longer than 512 characters")
     elif mask.get(Validators.BODY_LENGTH_2224) and length > 2224:
-        response.append('Body is longer than 2224 characters')
+        response.append("Body is longer than 2224 characters")
     elif mask.get(Validators.BODY_LENGTH_6400) and length > 6400:
-        response.append('Body is longer than 6400 characters')
+        response.append("Body is longer than 6400 characters")
 
-    associations = item.get('associations') or {}
-    pictures = [val for val in associations.values() if val and val.get('type') == 'picture']
-    gallery = [val for key, val in associations.items()
-               if val and val.get('type') == 'picture' and key.startswith(GALLERY)]
+    associations = item.get("associations") or {}
+    pictures = [
+        val for val in associations.values() if val and val.get("type") == "picture"
+    ]
+    gallery = [
+        val
+        for key, val in associations.items()
+        if val and val.get("type") == "picture" and key.startswith(GALLERY)
+    ]
 
     if mask.get(Validators.FEATURED_REQUIRED):
         if not pictures:
-            response.append('Photo is required')
+            response.append("Photo is required")
 
     if mask.get(Validators.GALLERY_REQUIRED):
         if not gallery:
-            response.append('Photo gallery is required')
+            response.append("Photo gallery is required")
 
     for picture in pictures:
-        if picture.get('extra') and picture['extra'].get('supplier') and picture['extra']['supplier'].lower() == 'afp':
+        if (
+            picture.get("extra")
+            and picture["extra"].get("supplier")
+            and picture["extra"]["supplier"].lower() == "afp"
+        ):
             response.append(Errors.AFP_IMAGE_USAGE)
             break
 
