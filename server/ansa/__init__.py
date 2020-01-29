541c3047a794312df0bf324a620b4d359578e2f9
Petr <petr.jasek@sourcefabric.org>
2020-01-29 16:23:34 +0100

    set product required for picture
    but set default product when picture is published as featured

--- server/ansa/__init__.py
+++ server/ansa/__init__.py
@@ -1,5 +1,32 @@
 import superdesk
 
+from .constants import PRODUCTS_ID
+
+
+def on_update(resource, updates, original):
+    if resource == 'archive_publish':
+        on_publish(updates, original)
+        return
+
+
+def on_publish(updates, original):
+    associations = updates.get('associations')
+    if not associations:
+        return
+    for key, item in associations.items():
+        if not item.get('type') == 'picture':
+            continue
+        subject = item.setdefault('subject', [])
+        product = next((subj for subj in subject if subj.get('scheme') == PRODUCTS_ID), None)
+        if not product:
+            subject.append({
+                'name': 'Browser Allegati',
+                'qcode': '0050000100002',
+                'scheme': PRODUCTS_ID,
+            })
+        item['auto_publish'] = True  # toggles auto validation
+        original.setdefault('associations', {})[key].update(item)
+
 
 def init_app(app):
     superdesk.privilege(
@@ -19,3 +46,5 @@ def init_app(app):
         label='ANSA - ai news',
         decsription=''
     )
+
+    app.on_update += on_update
