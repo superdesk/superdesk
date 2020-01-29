143284aa306389b7958a32c493d3becc5bb1b1ee
Petr <petr.jasek@sourcefabric.org>
2020-01-29 17:56:52 +0100

    don't set auto_publish for associated items

--- server/ansa/__init__.py
+++ server/ansa/__init__.py
@@ -1,5 +1,8 @@
 import superdesk
 
+from superdesk.signals import item_publish
+from superdesk.metadata.item import PUBLISH_STATES
+
 from .constants import PRODUCTS_ID
 
 
@@ -10,11 +13,14 @@ def on_update(resource, updates, original):
 
 
 def on_publish(updates, original):
+    """When item is published with associated pictures
+    make sure these have product assigned.
+    """
     associations = updates.get('associations')
     if not associations:
         return
     for key, item in associations.items():
-        if not item.get('type') == 'picture':
+        if not item.get('type') == 'picture' or item.get('state') in PUBLISH_STATES:
             continue
         subject = item.setdefault('subject', [])
         product = next((subj for subj in subject if subj.get('scheme') == PRODUCTS_ID), None)
@@ -24,8 +30,16 @@ def on_publish(updates, original):
                 'qcode': '0050000100002',
                 'scheme': PRODUCTS_ID,
             })
-        item['auto_publish'] = True  # toggles auto validation
         original.setdefault('associations', {})[key].update(item)
+
+
+def udpate_sign_off(sender, item, **kwargs):
+    """When item is auto published add -RED to sign_off."""
+    if item.get('auto_publish'):
+        if item.get('sign_off'):
+            item['sign_off'] += '-RED'
+        else:
+            item['sign_off'] = 'RED'
 
 
 def init_app(app):
@@ -48,3 +62,4 @@ def init_app(app):
     )
 
     app.on_update += on_update
+    item_publish.connect(udpate_sign_off)
