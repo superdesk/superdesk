a55206440ae12c72d44c60d7cac4b237d25aa215
Petr <petr.jasek@sourcefabric.org>
2020-03-11 20:23:43 +0100

    fix unlink on publish via stage auto publish
    SDANSA-385

--- server/ansa/__init__.py
+++ server/ansa/__init__.py
@@ -18,34 +18,6 @@ def on_update(resource, updates, original):
 
 def on_publish(updates, original):
     assign_product_to_pictures(updates, original)
-    unlink_update_on_auto_publish(updates, original)
-
-
-def unlink_update_on_auto_publish(updates, original):
-    """
-    Unlink auto published item when original is not published yet.
-
-    SDANSA-385
-    """
-    if not updates.get('auto_publish'):
-        return
-
-    updated = original.copy()
-    updated.update(updates)
-
-    if updated.get('rewrite_of'):
-        main = superdesk.get_resource_service('archive').find_one(req=None, _id=updated['rewrite_of'])
-        if main and main.get('state') not in PUBLISH_STATES:
-            # unlink published item
-            updates.update({
-                'rewrite_of': None,
-                'anpa_take_key': None,
-                'rewrite_sequence': None,
-                'sequence': None,
-            })
-
-            # unlink original
-            superdesk.get_resource_service('archive').system_update(main['_id'], {'rewritten_by': None}, main)
 
 
 def assign_product_to_pictures(updates, original):
--- server/ansa/stage_auto_publishing.py
+++ server/ansa/stage_auto_publishing.py
@@ -1,10 +1,13 @@
 
+import logging
 import cerberus
 import superdesk
 
-from superdesk.logging import logger
+from superdesk.metadata.item import PUBLISH_STATES
 
 AUTO_PUBLISH_FIELD = 'auto_publish'
+
+logger = logging.getLogger(__name__)
 
 
 def item_fetched_handler(sender, item, **kwargs):
@@ -15,6 +18,27 @@ def item_moved_handler(sender, item, **kwargs):
     publish_item_on_auto_publish_stage(item)
 
 
+def unlink_update_on_auto_publish(item, updates):
+    """
+    Unlink auto published item when original is not published yet.
+
+    SDANSA-385
+    """
+    if item.get('rewrite_of'):
+        main = superdesk.get_resource_service('archive').find_one(req=None, _id=item['rewrite_of'])
+        if main and main.get('state') not in PUBLISH_STATES:
+            # unlink published item
+            updates.update({
+                'rewrite_of': None,
+                'anpa_take_key': None,
+                'rewrite_sequence': None,
+                'sequence': None,
+            })
+
+            # unlink original
+            superdesk.get_resource_service('archive').system_update(main['_id'], {'rewritten_by': None}, main)
+
+
 def publish_item_on_auto_publish_stage(item):
     stage_id = item.get('task', {}).get('stage')
     if not stage_id:
@@ -23,8 +47,9 @@ def publish_item_on_auto_publish_stage(item):
     stage = superdesk.get_resource_service('stages').find_one(req=None, _id=stage_id)
     if stage and stage.get(AUTO_PUBLISH_FIELD):
         try:
-            superdesk.get_resource_service('archive_publish').patch(item[superdesk.config.ID_FIELD],
-                                                                    {'auto_publish': True})
+            updates = {'auto_publish': True}
+            unlink_update_on_auto_publish(item, updates)
+            superdesk.get_resource_service('archive_publish').patch(item[superdesk.config.ID_FIELD], updates)
         except cerberus.cerberus.ValidationError:
             logger.error('item was not auto published item=%s stage=%s', item[superdesk.config.ID_FIELD], stage['name'])
 
