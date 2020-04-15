db2dae3b1d5796141c2f7ad5d7823d977844c153
MarkLark86 <mark.pittaway@gmail.com>
2020-04-15 21:55:27 +1000

    Update Planning to release/1.33 (#3153)
    * Update Planning to release/1.33
    
    * Fix planning-extension import

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import planningExtension from 'superdesk-planning-extension';
 import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
 import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
 import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
