fc79dfced6db9e0bff18e97c3d7034d4db3d5065
Mark Pittaway <mark.pittaway@gmail.com>
2020-07-15 18:10:43 +1000

    [SDESK-5363] Add SAMS config

---
+++ server/sams/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2020 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+"""SAMS specific settings"""
+
+SAMS_AUTH_TYPE = 'sams.auth.public'
