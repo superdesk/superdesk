74c4188dcf2430d77430e82170f169fc2f50ba39
Mark Pittaway <mark.pittaway@gmail.com>
2020-08-06 14:32:07 +1000

    SAMS: Remove SAMS_ prefix in settings

--- server/sams/settings.py
+++ server/sams/settings.py
@@ -14,13 +14,14 @@
 SAMS_AUTH_TYPE = 'sams.auth.public'
 
 # The following is to be removed once fireq SAMS env. var issues are resolved
-SAMS_HOST = 'localhost'
-SAMS_PORT = '5700'
-SAMS_URL = 'http://localhost:5700'
-SAMS_MONGO_DBNAME = 'sd-sams_sams'
-SAMS_MONGO_URI = 'mongodb://data-sd/sd-sams_sams'
-SAMS_ELASTICSEARCH_URL = 'http://data-sd:9201'
-SAMS_ELASTICSEARCH_INDEX = 'sd-sams_sams'
+HOST = 'localhost'
+PORT = '5700'
+SERVER_URL = 'http://localhost:5700'
+MONGO_DBNAME = 'sd-sams_sams'
+MONGO_URI = 'mongodb://data-sd/sd-sams_sams'
+ELASTICSEARCH_URL = 'http://data-sd:9201'
+ELASTICSEARCH_INDEX = 'sd-sams_sams'
+
 STORAGE_DESTINATION_1 = 'MongoGridFS,Default,mongodb://data-sd/sd-sams_sams'
 STORAGE_DESTINATION_2 = 'MongoGridFS,Media,mongodb://data-sd/sd-sams_sams'
 STORAGE_DESTINATION_3 = 'MongoGridFS,Publications,mongodb://data-sd/sd-sams_sams'
