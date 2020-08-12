6d3b67f8f6ab225c60fed377e34f2aac8d99840b
Karel Petrak <karel.petrak@sourcefabric.org>
2020-08-12 15:12:39 +0200

    Add annotations, datetime field extensions

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import {startApp} from 'superdesk-core/scripts/index';
 import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser';
+import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
+import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
 
 setTimeout(() => {
     startApp(
         [
             planningExtension,
             markForUserExtension,
+            annotationsLibraryExtension, 
+            datetimeFieldExtension
         ],
         {},
     );
