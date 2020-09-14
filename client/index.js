953dfc2a29bc34b563ab9e8b0c83425ecb877587
Tomas <t.kikutis@gmail.com>
2020-09-14 12:01:57 +0200

    auto-tagging-demo

--- client/index.js
+++ client/index.js
@@ -2,10 +2,11 @@ import {startApp} from 'superdesk-core/scripts/index';
 import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
 import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
 import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
+import autoTaggingWidget from 'superdesk-core/scripts/extensions/auto-tagging-widget/dist/src/extension';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, markForUserExtension, datetimeFieldExtension],
+        [annotationsLibraryExtension, markForUserExtension, datetimeFieldExtension, autoTaggingWidget],
         {},
     );
 });
