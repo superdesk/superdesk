ccee5ffcd04bb35c8f456fac2f44c154256e114b
Mark Pittaway <mark.pittaway@gmail.com>
2020-10-21 17:25:32 +1100

    Remove planning extension

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import {startApp} from 'superdesk-core/scripts/index';
 import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
 import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
 import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
-import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 import samsExtension from 'superdesk-core/scripts/extensions/sams/dist/src/extension';
 
 setTimeout(() => {
@@ -11,7 +10,6 @@ setTimeout(() => {
             annotationsLibraryExtension,
             markForUserExtension,
             datetimeFieldExtension,
-            planningExtension,
             samsExtension,
         ],
         {},
