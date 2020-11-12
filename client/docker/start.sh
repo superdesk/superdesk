054f419d3d14072350f5028d63fb3ba272cab26f
Petr Jašek <jasekpetr@gmail.com>
2020-11-12 10:40:59 +0100

    update docker config (#3210)
    * update docker config
    
    * set schedule for testing
    
    * update setup
    
    * fix docker test

--- client/docker/start.sh
+++ client/docker/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cd /opt/superdesk/client/dist &&
+set -e
+
+cd /opt/superdesk/client/dist
+
+# replace default client config with env vars
 sed -i \
  -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" \
  -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
  -e "s/ws:\/\/0.0.0.0:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
  -e 's/iframely:{key:""}/iframely:{key:"'$IFRAMELY_KEY'"}/g' \
- app*.js &&
-nginx &
+ app*.js
 
-cd /opt/superdesk && sleep 5 && bash honcho start
+which nginx
+
+exec "$@"
