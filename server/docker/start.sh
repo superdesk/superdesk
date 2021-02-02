9a3bbe185eb914d22365c036e580d0f45af96878
Petr Jasek <jasekpetr@gmail.com>
2021-02-02 17:23:06 +0100

    wait for elastic before doing init on server

--- server/docker/start.sh
+++ server/docker/start.sh
@@ -3,6 +3,15 @@ set -e
 
 cd /opt/superdesk/
 
+# wait for elastic to be up
+printf 'waiting for elastic.'
+until $(curl --output /dev/null --silent --head --fail "${ELASTICSEARCH_URL}"); do
+    printf '.'
+    sleep .5
+done
+echo 'done.'
+
+
 # init dbs
 honcho run python3 manage.py app:initialize_data
 
