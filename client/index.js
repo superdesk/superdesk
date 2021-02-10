57dc2cf0a69a53194766c3eb81f4e443fdb70ac3
Tomas Kikutis <t.kikutis@gmail.com>
2021-02-10 14:10:16 +0100

    update extensions registration

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
-import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, markForUserExtension, datetimeFieldExtension],
+        [
+            {
+                id: 'annotationsLibrary',
+                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension').then((res) => res.default),
+            },
+            {
+                id: 'markForUser',
+                load: () => import('superdesk-core/scripts/extensions/markForUser/dist/src/extension').then((res) => res.default),
+            },
+            {
+                id: 'datetimeField',
+                load: () => import('superdesk-core/scripts/extensions/datetimeField/dist/src/extension').then((res) => res.default),
+            },
+        ],
         {},
     );
 });
