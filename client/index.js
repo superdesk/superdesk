63cef6fbf23f1882dfe8f3806d59f3debe2eb9a8
Mark Pittaway <mark.pittaway@gmail.com>
2021-02-17 17:16:52 +1100

    Update extensions registration

--- client/index.js
+++ client/index.js
@@ -1,13 +1,16 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
-import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, datetimeFieldExtension, planningExtension],
-        {},
-    );
+        [{
+            id: 'planning',
+            load: () => (
+                import('superdesk-planning/client/planning-extension/dist/extension')
+                    .then((res) => res.default)
+            ),
+        }],
+        {}
+    )
 });
 
 export default angular.module('main.superdesk', []);
