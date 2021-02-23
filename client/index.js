ef93f70d07f639d35c3c3b898923e42472705d24
Mark Pittaway <mark.pittaway@gmail.com>
2021-02-24 08:51:16 +1100

    Update extensions registration

--- client/index.js
+++ client/index.js
@@ -1,22 +1,30 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
-import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
-import samsExtension from 'superdesk-core/scripts/extensions/sams/dist/src/extension';
 import {SamsAttachmentsWidget} from 'superdesk-core/scripts/extensions/sams/dist/src/apps/samsAttachmentsWidget';
 
 setTimeout(() => {
     startApp(
-        [
-            annotationsLibraryExtension,
-            markForUserExtension,
-            datetimeFieldExtension,
-            samsExtension,
+         [
+            {
+                id: 'annotationsLibrary',
+                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension').then((res) => res.default),
+            },
+            {
+                id: 'markForUser',
+                load: () => import('superdesk-core/scripts/extensions/markForUser/dist/src/extension').then((res) => res.default),
+            },
+            {
+                id: 'datetimeField',
+                load: () => import('superdesk-core/scripts/extensions/datetimeField/dist/src/extension').then((res) => res.default),
+            },
+             {
+                 id: 'samsExtension',
+                 load: () => import('superdesk-core/scripts/extensions/sams/dist/src/extension').then((res) => res.default),
+             }
         ],
         {
             AuthoringAttachmentsWidget: SamsAttachmentsWidget,
-        },
-    );
+        }
+    )
 });
 
 export default angular.module('main.superdesk', []);
