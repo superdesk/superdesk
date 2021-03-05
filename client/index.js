67bc8db00b70e27e36daad8f9e7d5cd9c7a433bc
Mark Pittaway <mark.pittaway@gmail.com>
2021-03-05 15:34:39 +1100

    Revert "Update extensions registration (#3220)"
    This reverts commit 697267efd85f1ada39c9a184aaa2f6e53a250b8c.

--- client/index.js
+++ client/index.js
@@ -1,21 +1,11 @@
 import {startApp} from 'superdesk-core/scripts/index';
+import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
+import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
+import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
 
 setTimeout(() => {
     startApp(
-        [
-            {
-                id: 'annotationsLibrary',
-                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension').then((res) => res.default),
-            },
-            {
-                id: 'markForUser',
-                load: () => import('superdesk-core/scripts/extensions/markForUser/dist/src/extension').then((res) => res.default),
-            },
-            {
-                id: 'datetimeField',
-                load: () => import('superdesk-core/scripts/extensions/datetimeField/dist/src/extension').then((res) => res.default),
-            },
-        ],
+        [annotationsLibraryExtension, markForUserExtension, datetimeFieldExtension],
         {},
     );
 });
