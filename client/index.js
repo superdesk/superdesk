a97373a1b75ec3c226739384c06ae0f31de40133
Petr Jašek <jasekpetr@gmail.com>
2021-05-10 17:15:00 +0200

    redo extension loading (#3234)

--- client/index.js
+++ client/index.js
@@ -1,16 +1,24 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
-import datetimeFieldExtension from 'superdesk-core/scripts/extensions/datetimeField/dist/src/extension';
-import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
         [
-            annotationsLibraryExtension,
-            markForUserExtension,
-            datetimeFieldExtension,
-            planningExtension,
+            {
+                id: 'annotationsLibrary',
+                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary'),
+            },
+            {
+                id: 'markForUserExtension',
+                load: () => import('superdesk-core/scripts/extensions/markForUser'),
+            },
+            {
+                id: 'datetimeFieldExtension',
+                load: () => import('superdesk-core/scripts/extensions/datetimeField'),
+            },
+            {
+                id: 'planningExtension',
+                load: () => import('superdesk-planning/client/planning-extension'),
+            },
         ],
         {},
     );
