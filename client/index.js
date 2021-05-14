0be28680c6cb07c0eb7545885084b5be42beefcb
Tomas Kikutis <t.kikutis@gmail.com>
2021-05-14 16:22:14 +0200

    correct extension import paths, add planning extension

--- client/index.js
+++ client/index.js
@@ -5,19 +5,19 @@ setTimeout(() => {
         [
             {
                 id: 'annotationsLibrary',
-                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary'),
+                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension'),
             },
             {
                 id: 'markForUserExtension',
-                load: () => import('superdesk-core/scripts/extensions/markForUser'),
+                load: () => import('superdesk-core/scripts/extensions/markForUser/dist/src/extension'),
             },
             {
                 id: 'datetimeFieldExtension',
-                load: () => import('superdesk-core/scripts/extensions/datetimeField'),
+                load: () => import('superdesk-core/scripts/extensions/datetimeField/dist/src/extension'),
             },
             {
-                id: 'planningExtension',
-                load: () => import('superdesk-planning/client/planning-extension'),
+                id: 'planning-extension',
+                load: () => import('@superdesk/planning-extension/dist/planning-extension/src/extension'),
             },
         ],
         {},
