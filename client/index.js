b6236207fc2635527b98ba8f8a2476a255a6de14
Tomas Kikutis <t.kikutis@gmail.com>
2021-05-31 11:28:12 +0200

    use build tools (#3243)

--- client/index.js
+++ client/index.js
@@ -1,9 +1,13 @@
 import {startApp} from 'superdesk-core/scripts/index';
-import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
-        [planningExtension],
+        [
+            {
+                id: 'planning-extension',
+                load: () => import('superdesk-planning/client/planning-extension'),
+            },
+        ],
         {},
     );
 });
