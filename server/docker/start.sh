cb1f892e5108dda236814e5f75ba00662ceddbe2
Petr Jasek <jasekpetr@gmail.com>
2021-09-01 14:24:45 +0200

    fix docker dump restore

--- server/docker/start.sh
+++ server/docker/start.sh
@@ -16,10 +16,10 @@ honcho run python3 manage.py app:initialize_data
 
 if [[ -d dump ]] && [[ "${DEMO_DATA:0}" == "1" ]]; then
     echo 'installing demo data'
-    mongorestore -h mongodb -d test --gzip dump
+    mongorestore -h mongodb -d superdesk --gzip dump
     honcho run python3 manage.py app:rebuild_elastic_index
-else  # no dump, just create admin
-    echo 'installing new instance'
+else  # no dump
+    echo 'installing empty instance'
     honcho run python3 manage.py users:create -u admin -p admin -e admin@localhost --admin
 fi
 
