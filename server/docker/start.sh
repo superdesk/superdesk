d6c53c455b30e76ade147622469f113d919da92c
Petr Jasek <jasekpetr@gmail.com>
2021-09-07 17:34:54 +0200

    fix docker

--- server/docker/start.sh
+++ server/docker/start.sh
@@ -18,9 +18,9 @@ if [[ -d dump ]] && [[ "${DEMO_DATA:0}" == "1" ]]; then
     echo 'installing demo data'
     mongorestore -h mongodb -d superdesk --gzip dump
     honcho run python3 manage.py app:rebuild_elastic_index
-else  # no dump
-    echo 'installing empty instance'
-    honcho run python3 manage.py users:create -u admin -p admin -e admin@localhost --admin
 fi
+
+# make sure there is admin user, with dump it's noop
+honcho run python3 manage.py users:create -u admin -p admin -e admin@localhost --admin
 
 exec "$@"
