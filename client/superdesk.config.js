97c67e2186f6adf5cca502cbb37125b70be1900a
Petr Jasek <jasekpetr@gmail.com>
2024-02-12 17:46:50 +0100

    enable publisher

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -8,12 +8,14 @@ module.exports = function() {
     return {
         apps: [
             'superdesk-planning',
-            'superdesk-analytics',
+            'superdesk.analytics',
+            'superdesk-publisher',
         ],
         importApps: [
             '../index',
             'superdesk-planning',
             'superdesk-analytics',
+            'superdesk-publisher',
         ],
 
         defaultRoute: '/workspace/monitoring',
@@ -115,6 +117,22 @@ module.exports = function() {
                     allowed_fields_to_sort: [ 'publish_schedule' ]
                 }
             },
+        },
+
+        publisher: {
+            protocol: 'https',                /* http or https */
+            tenant: '',              /* tenant - semantically subdomain, '' is allowed */
+            domain: 'sp-publisher.superdesk.pro',           /* domain name for the publisher */
+            base: 'api/v2',                  /* api base path */
+
+            wsProtocol: 'wss',                /* ws or wss (websocket); if unspecified or '' defaults to 'wss' */
+            wsDomain: 'sp-publisher.superdesk.pro',  /* domain name (usually domain as above) */
+                                            /* e.g.: example.com, abc.example.com */
+                                            /* tenant, as above, is NOT used for websocket */
+            wsPath: '/ws',                    /* path to websocket root dir */
+            wsPort: '80',                   /* if not specified: defaults to 443 for wss, 80 for ws */
+            hideContentRoutesInPublishPane: false, /* hides routes of type "content" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+            hideCustomRoutesInPublishPane: false   /* hides routes of type "custom" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
         },
     };
 };
