9701b65d0d3447873d6bec3e6b19814132714266
Petr Jasek <jasekpetr@gmail.com>
2024-04-22 09:51:03 +0200

    update server requirements

--- server/docker/start.sh
+++ server/docker/start.sh
@@ -11,16 +11,10 @@ until $(curl --output /dev/null --silent --head --fail "${ELASTICSEARCH_URL}"); 
 done
 echo 'done.'
 
-# init dbs
+# init app
 honcho run python3 manage.py app:initialize_data
 
-if [[ -d dump ]] && [[ "${DEMO_DATA:0}" == "1" ]]; then
-    echo 'installing demo data'
-    mongorestore -h mongodb -d superdesk --gzip dump
-    honcho run python3 manage.py app:rebuild_elastic_index
-fi
-
-# make sure there is admin user, with dump it's noop
+# make sure there is admin:admin user
 honcho run python3 manage.py users:create -u admin -p admin -e admin@localhost --admin
 
 exec "$@"
