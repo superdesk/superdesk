3f990eac50e3ffff2df85fe4d3e10c9f67993eed
Ivan Jelić <99324896+IvanJelicSF@users.noreply.github.com>
2025-01-28 14:16:06 +0100

    Turning Publisher on (#3598)
    * Turning Publisher on
    
    * Syncing with develop version

--- client/index.ts
+++ client/index.ts
@@ -4,10 +4,6 @@ setTimeout(() => {
     startApp(
         [
             {
-                id: 'broadcasting',
-                load: () => import('superdesk-core/scripts/extensions/broadcasting'),
-            },
-            {
                 id: 'annotationsLibrary',
                 load: () => import('superdesk-core/scripts/extensions/annotationsLibrary'),
             },
@@ -35,6 +31,48 @@ setTimeout(() => {
             },
         ],
         {},
+        {
+            editor3: {
+                customBlocks: {
+                    getAdditionalWrapperAttributes: (_vocabulary, html) => {
+                        function getHighestHeadingText(el: HTMLElement): string | null {
+                            const headings: Array<keyof HTMLElementTagNameMap> = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+                            for (const tag of headings) {
+                                const result = el.querySelector(tag);
+
+                                if (result != null) {
+                                    return result.textContent;
+                                }
+                            }
+
+                            return null;
+                        }
+
+                        const tableCellContentElement: HTMLElement =
+                            new DOMParser().parseFromString(html, 'text/html').body;
+
+                        const replaceSpecialCharacters = (str) =>
+                            str.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // Remove accents
+                                .replace(/([^\w]+|\s+)/g, '-') // Replace space and other characters by hyphen
+                                .replace(/\-\-+/g, '-')	// replace multiple hyphens with one hyphen
+                                .replace(/(^-+|-+$)/g, ''); // remove extra hyphens from beginning or end of the string
+
+                        const heading: string | null = getHighestHeadingText(tableCellContentElement);
+
+                        const attributes: Array<{name: string; value: string}> = [
+                            {name: 'class', value: 'custom-block'},
+                        ];
+
+                        if (heading != null) {
+                            attributes.push({name: 'data-custom-block-title', value: replaceSpecialCharacters(heading).toLocaleLowerCase()});
+                        }
+
+                        return attributes;
+                    },
+                },
+            },
+        },
     );
 });
 
