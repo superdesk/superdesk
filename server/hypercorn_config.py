b2bdd24850dfa1496b1db14a24557d292d19e9cb
Petr Jašek <jasekpetr@gmail.com>
2026-03-31 09:05:52 +0200

    set default number of hypercorn workers based on CPU (#4096)

--- server/hypercorn_config.py
+++ server/hypercorn_config.py
@@ -1,14 +1,14 @@
 import os
+import multiprocessing
+
+DEFAULT_MAX_WORKERS = 8
 
 bind = "0.0.0.0:%s" % os.environ.get("PORT", "5000")
-_web_concurrency = os.environ.get("WEB_CONCURRENCY")
-if _web_concurrency is not None:
-    workers = int(_web_concurrency)
-else:
-    workers = 1
+default_workers = min(multiprocessing.cpu_count() + 1, DEFAULT_MAX_WORKERS)
+workers = int(os.environ.get("WEB_CONCURRENCY", default_workers))
+read_timeout = int(os.environ.get("WEB_TIMEOUT", 30))
 
 accesslog = "-"
 access_log_format = "%(m)s %(U)s status=%(s)s time=%(T)ss size=%(B)sb"
 
-use_reload = "SUPERDESK_RELOAD" in os.environ
-read_timeout = int(os.environ.get("WEB_TIMEOUT", 30))
+use_reloader = "SUPERDESK_RELOAD" in os.environ
