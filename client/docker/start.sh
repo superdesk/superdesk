262751f203b49094578d888d68f60dc148d6f708
bmg1919 <bmg1919@outlook.com>
2026-04-01 14:24:22 -0400

    Updated start.sh for docker container to modify init

--- client/docker/start.sh
+++ client/docker/start.sh
@@ -11,6 +11,13 @@ sed -i \
  -e 's/iframely:{key:""}/iframely:{key:"'$IFRAMELY_KEY'"}/g' \
  app*.js
 
+sed -i \
+ -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" \
+ -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
+ -e "s/ws:\/\/0.0.0.0:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
+ -e 's/iframely:{key:""}/iframely:{key:"'$IFRAMELY_KEY'"}/g' \
+ init*.js 
+
 which nginx
 
 exec "$@"
